4631d99c79276e086d829a4c01fa56374c9e6f36
Steven Penny <srpen6@gmail.com>
2021-04-29 15:49:11 -0500

    -func sortFormat(i int, j int, formats FormatList) bool {

--- client.go
+++ client.go
@@ -18,9 +18,6 @@ type Client struct {
 	// HTTPClient can be used to set a custom HTTP client.
 	// If not set, http.DefaultClient will be used
 	HTTPClient *http.Client
-
-	// decipherOpsCache cache decipher operations
-	decipherOpsCache DecipherOperationsCache
 }
 
 // GetVideo fetches video metadata
@@ -94,20 +91,6 @@ func (c *Client) httpGetBodyBytes(ctx context.Context, url string) ([]byte, erro
 	return io.ReadAll(resp.Body)
 }
 
-const (
-	jsvarStr   = "[a-zA-Z_\\$][a-zA-Z_0-9]*"
-	reverseStr = ":function\\(a\\)\\{" +
-		"(?:return )?a\\.reverse\\(\\)" +
-		"\\}"
-	spliceStr = ":function\\(a,b\\)\\{" +
-		"a\\.splice\\(0,b\\)" +
-		"\\}"
-	swapStr = ":function\\(a,b\\)\\{" +
-		"var c=a\\[0\\];a\\[0\\]=a\\[b(?:%a\\.length)?\\];a\\[b(?:%a\\.length)?\\]=c(?:;return a)?" +
-		"\\}"
-)
-
-
 type FormatList []Format
 
 // Quality returns a new FormatList filtered by quality, quality label or itag,
@@ -120,69 +103,4 @@ func (list FormatList) Quality(quality string) (result FormatList) {
 		}
 	}
 	return result
-}
-
-// sortFormat sorts video by resolution, FPS, codec (av01, vp9, avc1), bitrate
-// sorts audio by codec (mp4, opus), channels, bitrate, sample rate
-func sortFormat(i int, j int, formats FormatList) bool {
-
-	// Sort by Width
-	if formats[i].Width == formats[j].Width {
-		// Format 137 downloads slowly, give it less priority
-		// see https://github.com/kkdai/youtube/pull/171
-		switch 137 {
-		case formats[i].ItagNo:
-			return false
-		case formats[j].ItagNo:
-			return true
-		}
-
-		// Sort by FPS
-		if formats[i].FPS == formats[j].FPS {
-			if formats[i].FPS == 0 && formats[i].AudioChannels > 0 && formats[j].AudioChannels > 0 {
-				// Audio
-				// Sort by codec
-				codec := map[int]int{}
-				for _, index := range []int{i, j} {
-					if strings.Contains(formats[index].MimeType, "mp4") {
-						codec[index] = 1
-					} else if strings.Contains(formats[index].MimeType, "opus") {
-						codec[index] = 2
-					}
-				}
-				if codec[i] == codec[j] {
-					// Sort by Audio Channel
-					if formats[i].AudioChannels == formats[j].AudioChannels {
-						// Sort by Audio Bitrate
-						if formats[i].Bitrate == formats[j].Bitrate {
-							// Sort by Audio Sample Rate
-							return formats[i].AudioSampleRate > formats[j].AudioSampleRate
-						}
-						return formats[i].Bitrate > formats[j].Bitrate
-					}
-					return formats[i].AudioChannels > formats[j].AudioChannels
-				}
-				return codec[i] < codec[j]
-			}
-			// Video
-			// Sort by codec
-			codec := map[int]int{}
-			for _, index := range []int{i, j} {
-				if strings.Contains(formats[index].MimeType, "av01") {
-					codec[index] = 1
-				} else if strings.Contains(formats[index].MimeType, "vp9") {
-					codec[index] = 2
-				} else if strings.Contains(formats[index].MimeType, "avc1") {
-					codec[index] = 3
-				}
-			}
-			if codec[i] == codec[j] {
-				// Sort by Audio Bitrate
-				return formats[i].Bitrate > formats[j].Bitrate
-			}
-			return codec[i] < codec[j]
-		}
-		return formats[i].FPS > formats[j].FPS
-	}
-	return formats[i].Width > formats[j].Width
 }
