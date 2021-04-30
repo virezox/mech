905f4257ef1d877f3f31dbc0270122006216aaad
Steven Penny <srpen6@gmail.com>
2021-04-30 10:43:03 -0500

    deleted:    old.go

--- video.go
+++ video.go
@@ -1,29 +1,13 @@
 package youtube
 
 import (
+   "bytes"
    "encoding/json"
-   "errors"
    "fmt"
-   "io"
    "net/http"
-   "net/url"
 )
 
-// httpGetBodyBytes reads the whole HTTP body and returns it
-func httpGetBodyBytes(addr string) ([]byte, error) {
-   req, err := http.NewRequest(http.MethodGet, addr, nil)
-   if err != nil { return nil, err }
-   req.Header.Set("Range", "bytes=0-")
-   res, err := new(http.Client).Do(req)
-   if err != nil { return nil, err }
-   defer res.Body.Close()
-   switch res.StatusCode {
-   case http.StatusOK, http.StatusPartialContent:
-   default:
-      return nil, fmt.Errorf("unexpected status code: %v", res.StatusCode)
-   }
-   return io.ReadAll(res.Body)
-}
+const API = "https://www.youtube.com/get_video_info"
 
 type Video struct {
    Microformat struct {
@@ -31,45 +15,63 @@ type Video struct {
          Description struct {
             SimpleText string
          }
-         PublishDate        string
+         PublishDate string
          Title struct {
             SimpleText string
          }
-         ViewCount          string
+         ViewCount int `json:",string"`
       }
    }
    StreamingData struct {
-      DashManifestURL  string
+      DashManifestURL string
    }
 }
 
 // NewVideo fetches video metadata
 func NewVideo(id string) (Video, error) {
-   eurl := "https://youtube.googleapis.com/v/" + id
-   body, err := httpGetBodyBytes("https://youtube.com/get_video_info?video_id="+id+"&eurl="+eurl)
+   req, err := http.NewRequest(http.MethodGet, API, nil)
    if err != nil {
       return Video{}, err
    }
-   query, err := url.ParseQuery(string(body))
+   val := req.URL.Query()
+   val.Set("video_id", id)
+   req.URL.RawQuery = val.Encode()
+   req.Header.Set("Range", "bytes=0-")
+   res, err := new(http.Client).Do(req)
    if err != nil {
       return Video{}, err
    }
-   status := query.Get("status")
-   if status != "ok" {
-      return Video{}, fmt.Errorf(
-         "response status: %q, reason: %q", status, query.Get("reason"),
-      )
+   defer res.Body.Close()
+   switch res.StatusCode {
+   case http.StatusOK, http.StatusPartialContent:
+   default:
+      return Video{}, fmt.Errorf("StatusCode %v", res.StatusCode)
    }
-   playerResponse := query.Get("player_response")
-   if playerResponse == "" {
-      return Video{}, errors.New(
-         "no player_response found in the server's answer",
-      )
-   }
-   var prData Video
-   err = json.Unmarshal([]byte(playerResponse), &prData)
+   buf := new(bytes.Buffer)
+   buf.ReadFrom(res.Body)
+   req.URL.RawQuery = buf.String()
+   play := req.URL.Query().Get("player_response")
+   buf = bytes.NewBufferString(play)
+   var vid Video
+   err = json.NewDecoder(buf).Decode(&vid)
    if err != nil {
-      return Video{}, fmt.Errorf("unable to parse player response JSON %v", err)
+      return Video{}, err
    }
-   return prData, nil
+   return vid, nil
+}
+
+func (v Video) Description() string {
+   return v.Microformat.PlayerMicroformatRenderer.Description.SimpleText
+}
+
+func (v Video) PublishDate() string {
+   return v.Microformat.PlayerMicroformatRenderer.PublishDate
+}
+
+func (v Video) Title() string {
+   return v.Microformat.PlayerMicroformatRenderer.Title.SimpleText
+}
+
+func (v Video) ViewCount() int {
+   return v.Microformat.PlayerMicroformatRenderer.ViewCount
 }
