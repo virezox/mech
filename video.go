7b744ecc365acd35496783098eb515bf601efbfc
Steven Penny <srpen6@gmail.com>
2021-05-01 18:37:11 -0500

    deleted:    response_data.go

--- video.go
+++ video.go
@@ -1,6 +1,7 @@
 package youtube
 
 import (
+   "bytes"
    "encoding/json"
    "errors"
    "fmt"
@@ -53,13 +54,6 @@ func (c *Client) GetVideo(id string) (*Video, error) {
    return &v, err
 }
 
-// GetStream returns the HTTP response for a specific format
-func (c *Client) GetStream(video *Video, format *Format) (*http.Response, error) {
-   url, err := c.GetStreamURL(video, format)
-   if err != nil { return nil, err }
-   return c.httpGet(url)
-}
-
 // GetStreamURL returns the url for a specific format
 func (c *Client) GetStreamURL(video *Video, format *Format) (string, error) {
    if format.URL != "" { return format.URL, nil }
@@ -101,3 +95,139 @@ func (c *Client) httpGetBodyBytes(url string) ([]byte, error) {
 
 	return io.ReadAll(resp.Body)
 }
+
+
+const API = "https://www.youtube.com/get_video_info"
+
+type oldVideo struct {
+   Microformat struct {
+      PlayerMicroformatRenderer struct {
+         Description struct {
+            SimpleText string
+         }
+         PublishDate string
+         Title struct {
+            SimpleText string
+         }
+         ViewCount int `json:",string"`
+      }
+   }
+   StreamingData struct {
+      DashManifestURL string
+   }
+}
+
+// NewVideo fetches video metadata
+func NewVideo(id string) (oldVideo, error) {
+   req, err := http.NewRequest(http.MethodGet, API, nil)
+   if err != nil {
+      return oldVideo{}, err
+   }
+   val := req.URL.Query()
+   val.Set("video_id", id)
+   req.URL.RawQuery = val.Encode()
+   req.Header.Set("Range", "bytes=0-")
+   res, err := new(http.Client).Do(req)
+   if err != nil {
+      return oldVideo{}, err
+   }
+   defer res.Body.Close()
+   switch res.StatusCode {
+   case http.StatusOK, http.StatusPartialContent:
+   default:
+      return oldVideo{}, fmt.Errorf("StatusCode %v", res.StatusCode)
+   }
+   buf := new(bytes.Buffer)
+   buf.ReadFrom(res.Body)
+   req.URL.RawQuery = buf.String()
+   play := req.URL.Query().Get("player_response")
+   buf = bytes.NewBufferString(play)
+   var vid oldVideo
+   err = json.NewDecoder(buf).Decode(&vid)
+   if err != nil {
+      return oldVideo{}, err
+   }
+   return vid, nil
+}
+
+func (v Video) Description() string {
+   return v.Microformat.PlayerMicroformatRenderer.Description.SimpleText
+}
+
+func (v oldVideo) PublishDate() string {
+   return v.Microformat.PlayerMicroformatRenderer.PublishDate
+}
+
+func (v oldVideo) Title() string {
+   return v.Microformat.PlayerMicroformatRenderer.Title.SimpleText
+}
+
+func (v oldVideo) ViewCount() int {
+   return v.Microformat.PlayerMicroformatRenderer.ViewCount
+}
+
+type Video struct {
+   // FIXME kill this
+   ID string
+   PlayabilityStatus struct {
+      Status          string
+      Reason          string
+      PlayableInEmbed bool
+      ContextParams   string
+   }
+   StreamingData struct {
+      ExpiresInSeconds string
+      Formats          []Format
+      AdaptiveFormats  []Format
+   }
+   Microformat struct {
+      PlayerMicroformatRenderer struct {
+         Thumbnail struct {
+            Thumbnails []struct {
+               URL    string
+               Width  int
+               Height int
+            }
+         }
+         Title struct {
+            SimpleText string
+         }
+         Description struct {
+            SimpleText string
+         }
+         LengthSeconds      string
+         OwnerProfileURL    string
+         ExternalChannelID  string
+         AvailableCountries []string
+         IsUnlisted         bool
+         HasYpcMetadata     bool
+         ViewCount          string
+         Category           string
+         PublishDate        string
+         OwnerChannelName   string
+         UploadDate         string
+      }
+   }
+}
+
+type Format struct {
+   ApproxDurationMs string
+   AudioChannels    int
+   AudioQuality     string
+   AudioSampleRate  string
+   AverageBitrate   int
+   Bitrate          int
+   Cipher           string `json:"signatureCipher"`
+   ContentLength    string
+   FPS              int
+   Height           int
+   ItagNo           int    `json:"itag"`
+   LastModified     string
+   MimeType         string
+   ProjectionType   string
+   Quality          string
+   QualityLabel     string
+   URL              string
+   Width            int
+}
+
