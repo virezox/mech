9ac94b9a510c1400ba8c2096b6f93041506a43c6
Steven Penny <srpen6@gmail.com>
2021-05-01 19:07:58 -0500

    +   queryParams, err := url.ParseQuery(cipher)

--- decipher.go
+++ decipher.go
@@ -11,76 +11,63 @@ import (
 )
 
 func (c *Client) decipherURL(videoID string, cipher string) (string, error) {
-	queryParams, err := url.ParseQuery(cipher)
-	if err != nil {
-		return "", err
-	}
+   queryParams, err := url.ParseQuery(cipher)
+   if err != nil { return "", err }
+   /* eg:
+   extract decipher from  https://youtube.com/s/player/4fbb4d5b/player_ias.vflset/en_US/base.js
 
-	/* eg:
-	    extract decipher from  https://youtube.com/s/player/4fbb4d5b/player_ias.vflset/en_US/base.js
+   var Mt={
+   splice:function(a,b){a.splice(0,b)},
+   reverse:function(a){a.reverse()},
+   EQ:function(a,b){var c=a[0];a[0]=a[b%a.length];a[b%a.length]=c}};
 
-	    var Mt={
-		splice:function(a,b){a.splice(0,b)},
-		reverse:function(a){a.reverse()},
-		EQ:function(a,b){var c=a[0];a[0]=a[b%a.length];a[b%a.length]=c}};
-
-		a=a.split("");
-		Mt.splice(a,3);
-		Mt.EQ(a,39);
-		Mt.splice(a,2);
-		Mt.EQ(a,1);
-		Mt.splice(a,1);
-		Mt.EQ(a,35);
-		Mt.EQ(a,51);
-		Mt.splice(a,2);
-		Mt.reverse(a,52);
-		return a.join("")
-	*/
-
-	operations, err := c.parseDecipherOpsWithCache(videoID)
-	if err != nil {
-		return "", err
-	}
-
-	// apply operations
-	bs := []byte(queryParams.Get("s"))
-	for _, op := range operations {
-		bs = op(bs)
-	}
-
-	decipheredURL := fmt.Sprintf("%s&%s=%s", queryParams.Get("url"), queryParams.Get("sp"), string(bs))
-	return decipheredURL, nil
+   a=a.split("");
+   Mt.splice(a,3);
+   Mt.EQ(a,39);
+   Mt.splice(a,2);
+   Mt.EQ(a,1);
+   Mt.splice(a,1);
+   Mt.EQ(a,35);
+   Mt.EQ(a,51);
+   Mt.splice(a,2);
+   Mt.reverse(a,52);
+   return a.join("")
+   */
+   operations, err := c.parseDecipherOpsWithCache(videoID)
+   if err != nil { return "", err }
+   // apply operations
+   bs := []byte(queryParams.Get("s"))
+   for _, op := range operations {
+      bs = op(bs)
+   }
+   decipheredURL := fmt.Sprintf("%s&%s=%s", queryParams.Get("url"), queryParams.Get("sp"), string(bs))
+   return decipheredURL, nil
 }
 
 const (
-	jsvarStr   = "[a-zA-Z_\\$][a-zA-Z_0-9]*"
-	reverseStr = ":function\\(a\\)\\{" +
-		"(?:return )?a\\.reverse\\(\\)" +
-		"\\}"
-	spliceStr = ":function\\(a,b\\)\\{" +
-		"a\\.splice\\(0,b\\)" +
-		"\\}"
-	swapStr = ":function\\(a,b\\)\\{" +
-		"var c=a\\[0\\];a\\[0\\]=a\\[b(?:%a\\.length)?\\];a\\[b(?:%a\\.length)?\\]=c(?:;return a)?" +
-		"\\}"
+   jsvarStr = "[a-zA-Z_\\$][a-zA-Z_0-9]*"
+   reverseStr = ":function\\(a\\)\\{(?:return )?a\\.reverse\\(\\)\\}"
+   spliceStr = ":function\\(a,b\\)\\{a\\.splice\\(0,b\\)\\}"
 )
 
 var (
-	basejsPattern = regexp.MustCompile(`(/s/player/\w+/player_ias.vflset/\w+/base.js)`)
-
-	actionsObjRegexp = regexp.MustCompile(fmt.Sprintf(
-		"var (%s)=\\{((?:(?:%s%s|%s%s|%s%s),?\\n?)+)\\};", jsvarStr, jsvarStr, swapStr, jsvarStr, spliceStr, jsvarStr, reverseStr))
-
-	actionsFuncRegexp = regexp.MustCompile(fmt.Sprintf(
-		"function(?: %s)?\\(a\\)\\{"+
-			"a=a\\.split\\(\"\"\\);\\s*"+
-			"((?:(?:a=)?%s\\.%s\\(a,\\d+\\);)+)"+
-			"return a\\.join\\(\"\"\\)"+
-			"\\}", jsvarStr, jsvarStr, jsvarStr))
-
-	reverseRegexp = regexp.MustCompile(fmt.Sprintf("(?m)(?:^|,)(%s)%s", jsvarStr, reverseStr))
-	spliceRegexp  = regexp.MustCompile(fmt.Sprintf("(?m)(?:^|,)(%s)%s", jsvarStr, spliceStr))
-	swapRegexp    = regexp.MustCompile(fmt.Sprintf("(?m)(?:^|,)(%s)%s", jsvarStr, swapStr))
+   swapStr = fmt.Sprint(
+      `:function\(a,b\)\{var c=a\[0\];a\[0\]=a\[b(?:%a\.length)?\];`,
+      `a\[b(?:%a\.length)?\]=c(?:;return a)?\}`,
+   )
+   basejsPattern = regexp.MustCompile(`(/s/player/\w+/player_ias.vflset/\w+/base.js)`)
+   actionsObjRegexp = regexp.MustCompile(fmt.Sprintf(
+      "var (%s)=\\{((?:(?:%s%s|%s%s|%s%s),?\\n?)+)\\};",
+      jsvarStr, jsvarStr, swapStr, jsvarStr, spliceStr, jsvarStr, reverseStr,
+   ))
+   reverseRegexp = regexp.MustCompile(fmt.Sprintf("(?m)(?:^|,)(%s)%s", jsvarStr, reverseStr))
+   spliceRegexp  = regexp.MustCompile(fmt.Sprintf("(?m)(?:^|,)(%s)%s", jsvarStr, spliceStr))
+   swapRegexp    = regexp.MustCompile(fmt.Sprintf("(?m)(?:^|,)(%s)%s", jsvarStr, swapStr))
+   actionsFuncRegexp = regexp.MustCompile(fmt.Sprintf(
+      `function(?: %s)?\(a\)\{a=a\.split\(""\);` +
+      `\s*((?:(?:a=)?%s\.%s\(a,\d+\);)+)return a\.join\(""\)\}`,
+      jsvarStr, jsvarStr, jsvarStr,
+   ))
 )
 
 func (c *Client) parseDecipherOps(videoID string) (operations []DecipherOperation, err error) {
