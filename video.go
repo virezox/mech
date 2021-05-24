5dda7f8aab6dd7c42cfdb621a4ff50f318d1d78a
Steven Penny <srpen6@gmail.com>
2021-05-24 11:36:21 -0500

    new file:   youtube/main.go

--- video.go
+++ video.go
@@ -12,69 +12,6 @@ import (
    "regexp"
 )
 
-func (f Format) request() (*http.Request, error) {
-   if f.URL != "" {
-      return http.NewRequest("GET", f.URL, nil)
-   }
-   baseJS, err := NewBaseJS()
-   if err != nil { return nil, err }
-   js, err := os.ReadFile(baseJS.Create)
-   if err != nil { return nil, err }
-   re := `\n[^.]+\.split\(""\);[^\n]+`
-   child := regexp.MustCompile(re).Find(js)
-   if child == nil {
-      return nil, fmt.Errorf("find %v", re)
-   }
-   re = `\w+`
-   childName := regexp.MustCompile(re).Find(child)
-   if childName == nil {
-      return nil, fmt.Errorf("find %v", re)
-   }
-   re = `;(\w+)`
-   parentName := regexp.MustCompile(re).FindSubmatch(child)
-   if parentName == nil {
-      return nil, fmt.Errorf("find %v", re)
-   }
-   re = fmt.Sprintf(`var %s=[^\n]+\n[^\n]+\n[^}]+}};`, parentName[1])
-   parent := regexp.MustCompile(re).Find(js)
-   if parent == nil {
-      return nil, fmt.Errorf("find %v", re)
-   }
-   val, err := url.ParseQuery(f.SignatureCipher)
-   if err != nil { return nil, err }
-   vm := otto.New()
-   vm.Run(string(parent) + string(child))
-   sig, err := vm.Call(string(childName), nil, val.Get("s"))
-   if err != nil { return nil, err }
-   req, err := http.NewRequest("GET", val.Get("url"), nil)
-   if err != nil { return nil, err }
-   val = req.URL.Query()
-   val.Set("sig", sig.String())
-   req.URL.RawQuery = val.Encode()
-   return req, nil
-}
-
-func (f Format) Write(w io.Writer) error {
-   req, err := f.request()
-   if err != nil { return err }
-   var pos int64
-   fmt.Println(invert, "GET", reset, req.URL)
-   for pos < f.ContentLength {
-      bytes := fmt.Sprintf("bytes=%v-%v", pos, pos+chunk-1)
-      req.Header.Set("Range", bytes)
-      fmt.Println(bytes)
-      res, err := new(http.Client).Do(req)
-      if err != nil { return err }
-      defer res.Body.Close()
-      if res.StatusCode != http.StatusPartialContent {
-         return fmt.Errorf("StatusCode %v", res.StatusCode)
-      }
-      io.Copy(w, res.Body)
-      pos += chunk
-   }
-   return nil
-}
-
 const (
    Origin = "https://www.youtube.com"
    chunk = 10_000_000
@@ -133,6 +70,69 @@ type Format struct {
    MimeType string
    SignatureCipher string
    URL string
+}
+
+func (f Format) Write(w io.Writer) error {
+   req, err := f.request()
+   if err != nil { return err }
+   var pos int64
+   fmt.Println(invert, "GET", reset, req.URL)
+   for pos < f.ContentLength {
+      bytes := fmt.Sprintf("bytes=%v-%v", pos, pos+chunk-1)
+      req.Header.Set("Range", bytes)
+      fmt.Println(bytes)
+      res, err := new(http.Client).Do(req)
+      if err != nil { return err }
+      defer res.Body.Close()
+      if res.StatusCode != http.StatusPartialContent {
+         return fmt.Errorf("StatusCode %v", res.StatusCode)
+      }
+      io.Copy(w, res.Body)
+      pos += chunk
+   }
+   return nil
+}
+
+func (f Format) request() (*http.Request, error) {
+   if f.URL != "" {
+      return http.NewRequest("GET", f.URL, nil)
+   }
+   baseJS, err := NewBaseJS()
+   if err != nil { return nil, err }
+   js, err := os.ReadFile(baseJS.Create)
+   if err != nil { return nil, err }
+   re := `\n[^.]+\.split\(""\);[^\n]+`
+   child := regexp.MustCompile(re).Find(js)
+   if child == nil {
+      return nil, fmt.Errorf("find %v", re)
+   }
+   re = `\w+`
+   childName := regexp.MustCompile(re).Find(child)
+   if childName == nil {
+      return nil, fmt.Errorf("find %v", re)
+   }
+   re = `;(\w+)`
+   parentName := regexp.MustCompile(re).FindSubmatch(child)
+   if parentName == nil {
+      return nil, fmt.Errorf("find %v", re)
+   }
+   re = fmt.Sprintf(`var %s=[^\n]+\n[^\n]+\n[^}]+}};`, parentName[1])
+   parent := regexp.MustCompile(re).Find(js)
+   if parent == nil {
+      return nil, fmt.Errorf("find %v", re)
+   }
+   val, err := url.ParseQuery(f.SignatureCipher)
+   if err != nil { return nil, err }
+   vm := otto.New()
+   vm.Run(string(parent) + string(child))
+   sig, err := vm.Call(string(childName), nil, val.Get("s"))
+   if err != nil { return nil, err }
+   req, err := http.NewRequest("GET", val.Get("url"), nil)
+   if err != nil { return nil, err }
+   val = req.URL.Query()
+   val.Set("sig", sig.String())
+   req.URL.RawQuery = val.Encode()
+   return req, nil
 }
 
 type Video struct {
--- youtube/youtube.go
+++ youtube/youtube.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-   "flag"
    "fmt"
    "github.com/89z/youtube"
-   "net/url"
    "os"
    "strings"
    "time"
@@ -66,72 +64,4 @@ func download(video youtube.Video, format youtube.Format) error {
    err != nil { return err }
    fmt.Println(time.Since(begin))
    return nil
-}
-
-func main() {
-   var (
-      atag, vtag int
-      info, update bool
-   )
-   flag.BoolVar(&info, "i", false, "info only")
-   flag.BoolVar(&update, "u", false, "update base.js")
-   flag.IntVar(&atag, "a", 251, "audio (0 to skip)")
-   flag.IntVar(&vtag, "v", 247, "video (0 to skip)")
-   flag.Parse()
-   if len(os.Args) == 1 {
-      fmt.Println("youtube [flags] [URL]")
-      flag.PrintDefaults()
-      return
-   }
-   // update
-   if update {
-      base, err := youtube.NewBaseJS()
-      if err != nil {
-         panic(err)
-      }
-      base.Get()
-      return
-   }
-   // check URL
-   if flag.NArg() != 1 {
-      panic("missing URL")
-   }
-   arg := flag.Arg(0)
-   watch, err := url.Parse(arg)
-   if err != nil {
-      panic(err)
-   }
-   id := watch.Query().Get("v")
-   video, err := youtube.NewVideo(id)
-   if err != nil {
-      panic(err)
-   }
-   // info
-   if info {
-      getInfo(video)
-      return
-   }
-   // check formats
-   var afmt, vfmt youtube.Format
-   if atag > 0 {
-      if afmt, err = video.NewFormat(atag); err != nil {
-         panic(err)
-      }
-   }
-   if vtag > 0 {
-      if vfmt, err = video.NewFormat(vtag); err != nil {
-         panic(err)
-      }
-   }
-   // download
-   if atag > 0 {
-      if err := download(video, afmt); err != nil {
-         panic(err)
-      }
-   }
-   if vtag > 0 {
-      if err := download(video, vfmt); err != nil {
-         panic(err)
-      }
-   }
 }
