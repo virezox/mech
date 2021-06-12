767260613ee9f4c3cfdf28552c6ca8a7d733038f
Steven Penny <srpen6@gmail.com>
2021-06-12 15:13:37 -0500

    deleted:    youtube/youtube/main.go

--- cmd/youtube/youtube.go
+++ cmd/youtube/youtube.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+   "flag"
    "fmt"
-   "github.com/89z/youtube"
+   "github.com/89z/mech/youtube"
+   "net/url"
    "os"
    "strings"
    "time"
@@ -68,4 +70,73 @@ func download(video youtube.Video, format youtube.Format) error {
    err != nil { return err }
    fmt.Println(time.Since(begin))
    return nil
+}
+
+
+func main() {
+   var (
+      atag, vtag int
+      info, update bool
+   )
+   flag.BoolVar(&info, "i", false, "info only")
+   flag.BoolVar(&update, "u", false, "update base.js")
+   flag.IntVar(&atag, "a", 251, "audio (0 to skip)")
+   flag.IntVar(&vtag, "v", 247, "video (0 to skip)")
+   flag.Parse()
+   if len(os.Args) == 1 {
+      fmt.Println("youtube [flags] [URL]")
+      flag.PrintDefaults()
+      return
+   }
+   // update
+   if update {
+      base, err := youtube.NewBaseJS()
+      if err != nil {
+         panic(err)
+      }
+      base.Get()
+      return
+   }
+   // check URL
+   if flag.NArg() != 1 {
+      panic("missing URL")
+   }
+   arg := flag.Arg(0)
+   watch, err := url.Parse(arg)
+   if err != nil {
+      panic(err)
+   }
+   id := watch.Query().Get("v")
+   video, err := youtube.NewVideo(id)
+   if err != nil {
+      panic(err)
+   }
+   // info
+   if info {
+      getInfo(video)
+      return
+   }
+   // check formats
+   var forms []youtube.Format
+   if atag > 0 {
+      format, err := video.NewFormat(atag)
+      if err != nil {
+         panic(err)
+      }
+      forms = append(forms, format)
+   }
+   if vtag > 0 {
+      format, err := video.NewFormat(vtag)
+      if err != nil {
+         panic(err)
+      }
+      forms = append(forms, format)
+   }
+   // download
+   for _, form := range forms {
+      err := download(video, form)
+      if err != nil {
+         panic(err)
+      }
+   }
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/89z/mech
 go 1.16
 
 require (
-	github.com/89z/youtube v1.10.2
 	github.com/robertkrimen/otto v0.0.0-20200922221731-ef014fd054ac
+	gopkg.in/sourcemap.v1 v1.0.5 // indirect
 )
