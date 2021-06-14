ce92f6f38cad8152208a79f7f2991adaa063bed3
Steven Penny <srpen6@gmail.com>
2021-06-14 12:21:33 -0500

    new file:   html.go

---
+++ html.go
@@ -0,0 +1,85 @@
+package mech
+
+import (
+   "golang.org/x/net/html"
+   "io"
+)
+
+func tag(t string) func(node) bool {
+   return func(n node) bool {
+      return n.Data == t
+   }
+}
+
+type node struct {
+   *html.Node
+}
+
+func NewNode(r io.Reader) (node, error) {
+   d, err := html.Parse(r)
+   if err != nil {
+      return node{}, err
+   }
+   return node{d}, nil
+}
+
+func (n node) Attr(key string) string {
+   for _, a := range n.Node.Attr {
+      if a.Key == key {
+         return a.Val
+      }
+   }
+   return ""
+}
+
+func (n node) ByAttrAll(key, val string) []node {
+   return n.selector(true, func(n node) bool {
+      for _, a := range n.Node.Attr {
+         if a.Key == key && a.Val == val {
+            return true
+         }
+      }
+      return false
+   })
+}
+
+func (n node) ByTag(name string) node {
+   for _, n := range n.selector(false, tag(name)) {
+      return n
+   }
+   return node{}
+}
+
+func (n node) ByTagAll(name string) []node {
+   return n.selector(true, tag(name))
+}
+
+func (n node) selector(all bool, f func(n node) bool) []node {
+   var (
+      in = []node{n}
+      out []node
+   )
+   for len(in) > 0 {
+      n := in[0]
+      if f(n) {
+         out = append(out, n)
+         if ! all {
+            break
+         }
+      }
+      for c := n.FirstChild; c != nil; c = c.NextSibling {
+         in = append(in, node{c})
+      }
+      in = in[1:]
+   }
+   return out
+}
+
+func (n node) Text() string {
+   for _, n := range n.selector(false, func(n node) bool {
+      return n.Type == html.TextNode
+   }) {
+      return n.Data
+   }
+   return ""
+}
--- rarbg/result.go
+++ rarbg/result.go
@@ -3,8 +3,7 @@ package rarbg
 import (
    "encoding/json"
    "fmt"
-   "golang.org/x/net/html"
-   "io"
+   "github.com/89z/mech"
    "net/http"
    "os"
    "regexp"
@@ -53,111 +52,32 @@ func NewResults(search, page string) ([]Result, error) {
       return nil, err
    }
    defer res.Body.Close()
-   doc, err := newNode(res.Body)
+   doc, err := mech.NewNode(res.Body)
    if err != nil {
       return nil, err
    }
    var results []Result
-   for _, tr := range doc.byAttrAll("class", "lista2") {
+   for _, tr := range doc.ByAttrAll("class", "lista2") {
       var r Result
-      td := tr.byTagAll("td")
+      td := tr.ByTagAll("td")
       // title
-      r.Title = td[1].byTag("a").text()
+      r.Title = td[1].ByTag("a").Text()
       // image
-      r.Image = td[1].byTag("a").attr("onmouseover")
+      r.Image = td[1].ByTag("a").Attr("onmouseover")
       r.Image = regexp.MustCompile(`\d+`).FindString(r.Image)
       r.Image = fmt.Sprintf(
          "https://dyncdn.me/mimages/%v/poster_opt.jpg", r.Image,
       )
       // torrent
-      r.Torrent = Origin + td[1].byTag("a").attr("href")
+      r.Torrent = Origin + td[1].ByTag("a").Attr("href")
       // genre
-      r.Genre = td[1].byTag("span").text()
+      r.Genre = td[1].ByTag("span").Text()
       // size
-      r.Size = td[3].text()
+      r.Size = td[3].Text()
       // seeders
-      r.Seeders = td[4].byTag("font").text()
+      r.Seeders = td[4].ByTag("font").Text()
       // append
       results = append(results, r)
    }
    return results, nil
-}
-
-func tag(t string) func(node) bool {
-   return func(n node) bool {
-      return n.Data == t
-   }
-}
-
-type node struct {
-   *html.Node
-}
-
-func newNode(r io.Reader) (node, error) {
-   d, err := html.Parse(r)
-   if err != nil {
-      return node{}, err
-   }
-   return node{d}, nil
-}
-
-func (n node) attr(key string) string {
-   for _, a := range n.Attr {
-      if a.Key == key {
-         return a.Val
-      }
-   }
-   return ""
-}
-
-func (n node) byAttrAll(key, val string) []node {
-   return n.selector(true, func(n node) bool {
-      for _, a := range n.Attr {
-         if a.Key == key && a.Val == val {
-            return true
-         }
-      }
-      return false
-   })
-}
-
-func (n node) byTag(name string) node {
-   for _, n := range n.selector(false, tag(name)) {
-      return n
-   }
-   return node{}
-}
-
-func (n node) byTagAll(name string) []node {
-   return n.selector(true, tag(name))
-}
-
-func (n node) selector(all bool, f func(n node) bool) []node {
-   var (
-      in = []node{n}
-      out []node
-   )
-   for len(in) > 0 {
-      n := in[0]
-      if f(n) {
-         out = append(out, n)
-         if ! all {
-            break
-         }
-      }
-      for c := n.FirstChild; c != nil; c = c.NextSibling {
-         in = append(in, node{c})
-      }
-      in = in[1:]
-   }
-   return out
-}
-
-func (n node) text() string {
-   for _, n := range n.selector(false, func(n node) bool {
-      return n.Type == html.TextNode
-   }) {
-      return n.Data
-   }
-   return ""
 }
