9614155f25a667f73487347267d70bd31b688bd8
Steven Penny <srpen6@gmail.com>
2021-06-26 10:08:39 -0500

    +   WebP = 0

--- youtube/image.go
+++ youtube/image.go
@@ -1,73 +1,77 @@
 package youtube
+import "fmt"
 
-import (
-   "fmt"
-   "path"
+const (
+   JPG = 1
+   WebP = 0
 )
 
 const (
-   HeightCrop = iota
-   WidthAuto
-   WidthAutoHeightBlack
-   WidthBlack
+   WidthAutoHeightBlack = 0
+   WidthAuto = 10
+   WidthBlack = 20
+   HeightCrop = 30
 )
 
 var Images = []Image{
-   {"0.jpg", 360, WidthAutoHeightBlack},
-   {"0.webp", 360, WidthAutoHeightBlack},
-   {"1.jpg", 90, WidthBlack},
-   {"1.webp", 90, WidthBlack},
-   {"2.jpg", 90, WidthBlack},
-   {"2.webp", 90, WidthBlack},
-   {"3.jpg", 90, WidthBlack},
-   {"3.webp", 90, WidthBlack},
-   {"default.jpg", 90, WidthAutoHeightBlack},
-   {"default.webp", 90, WidthAutoHeightBlack},
-   {"hq1.jpg", 360, WidthBlack},
-   {"hq1.webp", 360, WidthBlack},
-   {"hq2.jpg", 360, WidthBlack},
-   {"hq2.webp", 360, WidthBlack},
-   {"hq3.jpg", 360, WidthBlack},
-   {"hq3.webp", 360, WidthBlack},
-   {"hq720.jpg", 720, WidthAuto},
-   {"hq720.webp", 720, WidthAuto},
-   {"hqdefault.jpg", 360, WidthAutoHeightBlack},
-   {"hqdefault.webp", 360, WidthAutoHeightBlack},
-   {"maxres1.jpg", 720, WidthBlack},
-   {"maxres1.webp", 720, WidthBlack},
-   {"maxres2.jpg", 720, WidthBlack},
-   {"maxres2.webp", 720, WidthBlack},
-   {"maxres3.jpg", 720, WidthBlack},
-   {"maxres3.webp", 720, WidthBlack},
-   {"maxresdefault.jpg", 720, WidthAuto},
-   {"maxresdefault.webp", 720, WidthAuto},
-   {"mq1.jpg", 180, HeightCrop},
-   {"mq1.webp", 180, HeightCrop},
-   {"mq2.jpg", 180, HeightCrop},
-   {"mq2.webp", 180, HeightCrop},
-   {"mq3.jpg", 180, HeightCrop},
-   {"mq3.webp", 180, HeightCrop},
-   {"mqdefault.jpg", 180, WidthAuto},
-   {"mqdefault.webp", 180, WidthAuto},
-   {"sd1.jpg", 480, WidthBlack},
-   {"sd1.webp", 480, WidthBlack},
-   {"sd2.jpg", 480, WidthBlack},
-   {"sd2.webp", 480, WidthBlack},
-   {"sd3.jpg", 480, WidthBlack},
-   {"sd3.webp", 480, WidthBlack},
-   {"sddefault.jpg", 480, WidthAutoHeightBlack},
-   {"sddefault.webp", 480, WidthAutoHeightBlack},
+   {720, WidthBlack, WebP, "maxres3"},
+   {720, WidthBlack, JPG, "maxres3"},
+   {720, WidthBlack, WebP, "maxres2"},
+   {720, WidthBlack, JPG, "maxres2"},
+   {720, WidthBlack, WebP, "maxres1"},
+   {720, WidthBlack, JPG, "maxres1"},
+   {720, WidthAuto, WebP, "maxresdefault"},
+   {720, WidthAuto, JPG, "maxresdefault"},
+   {720, WidthAuto, WebP, "hq720"},
+   {720, WidthAuto, JPG, "hq720"},
+   {480, WidthBlack, WebP, "sd3"},
+   {480, WidthBlack, JPG, "sd3"},
+   {480, WidthBlack, WebP, "sd2"},
+   {480, WidthBlack, JPG, "sd2"},
+   {480, WidthBlack, WebP, "sd1"},
+   {480, WidthBlack, JPG, "sd1"},
+   {480, WidthAutoHeightBlack, JPG, "sddefault"},
+   {480, WidthAutoHeightBlack, WebP, "sddefault"},
+   {360, WidthBlack, WebP, "hq3"},
+   {360, WidthBlack, JPG, "hq3"},
+   {360, WidthBlack, WebP, "hq2"},
+   {360, WidthBlack, JPG, "hq2"},
+   {360, WidthBlack, WebP, "hq1"},
+   {360, WidthBlack, JPG, "hq1"},
+   {360, WidthAutoHeightBlack, WebP, "hqdefault"},
+   {360, WidthAutoHeightBlack, JPG, "hqdefault"},
+   {360, WidthAutoHeightBlack, WebP, "0"},
+   {360, WidthAutoHeightBlack, JPG, "0"},
+   {180, WidthAuto, WebP, "mqdefault"},
+   {180, WidthAuto, JPG, "mqdefault"},
+   {180, HeightCrop, WebP, "mq3"},
+   {180, HeightCrop, JPG, "mq3"},
+   {180, HeightCrop, WebP, "mq2"},
+   {180, HeightCrop, JPG, "mq2"},
+   {180, HeightCrop, WebP, "mq1"},
+   {180, HeightCrop, JPG, "mq1"},
+   {90, WidthBlack, WebP, "3"},
+   {90, WidthBlack, JPG, "3"},
+   {90, WidthBlack, WebP, "2"},
+   {90, WidthBlack, JPG, "2"},
+   {90, WidthBlack, WebP, "1"},
+   {90, WidthBlack, JPG, "1"},
+   {90, WidthAutoHeightBlack, WebP, "default"},
+   {90, WidthAutoHeightBlack, JPG, "default"},
 }
-
-var imageDirs = map[string]string{".webp": "vi_webp", ".jpg": "vi"}
 
 type Image struct {
-   File string
-   Height int
-   Frame int
+   Height int64
+   Frame int64
+   Format int64
+   Base string
 }
 
+var imageDirs = map[int64]string{WebP: "vi_webp", JPG: "vi"}
+var imageExts = map[int64]string{WebP: "webp", JPG: "jpg"}
+
 func (i Image) Address(id string) string {
-   dir := imageDirs[path.Ext(i.File)]
-   return fmt.Sprintf("http://i.ytimg.com/%v/%v/%v", dir, id, i.File)
+   dir := imageDirs[i.Format]
+   ext := imageExts[i.Format]
+   return fmt.Sprintf("http://i.ytimg.com/%v/%v/%v.%v", dir, id, i.Base, ext)
 }
