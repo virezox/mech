29944f3779cf8fc3af57adaeb3b8bc7ef0e39990
Steven Penny <srpen6@gmail.com>
2021-07-06 12:27:55 -0500

    new file:   research/2021-07-06/playlist.go

---
+++ research/2021-07-06/playlist.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+   "fmt"
+   "github.com/89z/mech/musicbrainz"
+   "github.com/89z/mech/youtube"
+   "time"
+)
+
+func search(query string) (string, error) {
+   s, err := youtube.NewSearch(query)
+   if err != nil {
+      return "", err
+   }
+   for _, vid := range s.Videos() {
+      if vid.VideoID != "" {
+         return vid.VideoID, nil
+      }
+   }
+   return "", fmt.Errorf("%q fail", query)
+}
+
+func main() {
+   r, err := musicbrainz.NewRelease("a40cb6e9-c766-37c4-8677-7eb51393d5a1")
+   if err != nil {
+      panic(err)
+   }
+   artist := r.ArtistCredit[0].Name
+   for _, m := range r.Media {
+      for i, track := range m.Tracks {
+         id, err := search(artist + " " + track.Title)
+         if err != nil {
+            panic(err)
+         }
+         fmt.Println(i, track.Title, id)
+         time.Sleep(100 * time.Millisecond)
+      }
+   }
+}
---
+++ research/2021-07-06/readme.md
@@ -0,0 +1,27 @@
+# July 6 2021
+
+~~~
+0 {VideoID:qW9MHtYeQCA}
+1 {VideoID:}
+2 {VideoID:Nw6k8JdZmo8}
+3 {VideoID:nGj5N9Ll9pI}
+4 {VideoID:OqlKiC7fD6E}
+5 {VideoID:}
+6 {VideoID:qd3Q8arE_Yk}
+7 {VideoID:}
+8 {VideoID:PyqYFBBCWKI}
+9 {VideoID:G1flq8LKkzk}
+10 {VideoID:0qi7vEI42MY}
+11 {VideoID:}
+12 {VideoID:}
+13 {VideoID:}
+14 {VideoID:}
+15 {VideoID:t1UTeTcGd5I}
+16 {VideoID:}
+17 {VideoID:}
+18 {VideoID:iTjn1d-d2II}
+19 {VideoID:}
+20 {VideoID:_vhnMkcK5yo}
+21 {VideoID:GIqx2iFygmE}
+22 {VideoID:hD0c_5bmg9M}
+~~~
