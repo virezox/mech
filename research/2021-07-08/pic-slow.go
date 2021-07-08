3c5ce6edab76142675fc906edaf11813a1fc0118
Steven Penny <srpen6@gmail.com>
2021-07-08 18:54:15 -0500

    deleted:    research/2021-07-08/pic-fast.go

--- research/2021-07-08/pic-slow.go
+++ research/2021-07-08/pic-slow.go
@@ -35,18 +35,21 @@ func absDiff(a, b uint32) uint32 {
 func (a picture) difference(b *picture) float32 {
    var (
       part uint32
-      total float32
+      total uint64
    )
    r := a.Bounds()
    for x := r.Min.X; x < r.Max.X; x++ {
       for y := r.Min.Y; y < r.Max.Y; y++ {
          r1, g1, b1, _ := a.At(x, y).RGBA()
          r2, g2, b2, _ := b.At(x, y).RGBA()
-         part += absDiff(r1, r2) + absDiff(g1, g2) + absDiff(b1, b2)
+         part += absDiff(r1, r2)
+         part += absDiff(g1, g2)
+         part += absDiff(b1, b2)
          total += 0xFFFF + 0xFFFF + 0xFFFF
       }
    }
-   return float32(part) / total
+   // 0.017706862
+   return float32(part) / float32(total)
 }
 
 func main() {
