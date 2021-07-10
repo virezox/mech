7cc46a3a5b752fc9b2659073c9e4f456b0d2198f
Steven Penny <srpen6@gmail.com>
2021-07-10 13:59:09 -0500

    new file:   picture/readme.md

--- picture/idct2.go
+++ picture/idct2.go
@@ -18,53 +18,53 @@ func fsh(x int) int {
    return x * 4096
 }
 
-type transform struct {
-   x0 int
-   x1 int
-   x2 int
-   x3 int
+type inverseDCT struct {
+   t0, t1, t2, t3 []int
+   x0, x1, x2, x3 int
 }
 
-func idct_1d(s0, s1, s2, s3, s4, s5, s6, s7, []int) transform {
-   t0 := make([]int, 1)
-   t1 := make([]int, 1)
-   t2 := make([]int, 1)
-   t3 := make([]int, 1)
+func newInverseDCT() inverseDCT {
+   var n inverseDCT
+   n.t0 = make([]int, 1)
+   n.t1 = make([]int, 1)
+   n.t2 = make([]int, 1)
+   n.t3 = make([]int, 1)
+   return n
+}
+
+func (n *inverseDCT) transform(s0, s1, s2, s3, s4, s5, s6, s7 []int) {
    p2 := s2
    p3 := s6
    p1 := (p2[0] + p3[0]) * f2f(0.5411961)
-   t2[0] = p1 + p3[0] * f2f(-1.847759065)
-   t3[0] = p1 + p2[0] * f2f(0.765366865)
+   n.t2[0] = p1 + p3[0] * f2f(-1.847759065)
+   n.t3[0] = p1 + p2[0] * f2f(0.765366865)
    p2 = s0
    p3 = s4
-   t0[0] = fsh(p2[0] + p3[0])
-   t1[0] = fsh(p2[0] - p3[0])
-   dct := transform{
-      x0: t0[0] + t3[0],
-      x1: t1[0] + t2[0],
-      x2: t1[0] - t2[0],
-      x3: t0[0] - t3[0],
-   }
-   t0 = s7
-   t1 = s5
-   t2 = s3
-   t3 = s1
-   p3[0] = t0[0] + t2[0]
-   p4 := t1[0] + t3[0]
-   p1 = t0[0] + t3[0]
-   p2[0] = t1[0] + t2[0]
+   n.t0[0] = fsh(p2[0] + p3[0])
+   n.t1[0] = fsh(p2[0] - p3[0])
+   n.x0 = n.t0[0] + n.t3[0]
+   n.x3 = n.t0[0] - n.t3[0]
+   n.x1 = n.t1[0] + n.t2[0]
+   n.x2 = n.t1[0] - n.t2[0]
+   n.t0 = s7
+   n.t1 = s5
+   n.t2 = s3
+   n.t3 = s1
+   p3[0] = n.t0[0] + n.t2[0]
+   p4 := n.t1[0] + n.t3[0]
+   p1 = n.t0[0] + n.t3[0]
+   p2[0] = n.t1[0] + n.t2[0]
    p5 := (p3[0] + p4) * f2f(1.175875602)
-   t0[0] *= f2f(0.298631336)
-   t1[0] *= f2f(2.053119869)
-   t2[0] *= f2f(3.072711026)
-   t3[0] *= f2f(1.501321110)
+   n.t0[0] *= f2f(0.298631336)
+   n.t1[0] *= f2f(2.053119869)
+   n.t2[0] *= f2f(3.072711026)
+   n.t3[0] *= f2f(1.501321110)
    p1 = p5 + p1 * f2f(-0.899976223)
    p2[0] = p5 + p2[0] * f2f(-2.562915447)
    p3[0] *= f2f(-1.961570560)
    p4 *= f2f(-0.390180644)
-   t3[0] += p1 + p4
-   t2[0] += p2[0] + p3[0]
-   t1[0] += p2[0] + p4
-   t0[0] += p1 + p3[0]
-   return dct
+   n.t3[0] += p1 + p4
+   n.t2[0] += p2[0] + p3[0]
+   n.t1[0] += p2[0] + p4
+   n.t0[0] += p1 + p3[0]
 }
---
+++ picture/readme.md
@@ -0,0 +1,3 @@
+# Picture
+
+<https://github.com/nothings/stb/blob/8e51be04/stb_image.h#L2355-L2391>
