42f015b3aa83ea5c46c8adc7f56bce1ec5e89a3f
Steven Penny <srpen6@gmail.com>
2021-07-23 18:04:49 -0500

    deleted:    research/2021-07-23/test-2.go

--- research/2021-07-23/media.go
+++ research/2021-07-23/media.go
@@ -1,21 +1,58 @@
 package main
 
 import (
+   "encoding/json"
    "fmt"
    "github.com/89z/mech"
    "net/http"
 )
 
+var tests = []string{
+   "http://hrp.org.uk" +
+   "/tower-of-london/history-and-stories/tower-of-london-prison",
+   "http://independent.co.uk" +
+   "/news/world/americas/us-politics/b1873357.html",
+   "http://nytimes.com/2021/07/14/podcasts/the-daily" +
+   "/heat-wave-climate-change-pacific-northwest.html",
+   "http://youtube.com/watch?v=LxK5Ocehj10",
+}
+
+func video(doc *mech.Node) string {
+   script := doc.ByAttr("type", "application/ld+json")
+   for script.Scan() {
+      text := []byte(script.Text())
+      var article struct {
+         Video struct {
+            ContentURL string
+         }
+      }
+      json.Unmarshal(text, &article)
+      if article.Video.ContentURL != "" {
+         return article.Video.ContentURL
+      }
+   }
+   return ""
+}
+
+func audio(doc *mech.Node) string {
+   script := doc.ByAttr("type", "application/ld+json")
+   for script.Scan() {
+      text := []byte(script.Text())
+      var audio struct {
+         ContentURL string
+      }
+      json.Unmarshal(text, &audio)
+      if audio.ContentURL != "" {
+         return audio.ContentURL
+      }
+   }
+   return ""
+}
+
 func image(doc *mech.Node) string {
    img := doc.ByAttr("property", "og:image")
    img.Scan()
    return img.Attr("content")
-}
-
-var tests = []string{
-   "http://hrp.org.uk" +
-   "/tower-of-london/history-and-stories/tower-of-london-prison",
-   "http://youtube.com/watch?v=LxK5Ocehj10",
 }
 
 func main() {
@@ -30,6 +67,18 @@ func main() {
       if err != nil {
          panic(err)
       }
-      fmt.Println(image(doc))
+      img := image(doc)
+      if img != "" {
+         fmt.Println(img)
+      }
+      aud := audio(doc)
+      if aud != "" {
+         fmt.Println(aud)
+      }
+      vid := video(doc)
+      if vid != "" {
+         fmt.Println(vid)
+      }
+      fmt.Println()
    }
 }
