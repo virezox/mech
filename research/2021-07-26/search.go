c73bb01b789fc09759503ddba6f7d7393b53b93a
Steven Penny <srpen6@gmail.com>
2021-07-26 22:04:55 -0500

    +Nr90/imgsim:

--- research/2021-07-26/search.go
+++ research/2021-07-26/search.go
@@ -6,7 +6,6 @@ import (
    "github.com/Nr90/imgsim"
    "image/jpeg"
    "net/http"
-   "os"
    "time"
 )
 
@@ -35,14 +34,10 @@ var ids = []string{
    "vJMjpX4Ck2o", // good
 }
 
-const (
-   urlA =
-      "https://ia800309.us.archive.org/9/items" +
-      "/mbid-a40cb6e9-c766-37c4-8677-7eb51393d5a1" +
-      "/mbid-a40cb6e9-c766-37c4-8677-7eb51393d5a1-9261666555.jpg"
-   urlB = "http://i.ytimg.com/vi/11Bvzknjo2Q/hqdefault.jpg"
-   urlC = "http://i.ytimg.com/vi/jCMi9_6vnxk/hqdefault.jpg"
-)
+const mb =
+   "https://ia800309.us.archive.org/9/items" +
+   "/mbid-a40cb6e9-c766-37c4-8677-7eb51393d5a1" +
+   "/mbid-a40cb6e9-c766-37c4-8677-7eb51393d5a1-9261666555.jpg"
 
 var forms = []youtube.Image{
    {120, 90, 68, "default", youtube.JPG},
@@ -50,59 +45,34 @@ var forms = []youtube.Image{
    {480, 360, 270, "hqdefault", youtube.JPG},
 }
 
-func hash(addr string, crop bool) (*goimagehash.ImageHash, error) {
+func hash(addr string, img *youtube.Image) (imgsim.Hash, error) {
    r, err := http.Get(addr)
    if err != nil {
-      return nil, err
+      return 0, err
    }
    defer r.Body.Close()
    i, err := jpeg.Decode(r.Body)
    if err != nil {
-      return nil, err
+      return 0, err
    }
-   if crop {
-      height := 270
-      x0 := (480 - height) / 2
-      y0 := (360 - height) / 2
-      r := image.Rect(x0, y0, x0 + height, y0 + height)
-      i = i.(*image.YCbCr).SubImage(r)
+   if img != nil {
+      i = img.SubImage(i)
    }
-   return goimagehash.PerceptionHash(i)
+   return imgsim.AverageHash(i), nil
 }
+
 func main() {
-   f, err := os.Open("mb.jpg")
-   if err != nil {
-      panic(err)
-   }
-   defer f.Close()
-   i, err := jpeg.Decode(f)
-   if err != nil {
-      panic(err)
-   }
-   a, err := goimagehash.AverageHash(i)
+   a, err := hash(mb, nil)
    if err != nil {
       panic(err)
    }
    for _, id := range ids {
       for _, form := range forms {
-         res, err := http.Get(form.Address(id))
+         b, err := hash(form.Address(id), &form)
          if err != nil {
             panic(err)
          }
-         defer res.Body.Close()
-         i, err := jpeg.Decode(res.Body)
-         if err != nil {
-            panic(err)
-         }
-         b, err := goimagehash.AverageHash(form.SubImage(i))
-         if err != nil {
-            panic(err)
-         }
-         d, err := a.Distance(b)
-         if err != nil {
-            panic(err)
-         }
-         fmt.Println(d, id, form.Base)
+         fmt.Println(imgsim.Distance(a, b), id, form.Base)
          time.Sleep(100 * time.Millisecond)
       }
    }
