b7e9077710c078c806ee18c8fa83b1840d2c18db
Steven Penny <srpen6@gmail.com>
2021-08-03 09:14:22 -0500

    new file:   research/2021-08-03/write.go

--- research/2021-08-02/distance.go
+++ research/2021-08-02/distance.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+   "fmt"
    "github.com/89z/mech/musicbrainz"
    "github.com/89z/mech/youtube"
    "math"
    "net/http"
+   "sort"
+   "time"
 )
 
 var cache = make(map[string]int64)
@@ -68,4 +71,61 @@ type result struct {
    size float64
    distance float64
    youtube.Item
+}
+
+
+func main() {
+   musicbrainz.Verbose = true
+   youtube.Verbose = true
+   d := 4*time.Minute + 42*time.Second
+   t := musicbrainz.Track{
+      Length: d.Milliseconds(),
+   }
+   s, err := youtube.NewSearch("oneohtrix point never returnal returnal")
+   if err != nil {
+      panic(err)
+   }
+   for _, i := range s.Items() {
+      r, err := newResult(t, i)
+      if err != nil {
+         panic(err)
+      }
+      fmt.Printf(
+         "di %.3f du %.3f s %.3f %v %v\n",
+         r.distance, r.duration, r.size, r.VideoID(), r.Title(),
+      )
+   }
+}
+
+
+func main() {
+   musicbrainz.Verbose = true
+   r, err := musicbrainz.NewRelease("a40cb6e9-c766-37c4-8677-7eb51393d5a1")
+   if err != nil {
+      panic(err)
+   }
+   artist := r.ArtistCredit[0].Name
+   for _, med := range r.Media {
+      for _, t := range med.Tracks {
+         s, err := youtube.NewSearch(artist + " " + r.Title + " " + t.Title)
+         if err != nil {
+            panic(err)
+         }
+         var results []*result
+         for _, i := range s.Items() {
+            r, err := newResult(t, i)
+            if err != nil {
+               panic(err)
+            }
+            results = append(results, r)
+         }
+         sort.Slice(results, func(a, b int) bool {
+            return results[a].distance < results[b].distance
+         })
+         r := results[0]
+         fmt.Printf(
+            "%.3f %v %v\n", r.distance, r.VideoID(), r.Title(),
+         )
+      }
+   }
 }
---
+++ research/2021-08-03/write.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+)
+
+type song struct {
+}
+
+func main() {
+}
