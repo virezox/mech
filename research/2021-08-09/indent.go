1e3a4f60e0ad2dbad43b8f06b7cec3129662d2a6
Steven Penny <srpen6@gmail.com>
2021-08-09 10:10:03 -0500

    new file:   research/2021-08-09/indent.go

---
+++ research/2021-08-09/indent.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+   "encoding/xml"
+   "os"
+)
+
+func main() {
+   f, err := os.Open("outer.html")
+   if err != nil {
+      panic(err)
+   }
+   defer f.Close()
+   // decode
+   d := xml.NewDecoder(f)
+   d.Strict = false
+   type node struct {
+      Children []node `xml:",any"`
+      Text string `xml:",chardata"`
+      XMLName xml.Name
+   }
+   var x node
+   d.Decode(&x)
+   // encode
+   enc := xml.NewEncoder(os.Stdout)
+   enc.Indent("", " ")
+   enc.Encode(x)
+}
