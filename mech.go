4a75ebdb540157730bcc8ea001dfe0cca14826c2
Steven Penny <srpen6@gmail.com>
2021-08-10 10:17:47 -0500

    +type Scanner struct {

--- mech.go
+++ mech.go
@@ -34,67 +34,52 @@ func ReadRequest(r io.Reader) (*http.Request, error) {
    }, nil
 }
 
-type Node struct {
-   *html.Node
-   todo []*html.Node
-   callback func(*html.Node) bool
+type Scanner struct {
+   *html.Tokenizer
+   html.Token
 }
 
-func Parse(r io.Reader) (*Node, error) {
-   n, err := html.Parse(r)
-   if err != nil {
-      return nil, err
+func NewScanner(r io.Reader) Scanner {
+   return Scanner{
+      Tokenizer: html.NewTokenizer(r),
    }
-   return &Node{
-      n, []*html.Node{n}, func(*html.Node) bool {
-         return true
-      },
-   }, nil
 }
 
-func (n Node) Attr(key string) string {
-   for _, attr := range n.Node.Attr {
-      if attr.Key == key {
-         return attr.Val
+func (s Scanner) Attr(key string) string {
+   for _, a := range s.Token.Attr {
+      if a.Key == key {
+         return a.Val
       }
    }
    return ""
 }
 
-func (n Node) ByAttr(key, val string) *Node {
-   n.todo = []*html.Node{n.Node}
-   n.callback = func(c *html.Node) bool {
-      for _, attr := range c.Attr {
-         if attr.Key == key && attr.Val == val {
+func (s *Scanner) ScanAttr(key, val string) bool {
+   for {
+      if s.Next() == html.ErrorToken {
+         break
+      }
+      t := s.Tokenizer.Token()
+      for _, a := range t.Attr {
+         if a.Key == key && a.Val == val {
+            s.Token = t
             return true
          }
-      }
-      return false
-   }
-   return &n
-}
-
-// this can modify the struct now, as we are working with a copy
-func (n *Node) Scan() bool {
-   for len(n.todo) > 0 {
-      t := n.todo[0]
-      n.todo = n.todo[1:]
-      for c := t.FirstChild; c != nil; c = c.NextSibling {
-         n.todo = append(n.todo, c)
-      }
-      if n.callback(t) {
-         n.Node = t
-         return true
       }
    }
    return false
 }
 
-func (n Node) Text() string {
-   for c := n.FirstChild; c != nil; c = c.NextSibling {
-      if c.Type == html.TextNode {
-         return c.Data
+func (s *Scanner) ScanText() bool {
+   for {
+      n := s.Next()
+      if n == html.ErrorToken {
+         break
+      }
+      if n == html.TextToken {
+         s.Token = s.Tokenizer.Token()
+         return true
       }
    }
-   return ""
+   return false
 }
--- research/2021-08-10/query.go
+++ research/2021-08-10/query.go
@@ -2,61 +2,9 @@ package main
 
 import (
    "fmt"
-   "golang.org/x/net/html"
-   "io"
+   "github.com/89z/mech"
    "os"
 )
-
-type scanner struct {
-   *html.Tokenizer
-}
-
-func newScanner(r io.Reader) scanner {
-   return scanner{
-      html.NewTokenizer(r),
-   }
-}
-
-func (s scanner) text() string {
-   for {
-      n := s.Next()
-      if n == html.ErrorToken {
-         break
-      }
-      if n == html.TextToken {
-         return s.Token().Data
-      }
-   }
-   return ""
-}
-
-func (s scanner) token(key, val string) *token {
-   for {
-      if s.Next() == html.ErrorToken {
-         break
-      }
-      t := s.Token()
-      for _, a := range t.Attr {
-         if a.Key == key && a.Val == val {
-            return &token{t}
-         }
-      }
-   }
-   return nil
-}
-
-type token struct {
-   html.Token
-}
-
-func (t token) attr(key string) string {
-   for _, a := range t.Attr {
-      if a.Key == key {
-         return a.Val
-      }
-   }
-   return ""
-}
 
 func main() {
    f, err := os.Open("nyt.html")
@@ -64,8 +12,9 @@ func main() {
       panic(err)
    }
    defer f.Close()
-   s := newScanner(f)
-   t := s.token("type", "application/ld+json")
-   fmt.Println(t.attr("data-rh"))
-   fmt.Println(s.text())
+   s := mech.NewScanner(f)
+   s.ScanAttr("type", "application/ld+json")
+   fmt.Println(s.Attr("data-rh"))
+   s.ScanText()
+   fmt.Println(s.Data)
 }
