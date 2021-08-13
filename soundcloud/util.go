aba356241a4a56b0ec71b8b2d96bb42be6d4be3b
Steven Penny <srpen6@gmail.com>
2021-08-13 09:08:40 -0500

    +// FetchClientID fetches a SoundCloud client ID. This algorithm is adapted from

--- soundcloud/util.go
+++ soundcloud/util.go
@@ -98,69 +98,51 @@ func sliceContains(slice []int64, x int64) bool {
 	return false
 }
 
-// FetchClientID fetches a SoundCloud client ID.
-// This algorithm is adapted from:
-//     https://www.npmjs.com/package/soundcloud-key-fetch
+// FetchClientID fetches a SoundCloud client ID. This algorithm is adapted from
+// https://www.npmjs.com/package/soundcloud-key-fetch. The basic notion of how
+// this function works is that SoundCloud provides a client ID so its web app
+// can make API requests. This client ID (along with other intialization data
+// for the web app) is provided in a JavaScript file imported through a
+// <script> tag in the HTML. This function scrapes the HTML and tries to find
+// the URL to that JS file, and then scrapes the JS file to find the client ID.								//
 func FetchClientID() (string, error) {
-	// // // // // // // // // // // // // // // // // // // // // // // // // // // // //
-	// 																					//
-	// The basic notion of how this function works is that SoundCloud provides          //
-	// a client ID so its web app can make API requests.								//
-	//																					//
-	// This client ID (along with other intialization data for the web app) is provided //
-	// in a JavaScript file imported through a <script> tag in the HTML.				//
-	//																					//
-	// This function scrapes the HTML and tries to find the URL to that JS file,		//
-	// and then scrapes the JS file to find the client ID.								//
-	//																					//
-	// // // // // // // // // // // // // // // // // // // // // // // // // // // // //
-
-	resp, err := http.Get("https://soundcloud.com")
-	if err != nil {
-               return "", err
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-               return "", err
-	}
-
-	bodyString := string(body)
-	// The link to the JS file with the client ID looks like this:
-	// <script crossorigin src="https://a-v2.sndcdn.com/assets/sdfhkjhsdkf.js"></script
-	split := strings.Split(bodyString, `<script crossorigin src="`)
-	urls := []string{}
-
-	// Extract all the URLS that match our pattern
-	for _, raw := range split {
-		u := strings.Replace(raw, `"></script>`, "", 1)
-		u = strings.Split(u, "\n")[0]
-		if string([]rune(u)[0:31]) == "https://a-v2.sndcdn.com/assets/" {
-			urls = append(urls, u)
-		}
-	}
-
-	// It seems like our desired URL is always imported last,
-	// so we use urls[len(urls) - 1]
-	resp, err = http.Get(urls[len(urls)-1])
-	if err != nil {
-               return "", err
-	}
-
-	body, err = io.ReadAll(resp.Body)
-	if err != nil {
-               return "", err
-	}
-
-	bodyString = string(body)
-
-	// Extract the client ID
-	if strings.Contains(bodyString, `,client_id:"`) {
-		clientID := strings.Split(bodyString, `,client_id:"`)[1]
-		clientID = strings.Split(clientID, `"`)[0]
-		return clientID, nil
-	}
-      return "", fmt.Errorf("%v fail", bodyString)
+   resp, err := http.Get("https://soundcloud.com")
+   if err != nil {
+   return "", err
+   }
+   body, err := io.ReadAll(resp.Body)
+   if err != nil {
+   return "", err
+   }
+   bodyString := string(body)
+   // The link to the JS file with the client ID looks like this:
+   // <script crossorigin src="https://a-v2.sndcdn.com/assets/sdfhkjhsdkf.js"></script
+   split := strings.Split(bodyString, `<script crossorigin src="`)
+   urls := []string{}
+   // Extract all the URLS that match our pattern
+   for _, raw := range split {
+   u := strings.Replace(raw, `"></script>`, "", 1)
+   u = strings.Split(u, "\n")[0]
+   if string([]rune(u)[0:31]) == "https://a-v2.sndcdn.com/assets/" {
+   urls = append(urls, u)
+   }
+   }
+   // It seems like our desired URL is always imported last,
+   // so we use urls[len(urls) - 1]
+   resp, err = http.Get(urls[len(urls)-1])
+   if err != nil {
+   return "", err
+   }
+   body, err = io.ReadAll(resp.Body)
+   if err != nil {
+   return "", err
+   }
+   bodyString = string(body)
+   // Extract the client ID
+   if strings.Contains(bodyString, `,client_id:"`) {
+   clientID := strings.Split(bodyString, `,client_id:"`)[1]
+   clientID = strings.Split(clientID, `"`)[0]
+   return clientID, nil
+   }
+   return "", fmt.Errorf("%v fail", bodyString)
 }
-
-
