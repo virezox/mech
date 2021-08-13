f4779e4761a09ffae022ab056fb5fb41e6f68b47
Steven Penny <srpen6@gmail.com>
2021-08-13 09:15:36 -0500

    -   User              User   `json:"user"`

--- soundcloud/soundcloud.go
+++ soundcloud/soundcloud.go
@@ -125,7 +125,6 @@ type Track struct {
    TagList           string `json:"tag_list"`
    Title             string `json:"title"`
    URI               string `json:"uri"`
-   User              User   `json:"user"`
    UserID            int64  `json:"user_id"`
    WaveformURL       string `json:"waveform_url"`
 }
@@ -150,61 +149,6 @@ type TranscodingFormat struct {
 	MimeType string `json:"mime_type"`
 }
 
-// Playlist represents the JSON response of a playlist
-type Playlist struct {
-	ArtworkURL     string  `json:"artwork_url"`
-	CreatedAt      string  `json:"created_at"`
-	Description    string  `json:"description"`
-	DurationMS     int64   `json:"duration"`
-	EmbeddeableBy  string  `json:"embeddable_by"`
-	Genre          string  `json:"genre"`
-	ID             int64   `json:"id"`
-	Kind           string  `json:"kind"`
-	LabelName      string  `json:"label_name"`
-	LastModified   string  `json:"last_modified"`
-	License        string  `json:"license"`
-	LikesCount     int     `json:"likes_count"`
-	ManagedByFeeds bool    `json:"managed_by_feeds"`
-	Permalink      string  `json:"permalink"`
-	PermalinkURL   string  `json:"permalink_url"`
-	Public         bool    `json:"public"`
-	SecretToken    string  `json:"secret_token"`
-	Sharing        string  `json:"private"`
-	TagList        string  `json:"tag_list"`
-	Title          string  `json:"title"`
-	URI            string  `json:"uri"`
-	UserID         int64   `json:"user_id"`
-	SetType        string  `json:"set_type"`
-	IsAlbum        bool    `json:"is_album"`
-	PublishedAt    string  `json:"published_at"`
-	DisplayDate    string  `json:"display_date"`
-	User           User    `json:"user"`
-	Tracks         []Track `json:"tracks"`
-	TrackCount     int     `json:"track_count"`
-}
-
-// User represents the JSON payload for user data
-type User struct {
-	ID              int64  `json:"id"`
-	AvatarURL       string `json:"avatar_url"`
-	City            string `json:"city"`
-	CommentsCount   int64  `json:"comments_count"`
-	CountryCode     string `json:"country_code"`
-	CreatedAt       string `json:"created_at"`
-	Description     string `json:"description"`
-	FollowersCount  int64  `json:"followers_count"`
-	FollowingsCount int64  `json:"followings_count"`
-	FirstName       string `json:"first_name"`
-	LastName        string `json:"last_name"`
-	PermalinkURL    string `json:"permalink_url"`
-	URI             string `json:"uri"`
-	Username        string `json:"username"`
-	Kind            string `json:"kind"`
-	Likes           int    `json:"likes_count"`
-	PlaylistLikes   int    `json:"playlist_likes_count"`
-	Verified        bool   `json:"verified"`
-}
-
 // MediaURLResponse is the JSON response of retrieving media information of a track
 type MediaURLResponse struct {
 	URL string `json:"url"`
@@ -213,20 +157,4 @@ type MediaURLResponse struct {
 // DownloadURLResponse is the JSON respose of retrieving media information of a publicly downloadable track
 type DownloadURLResponse struct {
 	URL string `json:"redirectUri"`
-}
-
-// PaginatedQuery is the JSON response for a paginated query
-type PaginatedQuery struct {
-	Collection   []map[string]interface{} `json:"collection"`
-	TotalResults int                      `json:"total_results"`
-	NextHref     string                   `json:"next_href"`
-	QueryURN     string                   `json:"query_urn"`
-}
-
-// Like is the JSON response for a like
-type Like struct {
-	CreatedAt string   `json:"created_at"`
-	Kind      string   `json:"kind"`
-	Track     Track    `json:"track"`
-	Playlist  Playlist `json:"playlist"`
 }
