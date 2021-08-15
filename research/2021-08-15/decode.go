46c39e89a1644bb2f2bab550649f9bb7c996a5cb
Steven Penny <srpen6@gmail.com>
2021-08-15 12:47:51 -0500

    deleted:    research/2021-08-15/walk.go

--- research/2021-08-15/decode.go
+++ research/2021-08-15/decode.go
@@ -5,44 +5,49 @@ import (
    "github.com/tdewolff/parse/v2"
    "github.com/tdewolff/parse/v2/js"
    "io"
+   "os"
    "strconv"
    "strings"
 )
-
-type walker struct{}
-
-func (w walker) Enter(n js.INode) js.IVisitor {
-   switch n := n.(type) {
-   case *js.PropertyName:
-      s := string(n.Literal.Data)
-      n.Literal.Data = strconv.AppendQuote(nil, s)
-   }
-   return w
-}
-
-func (walker) Exit(js.INode) {}
-
-func decode(r io.Reader) (map[string]string, error) {
-   t, err := js.Parse(parse.NewInput(r))
-   if err != nil {
-      return nil, err
-   }
-   for _, s := range t.BlockStmt.List {
-      var w walker
-      js.Walk(w, s)
-      fmt.Println(s)
-   }
-   return nil, nil
-}
 
 const s = `
 apps = [12, 31];
 reference = {"month": 12, "day": 31};
 `
 
-func main() {
-   _, err := decode(strings.NewReader(s))
+func decode(r io.Reader) (map[string]string, error) {
+   ast, err := js.Parse(parse.NewInput(r))
    if err != nil {
-      panic(err)
+      return nil, err
    }
+   m := make(map[string]string)
+   for _, iStmt := range ast.BlockStmt.List {
+      eStmt, ok := iStmt.(*js.ExprStmt)
+      if !ok {
+         continue
+      }
+      bExpr, ok := eStmt.Value.(*js.BinaryExpr)
+      if !ok {
+         continue
+      }
+      m[bExpr.X.JS()] = bExpr.Y.JS()
+      /*
+      var w walker
+      js.Walk(w, s)
+      */
+   }
+   return m, nil
 }
+
+type walker struct{}
+
+func (w walker) Enter(n js.INode) js.IVisitor {
+   pName, ok := n.(*js.PropertyName)
+   if ok {
+      s := string(pName.Literal.Data)
+      pName.Literal.Data = strconv.AppendQuote(nil, s)
+   }
+   return w
+}
+
+func (walker) Exit(js.INode) {}
