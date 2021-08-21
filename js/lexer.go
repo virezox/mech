8782517e7655cf440daa70071eecf804b3e3fb27
Steven Penny <srpen6@gmail.com>
2021-08-20 19:20:00 -0500

    deleted:    research/2021-08-20/lexer.go

--- js/lexer.go
+++ js/lexer.go
@@ -21,42 +21,36 @@ func NewLexer(b []byte) Lexer {
 func (l Lexer) Values() map[string][]byte {
    vals := make(map[string][]byte)
    for {
-      // state 1: break if EqToken
       var (
+         in bool
          k string
-         inIdent bool
       )
+      // state 1: break if EqToken
       for {
-         if tt, data := l.Next(); tt == js.ErrorToken {
+         tt, data := l.Next()
+         if tt == js.ErrorToken {
             return vals
          } else if tt == js.EqToken {
             break
          } else if tt == js.IdentifierToken || tt == js.DotToken {
-            if inIdent {
+            if in {
                k += string(data)
             } else {
                k = string(data)
-               inIdent = true
+               in = true
             }
          } else {
-            inIdent = false
+            in = false
          }
       }
-      // state 2: break if !WhitespaceToken
-      for {
-         if tt, data := l.Next(); tt != js.WhitespaceToken {
-            if tt == js.DivToken {
-               tt, data = l.RegExp()
-            }
-            vals[k] = data
-            break
-         }
-      }
-      // state 3: break if SemicolonToken
+      // state 2: break if SemicolonToken
       for {
          tt, data := l.Next()
          if tt == js.SemicolonToken {
             break
+         } else if tt == js.DivToken && !in {
+            tt, data = l.RegExp()
+            in = true
          }
          vals[k] = append(vals[k], data...)
       }
