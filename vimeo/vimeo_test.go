17978345eeb7c0ddd3433100222d95918e435ca5
Steven Penny <srpen6@gmail.com>
2021-08-21 17:32:47 -0500

    +   stdhtml "html"

--- vimeo/vimeo_test.go
+++ vimeo/vimeo_test.go
@@ -5,14 +5,20 @@ import (
    "testing"
 )
 
+const id = "66531465"
+
 func TestConfig(t *testing.T) {
-   c, err := NewConfig("66531465")
+   c, err := NewConfig(id)
    if err != nil {
       t.Fatal(err)
    }
-   v, err := c.Video()
+   fmt.Printf("%+v\n", c)
+}
+
+func TestVideo(t *testing.T) {
+   v, err := NewVideo(id)
    if err != nil {
       t.Fatal(err)
    }
-   fmt.Printf("%+v\n", v)
+   fmt.Printf("%+v\n%v\n", v, v.Title())
 }
