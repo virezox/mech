ca3b82c26dc3588a3824a238d1dbc50098ebb95f
Steven Penny <srpen6@gmail.com>
2021-08-25 13:27:42 -0500

    new file:   research/2021-08-25/web.txt

--- research/2021-08-25/readme.md
+++ research/2021-08-25/readme.md
@@ -1,7 +1,20 @@
 # August 25 2021
 
-~~~
-MeJVWBSsPAY
-QWlNyzzwgcc
-aN76CmldknI publishDate
-~~~
+ID | Name
+---|----------
+.  | `ANDROID`
+.  | `ANDROID_CREATOR`
+.  | `ANDROID_EMBEDDED_PLAYER`
+.  | `ANDROID_KIDS`
+.  | `ANDROID_MUSIC`
+.  | `IOS`
+.  | `IOS_CREATOR`
+.  | `IOS_KIDS`
+.  | `IOS_MUSIC`
+.  | `MWEB`
+.  | `TVHTML5`
+.  | `WEB_EMBEDDED_PLAYER`
+1  | `WEB`
+62 | `WEB_REMIX`
+67 | `WEB_KIDS`
+76 | `WEB_CREATOR`
--- research/2021-08-25/search.go
+++ research/2021-08-25/search.go
@@ -3,7 +3,10 @@ package main
 import (
    "bytes"
    "encoding/json"
+   "fmt"
    "net/http"
+   "net/http/httputil"
+   "os"
 )
 
 type youTubeI struct {
@@ -24,10 +27,29 @@ func (i youTubeI) encode() (*bytes.Buffer, error) {
    return b, nil
 }
 
+var clients = []struct{name, version string}{
+   {"ANDROID", "16.02"},
+   {"ANDROID_CREATOR", "21.06.103"},
+   {"ANDROID_EMBEDDED_PLAYER", "16.02"},
+   {"ANDROID_KIDS", "6.02.3"},
+   {"ANDROID_MUSIC", "4.32"},
+   {"IOS", "16.02"},
+   {"IOS_CREATOR", "20.47.100"},
+   {"IOS_KIDS", "5.42.2"},
+   {"IOS_MUSIC", "4.16.1"},
+   {"MWEB", "2.19700101"},
+   {"TVHTML5", "7.20210224.00.00"},
+   {"WEB", "2.20210223.09.00"},
+   {"WEB_CREATOR", "1.20210223.01.00"},
+   {"WEB_EMBEDDED_PLAYER", "1.20210620.0.1"},
+   {"WEB_KIDS", "2.1.3"},
+   {"WEB_REMIX", "0.1"},
+}
+
 func main() {
    var i youTubeI
-   i.Context.Client.ClientName = "MWEB"
-   i.Context.Client.ClientVersion = "2.19700101"
+   i.Context.Client.ClientName = "WEB"
+   i.Context.Client.ClientVersion = "2.20210223.09.00"
    i.Query = "k4M53xndqiU"
    body, err := i.encode()
    if err != nil {
@@ -37,13 +59,39 @@ func main() {
       "POST", "https://www.youtube.com/youtubei/v1/search", body,
    )
    req.Header.Set("X-Goog-Api-Key", "AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8")
+   d, err := httputil.DumpRequest(req, true)
+   if err != nil {
+      panic(err)
+   }
+   os.Stdout.Write(d)
    res, err := new(http.Transport).RoundTrip(req)
    if err != nil {
       panic(err)
    }
    defer res.Body.Close()
+   fmt.Println(res.Status)
    var mw mweb
-   json.NewDecoder(res.Body).Decode(&mw)
+   if err := json.NewDecoder(res.Body).Decode(&mw); err != nil {
+      panic(err)
+   }
+   if mw.videoID() != "" {
+      fmt.Println(i.Context.Client, "pass")
+   } else {
+      fmt.Println(i.Context.Client, "fail")
+   }
+}
+
+func (m mweb) videoID() string {
+   for _, sect := range m.Contents.SectionListRenderer.Contents {
+      if sect.ItemSectionRenderer != nil {
+         for _, item := range sect.ItemSectionRenderer.Contents {
+            if item.CompactVideoRenderer != nil {
+               return item.CompactVideoRenderer.VideoID
+            }
+         }
+      }
+   }
+   return ""
 }
 
 type mweb struct {
@@ -60,15 +108,4 @@ type mweb struct {
          }
       }
    }
-}
-
-func (m mweb) videoID() string {
-   for _, sect := range m.Contents.SectionListRenderer.Contents {
-      if sect.ItemSectionRenderer != nil {
-         for _, item := range sect.ItemSectionRenderer.Contents {
-            return item.CompactVideoRenderer.VideoID
-         }
-      }
-   }
-   return ""
 }
