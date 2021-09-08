999184cf9a09139aae225c6a2f4b90c1998e9c3a
Steven Penny <srpen6@gmail.com>
2021-09-08 13:01:55 -0500

    new file:   research/2021-09-08/login.go

---
+++ research/2021-09-08/login.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+   "bufio"
+   "fmt"
+   "github.com/refraction-networking/utls"
+   "net"
+   "net/http"
+   "net/http/httputil"
+   "net/url"
+   "os"
+   "strings"
+)
+
+func main() {
+   pass := os.Getenv("ENCRYPTEDPASS")
+   if pass == "" {
+      fmt.Println("missing pass")
+      return
+   }
+   val := url.Values{
+      "Email": {"srpen6@gmail.com"},
+      "sdk_version": {"17"},
+      "EncryptedPasswd": {pass},
+   }
+   req, err := http.NewRequest(
+      "POST", "https://android.clients.google.com/auth",
+      strings.NewReader(val.Encode()),
+   )
+   if err != nil {
+      panic(err)
+   }
+   req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+   dReq, err := httputil.DumpRequest(req, true)
+   if err != nil {
+      panic(err)
+   }
+   os.Stdout.Write(append(dReq, '\n'))
+   config := tls.Config{ServerName: req.URL.Host}
+   dialConn, err := net.Dial("tcp", req.URL.Host + ":" + req.URL.Scheme)
+   if err != nil {
+      panic(err)
+   }
+   uTlsConn := tls.UClient(dialConn, &config, tls.HelloCustom)
+   if err := uTlsConn.ApplyPreset(preset); err != nil {
+      panic(err)
+   }
+   if err := req.Write(uTlsConn); err != nil {
+      panic(err)
+   }
+   res, err := http.ReadResponse(bufio.NewReader(uTlsConn), req)
+   if err != nil {
+      panic(err)
+   }
+   defer res.Body.Close()
+   dRes, err := httputil.DumpResponse(res, true)
+   if err != nil {
+      panic(err)
+   }
+   os.Stdout.Write(dRes)
+}
