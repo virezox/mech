c29afefb0d709bd8492e4c458232f7f44c620ea1
Steven Penny <srpen6@gmail.com>
2021-09-16 14:33:32 -0500

    renamed:    research/2021-09-16/one.py -> research/2021-09-15/one.py

--- research/2021-09-16/two.go
+++ research/2021-09-16/two.go
@@ -1,61 +1,66 @@
-package bandcamp
+package main
+import "fmt"
+
+const body =
+   "client_id=134&" +
+   "client_secret=1myK12VeCL3dWl9o%2FncV2VyUUbOJuNPVJK6bZZJxHvk%3D&" +
+   "grant_type=password&password=PASSWORD&username=4095486538&" +
+   "username_is_user_id=1"
+
+func main() {
+   s := dm(body, 9)
+   fmt.Println(s)
+}
 
 func dm(sInput string, nInput int) string {
-   bInput := []byte(sInput)
+   cInput := []byte(sInput)
    if nInput % 2 != 0 {
-      v11 := 3
-      for v11 * v11 <= nInput {
-         if nInput % v11 != 0 {
-            v11 += 2
-         } else {
-            int v10 = 0
-         }
-         if v10 != 0 {
-            nInput++
+      n11 := 3
+      // FIXME
+      for n11 * n11 <= nInput {
+         if nInput % n11 != 0 {
+            n11 += 2
          }
       }
-      v10 = 1
    }
-   int v13 = new boolean[nInput + 1]
-   v13[0] = 1
-   v13[1] = 1
-   int v41 = -1
-   int v50 = -1
-   int v6 = 2
-   for v6 <= nInput {
-      if v13[v6] == 0 {
-         int v49 = v6 + v6
-         for v49 <= nInput {
-            v13[v49] = 1
-            v49 += v6
+   b13 := make([]bool, nInput+1)
+   b13[0] = true
+   b13[1] = true
+   n41 := -1
+   n50 := -1
+   for n6 := 2; n6 <= nInput; n6++ {
+      if b13[n6] == false {
+         n49 := n6 + n6
+         for n49 <= nInput {
+            b13[n49] = true
+            n49 += n6
          }
-         v41 = v50
-         v50 = v6
+         n41 = n50
+         n50 = n6
       }
-      v6++
    }
-   String v91 = 10 - (v41 % 10)
-   int v14 = 26 - (v50 % 26)
-   for v2 := 0; v2 < len(bInput); v2++ {
-      int v30 = bInput[v2]
-      if v30 < 97 || v30 > 122 {
-         if v30 < 65 || v30 > 90 {
-            if v30 >= 48 && v30 <= 57 {
-               v30 += v91
-               if v30 > 57 {
-                  v30 -= 10
+   n91 := 10 - (n41 % 10)
+   n14 := 26 - (n50 % 26)
+   for n2 := 0; n2 < len(cInput); n2++ {
+      c30 := cInput[n2]
+      if c30 < 97 || c30 > 122 {
+         if c30 < 65 || c30 > 90 {
+            if c30 >= 48 && c30 <= 57 {
+               c30 += byte(n91)
+               if c30 > 57 {
+                  c30 -= 10
                }
             }
          } else {
-            v30 += v14
-            if v30 > 90 {
-               v30 -= 26
+            c30 += byte(n14)
+            if c30 > 90 {
+               c30 -= 26
             }
          }
       } else {
-         v30 += v14
+         c30 += byte(n14)
       }
-      bInput[v2] = (char) v30
+      cInput[n2] = c30
    }
-   return string(bInput)
+   return string(cInput)
 }
--- research/2021-09-16/two.java
+++ research/2021-09-16/two.java
@@ -1,66 +1,149 @@
-    private static String a(String p8, int p9)
+package com.bandcamp.android.network;
+public class d implements java.util.Observer {
+    private static String e = "X";
+    private static String f = "aB";
+    private static String g = "pmac";
+    private static String h = "D";
+    private static String i = "M";
+    private static char[] j;
+    private byte[] a;
+    private int b;
+    private byte[] c;
+    private int d;
+
+    static d()
     {
-        char[] v8_1 = p8.toCharArray();
-        int v0 = v8_1.length;
-        int v2 = 0;
-        if ((p9 % 2) != 0) {
-            int v1_1 = 3;
-            while ((v1_1 * v1_1) <= p9) {
-                if ((p9 % v1_1) != 0) {
-                    v1_1 += 2;
-                } else {
-                    int v1_0 = 0;
-                }
-                if (v1_0 != 0) {
-                    p9++;
-                }
-            }
-            v1_0 = 1;
-        }
-        int v1_3 = new boolean[(p9 + 1)];
-        v1_3[0] = 1;
-        v1_3[1] = 1;
-        int v4_1 = -1;
-        int v5_0 = -1;
-        int v6 = 2;
-        while (v6 <= p9) {
-            if (v1_3[v6] == 0) {
-                int v4_9 = (v6 + v6);
-                while (v4_9 <= p9) {
-                    v1_3[v4_9] = 1;
-                    v4_9 += v6;
-                }
-                v4_1 = v5_0;
-                v5_0 = v6;
-            }
-            v6++;
-        }
-        String v9_1 = (10 - (v4_1 % 10));
-        int v1_4 = (26 - (v5_0 % 26));
-        while (v2 < v0) {
-            int v3_0;
-            v3_0 = v8_1[v2];
-            if ((v3_0 < 97) || (v3_0 > 122)) {
-                if ((v3_0 < 65) || (v3_0 > 90)) {
-                    if ((v3_0 >= 48) && (v3_0 <= 57)) {
-                        v3_0 += v9_1;
-                        if (v3_0 > 57) {
-                            v3_0 -= 10;
-                        }
-                    }
-                } else {
-                    v3_0 += v1_4;
-                    if (v3_0 > 90) {
-                        v3_0 -= 26;
-                    }
-                }
-            } else {
-                v3_0 += v1_4;
-                if (v3_0 > 122) {
-                }
-            }
-            v8_1[v2] = ((char) v3_0);
-            v2++;
-        }
-        return new String(v8_1);
+        char[] v0_1 = new char[5];
+        com.bandcamp.android.network.d.j = v0_1;
+        char[] v0_8 = new StringBuilder();
+        v0_8.append(com.bandcamp.android.network.d.g);
+        v0_8.append("d");
+        com.bandcamp.android.network.d.g = v0_8.toString();
+        char[] v0_4 = new StringBuilder();
+        v0_4.append("n");
+        v0_4.append(com.bandcamp.android.network.d.f);
+        com.bandcamp.android.network.d.f = v0_4.toString();
+        char[] v0_6 = com.bandcamp.android.network.d.j;
+        v0_6[0] = 100;
+        v0_6[1] = 116;
+        v0_6[2] = 109;
+        v0_6[3] = 102;
+        v0_6[4] = 97;
+        return;
     }
+
+    public d(com.bandcamp.android.network.CacheListener p2)
+    {
+        this.b = -1;
+        this.c = "".getBytes();
+        this.d = 0;
+        p2.getObservable().a(this);
+        return;
+    }
+
+    private String a(byte[] p2)
+    {
+        return this.a(this.a(), p2);
+    }
+
+    private byte[] a()
+    {
+        byte[] v0_0 = this.a;
+        if (v0_0 == null) {
+            v0_0 = new byte[0];
+        }
+        return v0_0;
+    }
+
+    public String a(byte[] p4, byte[] p5)
+    {
+        if (p5 != null) {
+            byte[] v1_6 = new byte[(p4.length + p5.length)];
+            System.arraycopy(p4, 0, v1_6, 0, p4.length);
+            System.arraycopy(p5, 0, v1_6, p4.length, p5.length);
+            p4 = v1_6;
+        }
+        String v5_1 = this.d;
+        if (v5_1 <= null) {
+            return com.bandcamp.shared.util.i.b(new String(p4), new String(com.bandcamp.android.network.d.j));
+        } else {
+            String v4_6;
+            if (v5_1 <= 30) {
+                v4_6 = com.bandcamp.shared.util.i.a(new String(p4), new String(this.c), 0);
+            } else {
+                v4_6 = com.bandcamp.shared.util.i.a(new String(p4), new String(this.c), 0, 0);
+            }
+            return v4_6;
+        }
+    }
+
+    public void a(com.bandcamp.shared.network.d p8)
+    {
+        try {
+            String v2_5 = com.bandcamp.android.network.d.f.toCharArray();
+            String v3_3 = 0;
+        } catch (java.io.IOException v8_1) {
+            Object[] v1_1 = new Object[0];
+            com.bandcamp.shared.util.BCLog.c.b(v8_1, v1_1);
+            return;
+        } catch (java.io.IOException v8_1) {
+        } catch (java.io.IOException v8_1) {
+        }
+        while (v3_3 < (v2_5.length / 2)) {
+            StringBuilder v4_3 = v2_5[v3_3];
+            v2_5[v3_3] = v2_5[((v2_5.length - v3_3) - 1)];
+            v2_5[((v2_5.length - v3_3) - 1)] = v4_3;
+            v3_3++;
+        }
+        String v3_0 = new String(v2_5);
+        String v2_1 = com.bandcamp.android.network.d.g.toCharArray();
+        StringBuilder v4_0 = 0;
+        while (v4_0 < (v2_1.length / 2)) {
+            String v5_3 = v2_1[v4_0];
+            v2_1[v4_0] = v2_1[((v2_1.length - v4_0) - 1)];
+            v2_1[((v2_1.length - v4_0) - 1)] = v5_3;
+            v4_0++;
+        }
+        StringBuilder v4_2 = new StringBuilder();
+        v4_2.append(com.bandcamp.android.network.d.e);
+        v4_2.append("-");
+        v4_2.append(v3_0);
+        v4_2.append(new String(v2_1));
+        v4_2.append("-");
+        v4_2.append(com.bandcamp.android.network.d.h);
+        v4_2.append(com.bandcamp.android.network.d.i);
+        p8.b(v4_2.toString(), this.a(p8.d()));
+        return;
+    }
+
+    public void update(java.util.Observable p4, Object p5)
+    {
+        if (!(p5 instanceof com.bandcamp.android.network.CacheListenerEvent)) {
+            if (((p5 instanceof String)) && ((this.b + 48) == ((String) p5).charAt(0))) {
+                try {
+                    this.a = p5.toString().substring(1).getBytes("utf-8");
+                } catch (java.io.UnsupportedEncodingException v4_4) {
+                    Object[] v0_1 = new Object[0];
+                    com.bandcamp.shared.util.BCLog.c.b(v4_4, v0_1);
+                }
+            }
+        } else {
+            java.io.UnsupportedEncodingException v4_8 = com.bandcamp.shared.util.e.a(((com.bandcamp.android.network.CacheListenerEvent) p5).value());
+            if (v4_8 != -1) {
+                this.b = v4_8;
+                if (v4_8 != 3) {
+                    if (v4_8 != 4) {
+                        this.d = 0;
+                    } else {
+                        this.c = com.bandcamp.shared.platform.e.a().a(((com.bandcamp.android.network.CacheListenerEvent) p5).value(), 0).getBytes("utf-8");
+                        this.d = 60;
+                    }
+                } else {
+                    this.c = com.bandcamp.shared.platform.e.a().b(((com.bandcamp.android.network.CacheListenerEvent) p5).value()).getBytes("utf-8");
+                    this.d = 30;
+                }
+            }
+        }
+        return;
+    }
+}
