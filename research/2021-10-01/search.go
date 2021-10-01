7e00ce5ab30e0a7d4a4376459894e57270d9016e
Steven Penny <srpen6@gmail.com>
2021-10-01 08:20:02 -0500

    deleted:    research/2021-10-01/profiles.go

--- research/2021-10-01/search.go
+++ research/2021-10-01/search.go
@@ -2,6 +2,8 @@ package goinsta
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 )
 
 // Search is the object for all searches like Facebook, Location or Tag search.
@@ -108,4 +110,77 @@ func (search *Search) history() (*[]SearchHistory, error) {
 		i.User.insta = search.insta
 	}
 	return &s.Recent, nil
+}
+
+
+// Profiles allows user function interactions
+type Profiles struct {
+	insta *Instagram
+}
+
+func newProfiles(insta *Instagram) *Profiles {
+	profiles := &Profiles{
+		insta: insta,
+	}
+	return profiles
+}
+
+// ByName return a *User structure parsed by username.
+//   not simply call this endpoint. It is better to use insta.Search(user),
+//   or insta.Searchbar.SearchUser(user).
+//
+func (prof *Profiles) ByName(name string) (*User, error) {
+	body, err := prof.insta.sendSimpleRequest(urlUserByName, name)
+	if err == nil {
+		resp := userResp{}
+		err = json.Unmarshal(body, &resp)
+		if err == nil {
+			user := &resp.User
+			user.insta = prof.insta
+			return user, err
+		}
+	}
+	return nil, err
+}
+
+// ByID returns a *User structure parsed by user id.
+func (prof *Profiles) ByID(id_ interface{}) (*User, error) {
+	var id string
+	switch x := id_.(type) {
+	case int64:
+		id = fmt.Sprintf("%d", x)
+	case int:
+		id = fmt.Sprintf("%d", x)
+	case string:
+		id = x
+	default:
+		return nil, errors.New("Invalid id, please provide a string or int(64)")
+	}
+
+	body, _, err := prof.insta.sendRequest(
+		&reqOptions{
+			Endpoint: fmt.Sprintf(urlUserByID, id),
+		},
+	)
+	if err == nil {
+		resp := userResp{}
+		err = json.Unmarshal(body, &resp)
+		if err == nil {
+			user := &resp.User
+			user.insta = prof.insta
+			return user, err
+		}
+	}
+	return nil, err
+}
+
+// Blocked returns a list of users you have blocked.
+func (prof *Profiles) Blocked() ([]BlockedUser, error) {
+	body, err := prof.insta.sendSimpleRequest(urlBlockedList)
+	if err == nil {
+		resp := blockedListResp{}
+		err = json.Unmarshal(body, &resp)
+		return resp.BlockedList, err
+	}
+	return nil, err
 }
