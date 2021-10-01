57444c9e9e0b9ba0cfe22de5fcfe369082d636ea
Steven Penny <srpen6@gmail.com>
2021-10-01 09:07:57 -0500

    deleted:    research/2021-10-01/collections.go

--- research/2021-10-01/location.go
+++ research/2021-10-01/location.go
@@ -2,6 +2,7 @@ package goinsta
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 )
 
@@ -166,3 +167,95 @@ func (c *Contacts) UnlinkContacts() error {
 	}
 	return nil
 }
+
+
+var ErrAllSaved = errors.New("Unable to call function for collection all posts")
+
+// MediaItem defines a item media for the
+// SavedMedia struct
+type MediaItem struct {
+	Media Item `json:"media"`
+}
+
+// Save saves media item.
+//
+// You can get saved media using Account.Saved()
+func (item *Item) Save() error {
+	return item.changeSave(urlMediaSave)
+}
+
+// Unsave unsaves media item.
+func (item *Item) Unsave() error {
+	return item.changeSave(urlMediaUnsave)
+}
+
+func (item *Item) changeSave(endpoint string) error {
+	insta := item.insta
+	query := map[string]string{
+		"module_name":     "feed_timeline",
+		"client_position": toString(item.Index),
+		"nav_chain":       "",
+		"_uid":            toString(insta.Account.ID),
+		"_uuid":           insta.uuid,
+		"radio_type":      "wifi-none",
+	}
+	if item.IsCommercial {
+		query["delivery_class"] = "ad"
+	} else {
+		query["delivery_class"] = "organic"
+	}
+	if item.InventorySource != "" {
+		query["inventory_source"] = item.InventorySource
+	}
+	if len(item.CarouselMedia) > 0 || item.CarouselParentID != "" {
+		query["carousel_index"] = "0"
+	}
+	data, err := json.Marshal(query)
+	if err != nil {
+		return err
+	}
+
+	_, _, err = insta.sendRequest(
+		&reqOptions{
+			Endpoint: fmt.Sprintf(endpoint, item.ID),
+			Query:    generateSignature(data),
+			IsPost:   true,
+		},
+	)
+	return err
+}
+
+type reelShare struct {
+	Text        string `json:"text"`
+	IsPersisted bool   `json:"is_reel_persisted"`
+	OwnderID    int64  `json:"reel_owner_id"`
+	Type        string `json:"type"`
+	ReelType    string `json:"reel_type"`
+	Media       Item   `json:"media"`
+}
+
+type actionLog struct {
+	Description string `json:"description"`
+}
+
+type Place struct {
+	Title    string    `json:"title"`
+	Subtitle string    `json:"subtitle"`
+	Location *Location `json:"location"`
+}
+
+type mediaItem struct {
+	Item *Item `json:"media"`
+}
+
+type (
+	fetchReason string
+)
+
+var (
+	PULLTOREFRESH fetchReason = "pull_to_refresh"
+	COLDSTART     fetchReason = "cold_start_fetch"
+	WARMSTART     fetchReason = "warm_start_fetch"
+	PAGINATION    fetchReason = "pagination"
+	AUTOREFRESH   fetchReason = "auto_refresh" // so far unused
+)
