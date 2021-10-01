ffb7592e47b6f3b3f38f236f5f7a9e5fbb093b63
Steven Penny <srpen6@gmail.com>
2021-10-01 10:27:42 -0500

    deleted:    research/2021-10-01/const.go

--- research/2021-10-01/account.go
+++ research/2021-10-01/account.go
@@ -74,11 +74,6 @@ type Account struct {
 	CanBoostPost               bool         `json:"can_boost_post"`
 }
 
-type profResp struct {
-	Status  string  `json:"status"`
-	Account Account `json:"user"`
-}
-
 var ErrAllSaved = errors.New("Unable to call function for collection all posts")
 
 type (
@@ -120,3 +115,94 @@ type Device struct {
    ScreenDpi        string `json:"screen_dpi"`
    ScreenResolution string `json:"screen_resolution"`
 }
+
+
+const (
+	// urls
+	baseUrl        = "https://i.instagram.com/"
+	instaAPIUrl    = "https://i.instagram.com/api/v1/"
+	instaAPIUrlb   = "https://b.i.instagram.com/api/v1/"
+	instaAPIUrlv2  = "https://i.instagram.com/api/v2/"
+	instaAPIUrlv2b = "https://b.i.instagram.com/api/v2/"
+	// header values
+	bloksVerID         = "927f06374b80864ae6a0b04757048065714dc50ff15d2b8b3de8d0b6de961649"
+	fbAnalytics        = "567067343352427"
+	igCapabilities     = "3brTvx0="
+	connType           = "WIFI"
+	instaSigKeyVersion = "4"
+	locale             = "en_US"
+	appVersion         = "195.0.0.31.123"
+	appVersionCode     = "302733750"
+)
+
+var (
+	defaultHeaderOptions = map[string]string{
+		"X-Ig-Www-Claim": "0",
+	}
+	omitAPIHeadersExclude = []string{
+		"X-Ig-Bandwidth-Speed-Kbps",
+		"Ig-U-Shbts",
+		"X-Ig-Mapped-Locale",
+		"X-Ig-Family-Device-Id",
+		"X-Ig-Android-Id",
+		"X-Ig-Timezone-Offset",
+		"X-Ig-Device-Locale",
+		"X-Ig-Device-Id",
+		"Ig-Intended-User-Id",
+		"X-Ig-App-Locale",
+		"X-Bloks-Is-Layout-Rtl",
+		"X-Pigeon-Rawclienttime",
+		"X-Bloks-Version-Id",
+		"X-Ig-Bandwidth-Totalbytes-B",
+		"X-Ig-Bandwidth-Totaltime-Ms",
+		"X-Ig-App-Startup-Country",
+		"X-Ig-Www-Claim",
+		"X-Bloks-Is-Panorama-Enabled",
+	}
+	// Default Device
+	GalaxyS10 = Device{
+		Manufacturer:     "samsung",
+		Model:            "SM-G975F",
+		CodeName:         "beyond2",
+		AndroidVersion:   30,
+		AndroidRelease:   11,
+		ScreenDpi:        "560dpi",
+		ScreenResolution: "1440x2898",
+		Chipset:          "exynos9820",
+	}
+	timeOffset = getTimeOffset()
+)
+
+// Endpoints (with format vars)
+const (
+	// Login
+	urlMsisdnHeader               = "accounts/read_msisdn_header/"
+	urlGetPrefill                 = "accounts/get_prefill_candidates/"
+	urlContactPrefill             = "accounts/contact_point_prefill/"
+	urlGetAccFamily               = "multiple_accounts/get_account_family/"
+	urlZrToken                    = "zr/token/result/"
+	urlLogin                      = "accounts/login/"
+	urlLogout                     = "accounts/logout/"
+	urlAutoComplete               = "friendships/autocomplete_user_list/"
+	urlQeSync                     = "qe/sync/"
+	urlSync                       = "launcher/sync/"
+	urlLogAttribution             = "attribution/log_attribution/"
+	urlMegaphoneLog               = "megaphone/log/"
+	urlExpose                     = "qe/expose/"
+	urlGetNdxSteps                = "devices/ndx/api/async_get_ndx_ig_steps/"
+	urlBanyan                     = "banyan/banyan/"
+	urlCooldowns                  = "qp/get_cooldowns/"
+	urlFetchConfig                = "loom/fetch_config/"
+	urlBootstrapUserScores        = "scores/bootstrap/users/"
+	urlStoreClientPushPermissions = "notifications/store_client_push_permissions/"
+	urlProcessContactPointSignals = "accounts/process_contact_point_signals/"
+)
+
+// Account & Login Errors
+var ErrBadPassword     = errors.New("Password is incorrect")
+
+var ErrInstaNotDefined = errors.New(
+   "Insta has not been defined, this is most likely a bug in the code. Please " +
+   "backtrack which call this error came from, and open an issue detailing " +
+   "exactly how you got to this error.",
+)
