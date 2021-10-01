a7432dc74cb9c1fb797be86c5e9ba1a74eb9149d
Steven Penny <srpen6@gmail.com>
2021-10-01 10:54:26 -0500

    +	urlMsisdnHeader               = "accounts/read_msisdn_header/"

--- research/2021-10-01/account.go
+++ research/2021-10-01/account.go
@@ -16,115 +16,29 @@ import (
    "time"
 )
 
-type accountResp struct {
-	Status  string  `json:"status"`
-	Account Account `json:"logged_in_user"`
-
-	ErrorType         string         `json:"error_type"`
-	Message           string         `json:"message"`
-	TwoFactorRequired bool           `json:"two_factor_required"`
-}
-
-// Account is personal account object
-//
-// See examples: examples/account/*
-type Account struct {
-	insta *Instagram
-	ID                         int64        `json:"pk"`
-	Username                   string       `json:"username"`
-	FullName                   string       `json:"full_name"`
-	Biography                  string       `json:"biography"`
-	ProfilePicURL              string       `json:"profile_pic_url"`
-	Email                      string       `json:"email"`
-	PhoneNumber                string       `json:"phone_number"`
-	IsBusiness                 bool         `json:"is_business"`
-	Gender                     int          `json:"gender"`
-	ProfilePicID               string       `json:"profile_pic_id"`
-	CanSeeOrganicInsights      bool         `json:"can_see_organic_insights"`
-	ShowInsightsTerms          bool         `json:"show_insights_terms"`
-	HasAnonymousProfilePicture bool         `json:"has_anonymous_profile_picture"`
-	IsPrivate                  bool         `json:"is_private"`
-	IsUnpublished              bool         `json:"is_unpublished"`
-	AllowedCommenterType       string       `json:"allowed_commenter_type"`
-	IsVerified                 bool         `json:"is_verified"`
-	MediaCount                 int          `json:"media_count"`
-	FollowerCount              int          `json:"follower_count"`
-	FollowingCount             int          `json:"following_count"`
-	GeoMediaCount              int          `json:"geo_media_count"`
-	ExternalURL                string       `json:"external_url"`
-	HasBiographyTranslation    bool         `json:"has_biography_translation"`
-	ExternalLynxURL            string       `json:"external_lynx_url"`
-	UsertagsCount              int          `json:"usertags_count"`
-	HasChaining                bool         `json:"has_chaining"`
-	ReelAutoArchive            string       `json:"reel_auto_archive"`
-	PublicEmail                string       `json:"public_email"`
-	PublicPhoneNumber          string       `json:"public_phone_number"`
-	PublicPhoneCountryCode     string       `json:"public_phone_country_code"`
-	ContactPhoneNumber         string       `json:"contact_phone_number"`
-	Byline                     string       `json:"byline"`
-	SocialContext              string       `json:"social_context,omitempty"`
-	SearchSocialContext        string       `json:"search_social_context,omitempty"`
-	MutualFollowersCount       float64      `json:"mutual_followers_count"`
-	LatestReelMedia            int64        `json:"latest_reel_media,omitempty"`
-	CityID                     int64        `json:"city_id"`
-	CityName                   string       `json:"city_name"`
-	AddressStreet              string       `json:"address_street"`
-	DirectMessaging            string       `json:"direct_messaging"`
-	Latitude                   float64      `json:"latitude"`
-	Longitude                  float64      `json:"longitude"`
-	Category                   string       `json:"category"`
-	BusinessContactMethod      string       `json:"business_contact_method"`
-	IsCallToActionEnabled      bool         `json:"is_call_to_action_enabled"`
-	FbPageCallToActionID       string       `json:"fb_page_call_to_action_id"`
-	Zip                        string       `json:"zip"`
-	AllowContactsSync          bool         `json:"allow_contacts_sync"`
-	CanBoostPost               bool         `json:"can_boost_post"`
-}
-
-var ErrAllSaved = errors.New("Unable to call function for collection all posts")
-
-type (
-	fetchReason string
-)
-
-var (
-	PULLTOREFRESH fetchReason = "pull_to_refresh"
-	COLDSTART     fetchReason = "cold_start_fetch"
-	WARMSTART     fetchReason = "warm_start_fetch"
-	PAGINATION    fetchReason = "pagination"
-	AUTOREFRESH   fetchReason = "auto_refresh" // so far unused
-)
-
-// ConfigFile is a structure to store the session information so that can be exported or imported.
-type ConfigFile struct {
-	ID            int64             `json:"id"`
-	User          string            `json:"username"`
-	DeviceID      string            `json:"device_id"`
-	FamilyID      string            `json:"family_id"`
-	UUID          string            `json:"uuid"`
-	RankToken     string            `json:"rank_token"`
-	Token         string            `json:"token"`
-	PhoneID       string            `json:"phone_id"`
-	XmidExpiry    int64             `json:"xmid_expiry"`
-	HeaderOptions map[string]string `json:"header_options"`
-	Cookies       []*http.Cookie    `json:"cookies"`
-	Account       *Account          `json:"account"`
-	Device        Device            `json:"device"`
-}
-
-type Device struct {
-   AndroidRelease   int    `json:"android_release"`
-   AndroidVersion   int    `json:"android_version"`
-   Chipset          string `json:"chipset"`
-   CodeName         string `json:"code_name"`
-   Manufacturer     string `json:"manufacturer"`
-   Model            string `json:"model"`
-   ScreenDpi        string `json:"screen_dpi"`
-   ScreenResolution string `json:"screen_resolution"`
-}
-
-
+// Endpoints (with format vars)
 const (
+	// Login
+	urlMsisdnHeader               = "accounts/read_msisdn_header/"
+	urlGetPrefill                 = "accounts/get_prefill_candidates/"
+	urlContactPrefill             = "accounts/contact_point_prefill/"
+	urlGetAccFamily               = "multiple_accounts/get_account_family/"
+	urlZrToken                    = "zr/token/result/"
+	urlLogin                      = "accounts/login/"
+	urlLogout                     = "accounts/logout/"
+	urlAutoComplete               = "friendships/autocomplete_user_list/"
+	urlQeSync                     = "qe/sync/"
+	urlSync                       = "launcher/sync/"
+	urlLogAttribution             = "attribution/log_attribution/"
+	urlMegaphoneLog               = "megaphone/log/"
+	urlExpose                     = "qe/expose/"
+	urlGetNdxSteps                = "devices/ndx/api/async_get_ndx_ig_steps/"
+	urlBanyan                     = "banyan/banyan/"
+	urlCooldowns                  = "qp/get_cooldowns/"
+	urlFetchConfig                = "loom/fetch_config/"
+	urlBootstrapUserScores        = "scores/bootstrap/users/"
+	urlStoreClientPushPermissions = "notifications/store_client_push_permissions/"
+	urlProcessContactPointSignals = "accounts/process_contact_point_signals/"
 	// urls
 	baseUrl        = "https://i.instagram.com/"
 	instaAPIUrl    = "https://i.instagram.com/api/v1/"
@@ -141,6 +55,17 @@ const (
 	appVersion         = "195.0.0.31.123"
 	appVersionCode     = "302733750"
 )
+
+// Account & Login Errors
+var ErrBadPassword     = errors.New("Password is incorrect")
+
+var ErrInstaNotDefined = errors.New(
+   "Insta has not been defined, this is most likely a bug in the code. Please " +
+   "backtrack which call this error came from, and open an issue detailing " +
+   "exactly how you got to this error.",
+)
+
+var ErrAllSaved = errors.New("Unable to call function for collection all posts")
 
 var (
 	defaultHeaderOptions = map[string]string{
@@ -179,57 +104,6 @@ var (
 	}
 	timeOffset = getTimeOffset()
 )
-
-// Endpoints (with format vars)
-const (
-	// Login
-	urlMsisdnHeader               = "accounts/read_msisdn_header/"
-	urlGetPrefill                 = "accounts/get_prefill_candidates/"
-	urlContactPrefill             = "accounts/contact_point_prefill/"
-	urlGetAccFamily               = "multiple_accounts/get_account_family/"
-	urlZrToken                    = "zr/token/result/"
-	urlLogin                      = "accounts/login/"
-	urlLogout                     = "accounts/logout/"
-	urlAutoComplete               = "friendships/autocomplete_user_list/"
-	urlQeSync                     = "qe/sync/"
-	urlSync                       = "launcher/sync/"
-	urlLogAttribution             = "attribution/log_attribution/"
-	urlMegaphoneLog               = "megaphone/log/"
-	urlExpose                     = "qe/expose/"
-	urlGetNdxSteps                = "devices/ndx/api/async_get_ndx_ig_steps/"
-	urlBanyan                     = "banyan/banyan/"
-	urlCooldowns                  = "qp/get_cooldowns/"
-	urlFetchConfig                = "loom/fetch_config/"
-	urlBootstrapUserScores        = "scores/bootstrap/users/"
-	urlStoreClientPushPermissions = "notifications/store_client_push_permissions/"
-	urlProcessContactPointSignals = "accounts/process_contact_point_signals/"
-)
-
-// Account & Login Errors
-var ErrBadPassword     = errors.New("Password is incorrect")
-
-var ErrInstaNotDefined = errors.New(
-   "Insta has not been defined, this is most likely a bug in the code. Please " +
-   "backtrack which call this error came from, and open an issue detailing " +
-   "exactly how you got to this error.",
-)
-
-
-func RSADecodePublicKeyFromBase64(pubKeyBase64 string) (*rsa.PublicKey, error) {
-	pubKey, err := base64.StdEncoding.DecodeString(pubKeyBase64)
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-
-	block, _ := pem.Decode(pubKey)
-	pKey, err := x509.ParsePKIXPublicKey(block.Bytes)
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-	return pKey.(*rsa.PublicKey), nil
-}
 
 func AESGCMEncrypt(key, data, additionalData []byte) (iv, encrypted, tag []byte, err error) {
 	iv = make([]byte, 12)
@@ -292,4 +166,61 @@ func EncryptPassword(password, pubKeyEncoded string, pubKeyVersion int, t string
 	}
 	encoded := base64.StdEncoding.EncodeToString(s)
 	return fmt.Sprintf("#PWD_INSTAGRAM:4:%s:%s", t, encoded), nil
+}
+
+func RSADecodePublicKeyFromBase64(pubKeyBase64 string) (*rsa.PublicKey, error) {
+	pubKey, err := base64.StdEncoding.DecodeString(pubKeyBase64)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	block, _ := pem.Decode(pubKey)
+	pKey, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	return pKey.(*rsa.PublicKey), nil
+}
+
+type Account struct {
+   ID                         int64        `json:"pk"`
+   insta *Instagram
+}
+
+// ConfigFile is a structure to store the session information so that can be exported or imported.
+type ConfigFile struct {
+	ID            int64             `json:"id"`
+	User          string            `json:"username"`
+	DeviceID      string            `json:"device_id"`
+	FamilyID      string            `json:"family_id"`
+	UUID          string            `json:"uuid"`
+	RankToken     string            `json:"rank_token"`
+	Token         string            `json:"token"`
+	PhoneID       string            `json:"phone_id"`
+	XmidExpiry    int64             `json:"xmid_expiry"`
+	HeaderOptions map[string]string `json:"header_options"`
+	Cookies       []*http.Cookie    `json:"cookies"`
+	Account       *Account          `json:"account"`
+	Device        Device            `json:"device"`
+}
+
+type Device struct {
+   AndroidRelease   int    `json:"android_release"`
+   AndroidVersion   int    `json:"android_version"`
+   Chipset          string `json:"chipset"`
+   CodeName         string `json:"code_name"`
+   Manufacturer     string `json:"manufacturer"`
+   Model            string `json:"model"`
+   ScreenDpi        string `json:"screen_dpi"`
+   ScreenResolution string `json:"screen_resolution"`
+}
+
+type accountResp struct {
+   Account Account `json:"logged_in_user"`
+   ErrorType         string         `json:"error_type"`
+   Message           string         `json:"message"`
+   Status  string  `json:"status"`
+   TwoFactorRequired bool           `json:"two_factor_required"`
 }
