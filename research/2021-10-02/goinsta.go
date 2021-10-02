f775a903071cef9b306f35747102f3444f89f564
Steven Penny <srpen6@gmail.com>
2021-10-02 15:56:16 -0500

    +   appVersion = "195.0.0.31.123"

--- research/2021-10-02/goinsta.go
+++ research/2021-10-02/goinsta.go
@@ -9,8 +9,73 @@ import (
    "time"
 )
 
+// Endpoints (with format vars)
+const (
+   appVersion = "195.0.0.31.123"
+   appVersionCode = "302733750"
+   baseUrl        = "https://i.instagram.com/"
+   bloksVerID = "927f06374b80864ae6a0b04757048065714dc50ff15d2b8b3de8d0b6de961649"
+   connType = "WIFI"
+   fbAnalytics = "567067343352427"
+   igCapabilities = "3brTvx0="
+   instaAPIUrl    = "https://i.instagram.com/api/v1/"
+   instaAPIUrlb   = "https://b.i.instagram.com/api/v1/"
+   instaAPIUrlv2  = "https://i.instagram.com/api/v2/"
+   instaAPIUrlv2b = "https://b.i.instagram.com/api/v2/"
+   locale = "en_US"
+   urlLogin                      = "accounts/login/"
+)
+
+var defaultHeaderOptions = map[string]string{"X-Ig-Www-Claim": "0"}
+
+var omitAPIHeadersExclude = []string{
+   "Ig-Intended-User-Id",
+   "Ig-U-Shbts",
+   "X-Bloks-Is-Layout-Rtl",
+   "X-Bloks-Is-Panorama-Enabled",
+   "X-Bloks-Version-Id",
+   "X-Ig-Android-Id",
+   "X-Ig-App-Locale",
+   "X-Ig-App-Startup-Country",
+   "X-Ig-Bandwidth-Speed-Kbps",
+   "X-Ig-Bandwidth-Totalbytes-B",
+   "X-Ig-Bandwidth-Totaltime-Ms",
+   "X-Ig-Device-Id",
+   "X-Ig-Device-Locale",
+   "X-Ig-Family-Device-Id",
+   "X-Ig-Mapped-Locale",
+   "X-Ig-Timezone-Offset",
+   "X-Ig-Www-Claim",
+   "X-Pigeon-Rawclienttime",
+}
+
 func defaultHandler(args ...interface{}) {
    fmt.Println(args...)
+}
+
+// ConfigFile is a structure to store the session information so that can be
+// exported or imported.
+type ConfigFile struct {
+   DeviceID      string            `json:"device_id"`
+   FamilyID      string            `json:"family_id"`
+   HeaderOptions map[string]string `json:"header_options"`
+   ID            int64             `json:"id"`
+   PhoneID       string            `json:"phone_id"`
+   Token         string            `json:"token"`
+   UUID          string            `json:"uuid"`
+   User          string            `json:"username"`
+   XmidExpiry    int64             `json:"xmid_expiry"`
+}
+
+type Device struct {
+   AndroidRelease   int    `json:"android_release"`
+   AndroidVersion   int    `json:"android_version"`
+   Chipset          string `json:"chipset"`
+   CodeName         string `json:"code_name"`
+   Manufacturer     string `json:"manufacturer"`
+   Model            string `json:"model"`
+   ScreenDpi        string `json:"screen_dpi"`
+   ScreenResolution string `json:"screen_resolution"`
 }
 
 // Instagram represent the main API handler
@@ -51,17 +116,6 @@ type Instagram struct {
    infoHandler  func(...interface{})
    warnHandler  func(...interface{})
    debugHandler func(...interface{})
-}
-
-type Device struct {
-   AndroidRelease   int    `json:"android_release"`
-   AndroidVersion   int    `json:"android_version"`
-   Chipset          string `json:"chipset"`
-   CodeName         string `json:"code_name"`
-   Manufacturer     string `json:"manufacturer"`
-   Model            string `json:"model"`
-   ScreenDpi        string `json:"screen_dpi"`
-   ScreenResolution string `json:"screen_resolution"`
 }
 
 // New creates Instagram structure
@@ -184,58 +238,4 @@ func (insta *Instagram) Login() error {
       return fmt.Errorf("%+v", res)
    }
    return nil
-}
-
-// Endpoints (with format vars)
-const (
-   appVersion = "195.0.0.31.123"
-   appVersionCode = "302733750"
-   baseUrl        = "https://i.instagram.com/"
-   bloksVerID = "927f06374b80864ae6a0b04757048065714dc50ff15d2b8b3de8d0b6de961649"
-   connType = "WIFI"
-   fbAnalytics = "567067343352427"
-   igCapabilities = "3brTvx0="
-   instaAPIUrl    = "https://i.instagram.com/api/v1/"
-   instaAPIUrlb   = "https://b.i.instagram.com/api/v1/"
-   instaAPIUrlv2  = "https://i.instagram.com/api/v2/"
-   instaAPIUrlv2b = "https://b.i.instagram.com/api/v2/"
-   locale = "en_US"
-   urlLogin                      = "accounts/login/"
-)
-
-var defaultHeaderOptions = map[string]string{"X-Ig-Www-Claim": "0"}
-
-var omitAPIHeadersExclude = []string{
-   "Ig-Intended-User-Id",
-   "Ig-U-Shbts",
-   "X-Bloks-Is-Layout-Rtl",
-   "X-Bloks-Is-Panorama-Enabled",
-   "X-Bloks-Version-Id",
-   "X-Ig-Android-Id",
-   "X-Ig-App-Locale",
-   "X-Ig-App-Startup-Country",
-   "X-Ig-Bandwidth-Speed-Kbps",
-   "X-Ig-Bandwidth-Totalbytes-B",
-   "X-Ig-Bandwidth-Totaltime-Ms",
-   "X-Ig-Device-Id",
-   "X-Ig-Device-Locale",
-   "X-Ig-Family-Device-Id",
-   "X-Ig-Mapped-Locale",
-   "X-Ig-Timezone-Offset",
-   "X-Ig-Www-Claim",
-   "X-Pigeon-Rawclienttime",
-}
-
-// ConfigFile is a structure to store the session information so that can be
-// exported or imported.
-type ConfigFile struct {
-   DeviceID      string            `json:"device_id"`
-   FamilyID      string            `json:"family_id"`
-   HeaderOptions map[string]string `json:"header_options"`
-   ID            int64             `json:"id"`
-   PhoneID       string            `json:"phone_id"`
-   Token         string            `json:"token"`
-   UUID          string            `json:"uuid"`
-   User          string            `json:"username"`
-   XmidExpiry    int64             `json:"xmid_expiry"`
 }
