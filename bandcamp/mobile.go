fdbaf46c857f36e639818e79fee65d48d4dc6651
Steven Penny <srpen6@gmail.com>
2021-10-05 15:04:38 -0500

    +func (a *Album) PostForm(id string) error {

--- bandcamp/mobile.go
+++ bandcamp/mobile.go
@@ -62,13 +62,30 @@ func (a *Album) Post(id string) error {
    return json.NewDecoder(res.Body).Decode(a)
 }
 
-type Discography struct {
-   Discography []struct {
-      URL string
+func (a *Album) PostForm(id string) error {
+   val := url.Values{
+      "band_id": {"1"}, "tralbum_id": {id}, "tralbum_type": {"a"},
    }
+   req, err := http.NewRequest(
+      "POST", Origin + "/api/mobile/24/tralbum_details",
+      strings.NewReader(val.Encode()),
+   )
+   if err != nil {
+      return err
+   }
+   res, err := mech.RoundTrip(req)
+   if err != nil {
+      return err
+   }
+   defer res.Body.Close()
+   return json.NewDecoder(res.Body).Decode(a)
 }
 
-func (d *Discography) Get(id string) error {
+type Band struct {
+   Bandcamp_URL string
+}
+
+func (b *Band) Get(id string) error {
    req, err := http.NewRequest(
       "GET", Origin + "/api/mobile/24/band_details", nil,
    )
@@ -83,10 +100,10 @@ func (d *Discography) Get(id string) error {
       return err
    }
    defer res.Body.Close()
-   return json.NewDecoder(res.Body).Decode(d)
+   return json.NewDecoder(res.Body).Decode(b)
 }
 
-func (d *Discography) Post(id string) error {
+func (b *Band) Post(id string) error {
    body := map[string]string{"band_id": id}
    buf := new(bytes.Buffer)
    if err := json.NewEncoder(buf).Encode(body); err != nil {
@@ -103,7 +120,26 @@ func (d *Discography) Post(id string) error {
       return err
    }
    defer res.Body.Close()
-   return json.NewDecoder(res.Body).Decode(d)
+   return json.NewDecoder(res.Body).Decode(b)
+}
+
+func (b *Band) PostForm(id string) error {
+   val := url.Values{
+      "band_id": {id},
+   }
+   req, err := http.NewRequest(
+      "POST", Origin + "/api/mobile/24/band_details",
+      strings.NewReader(val.Encode()),
+   )
+   if err != nil {
+      return err
+   }
+   res, err := mech.RoundTrip(req)
+   if err != nil {
+      return err
+   }
+   defer res.Body.Close()
+   return json.NewDecoder(res.Body).Decode(b)
 }
 
 type Track struct {
@@ -122,6 +158,30 @@ func (t *Track) Get(id string) error {
    val.Set("tralbum_id", id)
    val.Set("tralbum_type", "t")
    req.URL.RawQuery = val.Encode()
+   res, err := mech.RoundTrip(req)
+   if err != nil {
+      return err
+   }
+   defer res.Body.Close()
+   return json.NewDecoder(res.Body).Decode(t)
+}
+
+func (t *Track) Post(id string) error {
+   body := map[string]string{
+      "band_id": "1",
+      "tralbum_id": id,
+      "tralbum_type": "t",
+   }
+   buf := new(bytes.Buffer)
+   if err := json.NewEncoder(buf).Encode(body); err != nil {
+      return err
+   }
+   req, err := http.NewRequest(
+      "POST", Origin + "/api/mobile/24/tralbum_details", buf,
+   )
+   if err != nil {
+      return err
+   }
    res, err := mech.RoundTrip(req)
    if err != nil {
       return err
