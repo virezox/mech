961cef0417d768bc57d4b75359d240190348b6ce
Steven Penny <srpen6@gmail.com>
2021-10-06 13:46:46 -0500

    deleted:    bandcamp/type.go

--- bandcamp/bandcamp.go
+++ bandcamp/bandcamp.go
@@ -1,7 +1,6 @@
 package bandcamp
 
 import (
-   "bytes"
    "encoding/json"
    "fmt"
    "github.com/89z/mech"
@@ -10,17 +9,89 @@ import (
    "strconv"
 )
 
+const (
+   ApiMobile = "http://bandcamp.com/api/mobile/24/tralbum_details"
+   ApiUrl = "http://bandcamp.com/api/url/2/info"
+)
+
+// thrjozkaskhjastaurrtygitylpt
+// throtaudvinroftignmarkreina
+// ullrettkalladrhampa
+const key = "veidihundr"
+
 var Verbose = mech.Verbose
 
-func (d Detail) Tralbum() (*Tralbum, error) {
-   buf := new(bytes.Buffer)
-   if err := json.NewEncoder(buf).Encode(d); err != nil {
-      return nil, err
+// URL to track_id or album_id, anonymous
+func Head(addr string) (byte, int, error) {
+   req, err := http.NewRequest("HEAD", addr, nil)
+   if err != nil {
+      return 0, 0, err
    }
-   req, err := http.NewRequest("POST", ApiMobile, buf)
+   res, err := mech.RoundTrip(req)
+   if err != nil {
+      return 0, 0, err
+   }
+   reg := regexp.MustCompile(`nilZ0([at])(\d+)x`)
+   for _, c := range res.Cookies() {
+      if c.Name == "session" {
+         // [nilZ0t2809477874x t 2809477874]
+         find := reg.FindStringSubmatch(c.Value)
+         if find != nil {
+            id, err := strconv.Atoi(find[2])
+            if err == nil {
+               return find[1][0], id, nil
+            }
+         }
+      }
+   }
+   return 0, 0, fmt.Errorf("cookies %v", res.Cookies())
+}
+
+type Info struct {
+   Album_ID int
+   Band_ID int
+   Track_ID int
+}
+
+// URL to track_id, album_id or band_id, key
+func NewInfo(addr string) (*Info, error) {
+   req, err := http.NewRequest("GET", ApiUrl, nil)
    if err != nil {
       return nil, err
    }
+   val := req.URL.Query()
+   val.Set("key", key)
+   val.Set("url", addr)
+   req.URL.RawQuery = val.Encode()
+   res, err := mech.RoundTrip(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   inf := new(Info)
+   if err := json.NewDecoder(res.Body).Decode(inf); err != nil {
+      return nil, err
+   }
+   return inf, nil
+}
+
+type Tralbum struct {
+   Bandcamp_URL string
+   Tracks []struct {
+      Title string
+   }
+}
+
+func NewTralbum(typ byte, id int) (*Tralbum, error) {
+   req, err := http.NewRequest("GET", ApiMobile, nil)
+   if err != nil {
+      return nil, err
+   }
+   val := req.URL.Query()
+   val.Set("band_id", "1")
+   val.Set("tralbum_type", string(typ))
+   val.Set("tralbum_id", strconv.Itoa(id))
+   req.URL.RawQuery = val.Encode()
    res, err := mech.RoundTrip(req)
    if err != nil {
       return nil, err
@@ -31,36 +102,4 @@ func (d Detail) Tralbum() (*Tralbum, error) {
       return nil, err
    }
    return tra, nil
-}
-
-// URL to track_id or album_id, anonymous
-func (d *Detail) Head(addr string) error {
-   req, err := http.NewRequest("HEAD", addr, nil)
-   if err != nil {
-      return err
-   }
-   res, err := mech.RoundTrip(req)
-   if err != nil {
-      return err
-   }
-   reg := regexp.MustCompile(`nilZ0([at])(\d+)x`)
-   for _, c := range res.Cookies() {
-      if c.Name != "session" {
-         continue
-      }
-      find := reg.FindStringSubmatch(c.Value)
-      if find == nil {
-         continue
-      }
-      // [nilZ0t2809477874x t 2809477874]
-      id, err := strconv.Atoi(find[2])
-      if err != nil {
-         continue
-      }
-      d.Band_ID = 1
-      d.Tralbum_ID = id
-      d.Tralbum_Type = find[1]
-      return nil
-   }
-   return fmt.Errorf("cookies %v", res.Cookies())
 }
