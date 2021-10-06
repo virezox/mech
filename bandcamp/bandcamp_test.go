cf6593eaae3196e9601d9a74fbe68d9f0d83f0f5
Steven Penny <srpen6@gmail.com>
2021-10-06 14:57:23 -0500

    +   ApiAlbum = "http://bandcamp.com/api/album/2/info"

--- bandcamp/bandcamp_test.go
+++ bandcamp/bandcamp_test.go
@@ -5,58 +5,47 @@ import (
    "net/http"
    "os"
    "testing"
-   "time"
 )
 
-type test struct {
-   in string
-   typ string
-   id int
-}
+const (
+   addr = "https://schnaussandmunk.bandcamp.com/track/amaris-2"
+   id = 2809477874
+)
 
-var tests = []test{
-   {"https://schnaussandmunk.bandcamp.com/album/passage-2", "a", 1670971920},
-   {"https://schnaussandmunk.bandcamp.com/track/amaris-2", "t", 2809477874},
-}
-
-var details = []Detail{
-   {1, 79940049, "a"},
-   {1, 2809477874, "t"},
-}
-
-func TestDetail(t *testing.T) {
+func TestTrack(t *testing.T) {
    Verbose(true)
-   for _, test := range tests {
-      d, err := TralbumDetail(test.in)
-      if err != nil {
-         t.Fatal(err)
-      }
-      if d.Tralbum_Type != test.typ {
-         t.Fatal(d.Tralbum_Type)
-      }
-      if d.Tralbum_ID != test.id {
-         t.Fatal(d.Tralbum_ID)
-      }
-      time.Sleep(99 * time.Millisecond)
+   inf, err := NewInfo(addr)
+   if err != nil {
+      t.Fatal(err)
    }
+   tra, err := NewTrack(inf.Track_ID)
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Printf("%+v\n", tra)
 }
 
-func TestTralbum(t *testing.T) {
-   Verbose(true)
-   for _, detail := range details {
-      tra, err := detail.Tralbum()
-      if err != nil {
-         t.Fatal(err)
-      }
-      fmt.Printf("%+v\n", tra)
+// 0.095s
+func TestInfo(t *testing.T) {
+   req, err := http.NewRequest("GET", ApiUrl, nil)
+   if err != nil {
+      t.Fatal(err)
    }
+   q := req.URL.Query()
+   q.Set("key", key)
+   q.Set("url", addr)
+   req.URL.RawQuery = q.Encode()
+   res, err := new(http.Transport).RoundTrip(req)
+   if err != nil {
+      t.Fatal(err)
+   }
+   defer res.Body.Close()
+   os.Stdout.ReadFrom(res.Body)
 }
 
-// 0.802s
-func TestNew(t *testing.T) {
-   req, err := http.NewRequest(
-      "HEAD", "https://schnaussandmunk.bandcamp.com", nil,
-   )
+// 0.405s
+func TestHead(t *testing.T) {
+   req, err := http.NewRequest("HEAD", addr, nil)
    if err != nil {
       t.Fatal(err)
    }
@@ -65,22 +54,4 @@ func TestNew(t *testing.T) {
       t.Fatal(err)
    }
    fmt.Println(res.Cookies())
-}
-
-// 0.078s
-func TestOld(t *testing.T) {
-   req, err := http.NewRequest("GET", "http://bandcamp.com/api/url/2/info", nil)
-   if err != nil {
-      t.Fatal(err)
-   }
-   q := req.URL.Query()
-   q.Set("key", "veidihundr")
-   q.Set("url", "schnaussandmunk.bandcamp.com")
-   req.URL.RawQuery = q.Encode()
-   res, err := new(http.Transport).RoundTrip(req)
-   if err != nil {
-      t.Fatal(err)
-   }
-   defer res.Body.Close()
-   os.Stdout.ReadFrom(res.Body)
 }
