dbeea7fd0447b25189392674afc4dbad36efa1bd
Steven Penny <srpen6@gmail.com>
2021-10-06 12:17:49 -0500

    deleted:    bandcamp/resolve_test.go

--- bandcamp/bandcamp.go
+++ bandcamp/bandcamp.go
@@ -10,21 +10,54 @@ import (
    "strconv"
 )
 
-const Mobile = "http://bandcamp.com/api/mobile/24/tralbum_details"
-
-// thrjozkaskhjastaurrtygitylpt
-// throtaudvinroftignmarkreina
-// ullrettkalladrhampa
-const key = "veidihundr"
-
-var Verbose = mech.Verbose
-
 type Detail struct {
    Band_ID int `json:"band_id"`
    Tralbum_ID int `json:"tralbum_id,omitempty"`
    Tralbum_Type string `json:"tralbum_type,omitempty"`
 }
 
+const Mobile = "http://bandcamp.com/api/mobile/24/tralbum_details"
+
+var Verbose = mech.Verbose
+
+func (d Detail) Tralbum() (*Tralbum, error) {
+   buf := new(bytes.Buffer)
+   if err := json.NewEncoder(buf).Encode(d); err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest("POST", Mobile, buf)
+   if err != nil {
+      return nil, err
+   }
+   res, err := mech.RoundTrip(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   tra := new(Tralbum)
+   if err := json.NewDecoder(res.Body).Decode(tra); err != nil {
+      return nil, err
+   }
+   return tra, nil
+}
+
+type Tralbum struct {
+   Bandcamp_URL string
+   Tracks []struct {
+      Title string
+   }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+// thrjozkaskhjastaurrtygitylpt
+// throtaudvinroftignmarkreina
+// ullrettkalladrhampa
+const key = "veidihundr"
+
+// URL to ID, key
+
+// URL to ID, anonymous
 func TralbumDetail(addr string) (*Detail, error) {
    req, err := http.NewRequest("HEAD", addr, nil)
    if err != nil {
@@ -53,32 +86,4 @@ func TralbumDetail(addr string) (*Detail, error) {
       }, nil
    }
    return nil, fmt.Errorf("cookies %v", res.Cookies())
-}
-
-func (d Detail) Tralbum() (*Tralbum, error) {
-   buf := new(bytes.Buffer)
-   if err := json.NewEncoder(buf).Encode(d); err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest("POST", Mobile, buf)
-   if err != nil {
-      return nil, err
-   }
-   res, err := mech.RoundTrip(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   tra := new(Tralbum)
-   if err := json.NewDecoder(res.Body).Decode(tra); err != nil {
-      return nil, err
-   }
-   return tra, nil
-}
-
-type Tralbum struct {
-   Bandcamp_URL string
-   Tracks []struct {
-      Title string
-   }
 }
