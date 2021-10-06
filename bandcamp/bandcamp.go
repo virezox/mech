4ec2839115b154b3db64ca58b2f38125e40fa691
Steven Penny <srpen6@gmail.com>
2021-10-06 11:20:01 -0500

    deleted:    bandcamp/band.go

--- bandcamp/bandcamp.go
+++ bandcamp/bandcamp.go
@@ -1,12 +1,18 @@
 package bandcamp
 
 import (
+   "bytes"
+   "encoding/json"
    "fmt"
    "github.com/89z/mech"
    "net/http"
    "regexp"
    "strconv"
 )
+
+const MobileTralbum = "http://bandcamp.com/api/mobile/24/tralbum_details"
+
+var Verbose = mech.Verbose
 
 type Detail struct {
    Band_ID int `json:"band_id"`
@@ -42,4 +48,32 @@ func TralbumDetail(addr string) (*Detail, error) {
       }, nil
    }
    return nil, fmt.Errorf("cookies %v", res.Cookies())
+}
+
+func (d Detail) Tralbum() (*Tralbum, error) {
+   buf := new(bytes.Buffer)
+   if err := json.NewEncoder(buf).Encode(d); err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest("POST", MobileTralbum, buf)
+   if err != nil {
+      return nil, err
+   }
+   res, err := mech.RoundTrip(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   tra := new(Tralbum)
+   if err := json.NewDecoder(res.Body).Decode(tra); err != nil {
+      return nil, err
+   }
+   return tra, nil
+}
+
+type Tralbum struct {
+   Bandcamp_URL string
+   Tracks []struct {
+      Title string
+   }
 }
--- bandcamp/bandcamp_test.go
+++ bandcamp/bandcamp_test.go
@@ -1,6 +1,29 @@
 package bandcamp
 
 import (
+   "fmt"
+   "testing"
+)
+
+var details = []Detail{
+   {1, 79940049, "a"},
+   {1, 2809477874, "t"},
+}
+
+func TestTralbum(t *testing.T) {
+   Verbose(true)
+   for _, detail := range details {
+      tra, err := detail.Tralbum()
+      if err != nil {
+         t.Fatal(err)
+      }
+      fmt.Printf("%+v\n", tra)
+   }
+}
+
+package bandcamp
+
+import (
    "testing"
    "time"
 )
@@ -31,4 +54,22 @@ func TestDetail(t *testing.T) {
       }
       time.Sleep(99 * time.Millisecond)
    }
+}
+
+package bandcamp
+
+import (
+   "fmt"
+   "testing"
+)
+
+const bandID = 2853020814
+
+func TestBand(t *testing.T) {
+   Verbose(true)
+   ban := new(Band)
+   if err := ban.Post(bandID); err != nil {
+      t.Fatal(err)
+   }
+   fmt.Printf("%+v\n", ban)
 }
