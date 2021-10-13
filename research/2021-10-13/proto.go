d12ff59e5cbf3701f1d316cce3b090f427790ba4
Steven Penny <srpen6@gmail.com>
2021-10-13 17:55:13 -0500

    new file:   research/2021-10-13/proto_test.go

--- research/2021-10-13/proto.go
+++ research/2021-10-13/proto.go
@@ -1,21 +1,10 @@
-package main
+package youtube
 
 import (
    "encoding/base64"
    "fmt"
    "github.com/segmentio/encoding/proto"
 )
-
-func main() {
-   // CAESAA==
-   p := param{SortBy: 1}
-   b, err := proto.Marshal(p)
-   if err != nil {
-      panic(err)
-   }
-   s := base64.StdEncoding.EncodeToString(b)
-   fmt.Println(s)
-}
 
 type param struct {
    SortBy uint `protobuf:"varint,1"`
@@ -35,4 +24,120 @@ type param struct {
       HDR uint `protobuf:"varint,25"`
       VR180 uint `protobuf:"varint,26"`
    } `protobuf:"bytes,2"`
+}
+
+func (p param) Encode() (string, error) {
+   b, err := plenc.Marshal(nil, p)
+   if err != nil {
+      return "", err
+   }
+   return base64.StdEncoding.EncodeToString(b), nil
+}
+
+func (p *param) Relevance() {
+   p.SortBy = pUint(0)
+}
+
+func (p *param) Rating() {
+   p.SortBy = pUint(1)
+}
+
+func (p *param) UploadDate() {
+   p.SortBy = pUint(2)
+}
+
+func (p *param) ViewCount() {
+   p.SortBy = pUint(3)
+}
+
+func (p *param) LastHour() {
+   p.Filter.UploadDate = pUint(1)
+}
+
+func (p *param) Today() {
+   p.Filter.UploadDate = pUint(2)
+}
+
+func (p *param) ThisWeek() {
+   p.Filter.UploadDate = pUint(3)
+}
+
+func (p *param) ThisMonth() {
+   p.Filter.UploadDate = pUint(4)
+}
+
+func (p *param) ThisYear() {
+   p.Filter.UploadDate = pUint(5)
+}
+
+func (p *param) Video() {
+   p.Filter.Type = pUint(1)
+}
+
+func (p *param) Channel() {
+   p.Filter.Type = pUint(2)
+}
+
+func (p *param) Playlist() {
+   p.Filter.Type = pUint(3)
+}
+
+func (p *param) Movie() {
+   p.Filter.Type = pUint(4)
+}
+
+func (p *param) UnderFourMinutes() {
+   p.Filter.Duration = pUint(1)
+}
+
+func (p *param) OverTwentyMinutes() {
+   p.Filter.Duration = pUint(2)
+}
+
+func (p *param) FourToTwentyMinutes() {
+   p.Filter.Duration = pUint(3)
+}
+
+func (p *param) HD() {
+   p.Filter.HD = pUint(1)
+}
+
+func (p *param) Subtitles() {
+   p.Filter.Subtitles = pUint(1)
+}
+
+func (p *param) CreativeCommons() {
+   p.Filter.CreativeCommons = pUint(1)
+}
+
+func (p *param) ThreeD() {
+   p.Filter.ThreeD = pUint(1)
+}
+
+func (p *param) Live() {
+   p.Filter.Live = pUint(1)
+}
+
+func (p *param) Purchased() {
+   p.Filter.Purchased = pUint(1)
+}
+
+func (p *param) FourK() {
+   p.Filter.FourK = pUint(1)
+}
+
+func (p *param) ThreeSixty() {
+   p.Filter.ThreeSixty = pUint(1)
+}
+
+func (p *param) Location() {
+   p.Filter.Location = pUint(1)
+}
+
+func (p *param) HDR() {
+   p.Filter.HDR = pUint(1)
+}
+
+func (p *param) VR180() {
+   p.Filter.VR180 = pUint(1)
 }
---
+++ research/2021-10-13/proto_test.go
@@ -0,0 +1,53 @@
+package youtube
+
+import (
+   "fmt"
+   "testing"
+)
+
+type test struct {
+   key string
+   val func(*Param)
+}
+
+var tests = []test{
+   {"Channel", (*Param).Channel},
+   {"CreativeCommons", (*Param).CreativeCommons},
+   {"FourK", (*Param).FourK},
+   {"FourToTwentyMinutes", (*Param).FourToTwentyMinutes},
+   {"HD", (*Param).HD},
+   {"HDR", (*Param).HDR},
+   {"LastHour", (*Param).LastHour},
+   {"Live", (*Param).Live},
+   {"Location", (*Param).Location},
+   {"Movie", (*Param).Movie},
+   {"OverTwentyMinutes", (*Param).OverTwentyMinutes},
+   {"Playlist", (*Param).Playlist},
+   {"Purchased", (*Param).Purchased},
+   {"Rating", (*Param).Rating},
+   {"Relevance", (*Param).Relevance},
+   {"Subtitles", (*Param).Subtitles},
+   {"ThisMonth", (*Param).ThisMonth},
+   {"ThisWeek", (*Param).ThisWeek},
+   {"ThisYear", (*Param).ThisYear},
+   {"ThreeD", (*Param).ThreeD},
+   {"ThreeSixty", (*Param).ThreeSixty},
+   {"Today", (*Param).Today},
+   {"UnderFourMinutes", (*Param).UnderFourMinutes},
+   {"UploadDate", (*Param).UploadDate},
+   {"VR180", (*Param).VR180},
+   {"Video", (*Param).Video},
+   {"ViewCount", (*Param).ViewCount},
+}
+
+func TestParam(t *testing.T) {
+   for _, each := range tests {
+      var p Param
+      each.val(&p)
+      s, err := p.Encode()
+      if err != nil {
+         t.Fatal(err)
+      }
+      fmt.Print(each.key, "\n", s, "\n\n")
+   }
+}
