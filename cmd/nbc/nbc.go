5e1e71d3dae5372a9691e0a465916840b0d634d9
Steven Penny <srpen6@gmail.com>
2021-12-04 22:33:44 -0600

    new file:   cmd/nbc/nbc.go

---
+++ cmd/nbc/nbc.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+   "flag"
+   "fmt"
+   "github.com/89z/mech"
+   "github.com/89z/mech/nbc"
+   "github.com/89z/parse/m3u"
+   "net/http"
+   "os"
+   "path"
+   "strconv"
+   "strings"
+)
+
+
+type choice struct {
+   format bool
+   ids map[string]bool
+}
+
+func main() {
+   cHLS := choice{
+      ids: make(map[string]bool),
+   }
+   flag.BoolVar(&cHLS.format, "hf", false, "HLS formats")
+   flag.Func("h", "HLS IDs", func(id string) error {
+      cHLS.ids[id] = true
+      return nil
+   })
+   flag.Parse()
+   if len(os.Args) == 1 {
+      fmt.Println("nbc [flags] [URL]")
+      flag.PrintDefaults()
+      return
+   }
+   if !cHLS.format && len(cHLS.ids) == 0 {
+      return
+   }
+   addr := flag.Arg(0)
+   if err := cHLS.HLS(addr); err != nil {
+      panic(err)
+   }
+}
+
+func (c choice) HLS(addr string) error {
+   news, err := nbc.NewNewsVideo(addr)
+   if err != nil {
+      return err
+   }
+   media, err := news.Media()
+   if err != nil {
+      return err
+   }
+   ext, err := mech.ExtensionByType(media.Type)
+   if err != nil {
+      return err
+   }
+   video, err := media.Video()
+   if err != nil {
+      return err
+   }
+   forms, err := video.HLS()
+   if err != nil {
+      return err
+   }
+   base := path.Base(addr)
+   for _, form := range forms {
+      if c.format {
+         if !strings.HasPrefix(form.URI.File, "keyframes/") {
+            fmt.Printf("%+v\n", form)
+         }
+      } else if c.ids[strconv.Itoa(form.ID)] {
+         fmt.Println("GET", form.URI)
+         res, err := http.Get(form.URI.String())
+         if err != nil {
+            return err
+         }
+         defer res.Body.Close()
+         forms, err := m3u.Decode(res.Body, form.URI.Dir)
+         if err != nil {
+            return err
+         }
+         file, err := os.Create(news.Caption + "-" + base + ext)
+         if err != nil {
+            return err
+         }
+         defer file.Close()
+         for _, form := range forms {
+            fmt.Println("GET", form.URI)
+            res, err := http.Get(form.URI.String())
+            if err != nil {
+               return err
+            }
+            defer res.Body.Close()
+            if _, err := file.ReadFrom(res.Body); err != nil {
+               return err
+            }
+         }
+      }
+   }
+   return nil
+}
--- nbc/nbc.go
+++ nbc/nbc.go
@@ -4,27 +4,23 @@ import (
    "github.com/89z/mech"
    "github.com/89z/parse/m3u"
    "net/http"
-   "net/url"
    "strconv"
 )
 
 const accountID = 2410887629
 
 func Media(guid int64) ([]m3u.Format, error) {
-   var addr []byte
-   addr = append(addr, "http://link.theplatform.com/s/NnzsPC/media/guid/"...)
+   addr := []byte("http://link.theplatform.com/s/NnzsPC/media/guid/")
    addr = strconv.AppendInt(addr, accountID, 10)
    addr = append(addr, '/')
    addr = strconv.AppendInt(addr, guid, 10)
+   addr = append(addr, "?manifest=m3u"...)
    req, err := http.NewRequest(
       "GET", string(addr), nil,
    )
    if err != nil {
       return nil, err
    }
-   req.URL.RawQuery = url.Values{
-      "manifest": {"m3u"},
-   }.Encode()
    mech.Dump(req)
    // this redirects, so cannot use RoundTrip
    res, err := new(http.Client).Do(req)
