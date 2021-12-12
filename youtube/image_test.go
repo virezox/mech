bd1d6e81e26a5e21815b649314088269ebd189e1
Steven Penny <srpen6@gmail.com>
2021-12-12 15:09:06 -0600

    deleted:    research/2021-12-12/image.go

--- youtube/image_test.go
+++ youtube/image_test.go
@@ -9,10 +9,10 @@ import (
 
 const id = "UpNXI3_ctAc"
 
-func TestPicture(t *testing.T) {
-   for _, p := range Pictures {
-      addr := p.Address(id)
-      fmt.Println("Head", addr)
+func TestImage(t *testing.T) {
+   for _, img := range Images {
+      addr := img.Format(id)
+      fmt.Println("HEAD", addr)
       res, err := http.Head(addr)
       if err != nil {
          t.Fatal(err)
@@ -20,6 +20,6 @@ func TestPicture(t *testing.T) {
       if res.StatusCode != http.StatusOK {
          t.Fatal(res.Status)
       }
-      time.Sleep(100 * time.Millisecond)
+      time.Sleep(99 * time.Millisecond)
    }
 }
