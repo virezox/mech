d08e06c7c54a15437a4684dddab166f4f318332e
Steven Penny <srpen6@gmail.com>
2021-12-16 09:40:56 -0600

    deleted:    research/2021-12-16/dec.py

--- research/2021-12-16/legacy.go
+++ research/2021-12-16/legacy.go
@@ -15,105 +15,20 @@ const data =
    "559ccf552d87546587a1ecf9cb940b2a3417f2f080c3e3cd5dda618392fa73d82b896e12f222c09c70ed7e67df541a956a0c86f570dfed8d921a32f18e82f705579b232dc6ba4debf9d52d0bf280d5d" +
    "4b5b3f07a0b5c1965561912c2a452e255b799ba187c2c6eab13480a41275f6bddd777fddc2380d380bab700f77a58ee7b37328b66d7a7f3fbfd266"
 
+const blockSize = 8
+
 func main() {
    src, err := hex.DecodeString(data)
    if err != nil {
       panic(err)
    }
    dst := make([]byte, len(src))
-   legacyDecrypt(dst, src)
+   c, err := blowfish.NewCipher([]byte(`6#26FRL$ZWD`))
+   if err != nil {
+      panic(err)
+   }
+   for bs, be := 0, blockSize; bs < len(src); bs, be = bs+blockSize, be+blockSize {
+      c.Decrypt(dst[bs:be], src[bs:be])
+   }
    fmt.Printf("%q\n", dst)
-}
-
-const (
-	blowfishBlockSize = 8
-
-	legacyAPIEndpoint = "https://tuner.pandora.com/services/json/"
-
-	legacyPartnerUsername        = "android"
-	legacyPartnerPassword        = "AC7IBG09A3DTSYM4R41UJWL07VLN8JI7"
-	legacyPartnerDeviceID        = "android-generic"
-	legacyPartnerEncryptPassword = `6#26FRL$ZWD`
-	legacyPartnerDecryptPassword = `R=U!LH$O2B#`
-	legacyPartnerAPIVersion      = "5"
-)
-
-func mustCipher(key string) *blowfish.Cipher {
-	c, err := blowfish.NewCipher([]byte(key))
-	if err != nil {
-		panic(err)
-	}
-
-	return c
-}
-
-var (
-	legacyEncryptCipher = mustCipher(legacyPartnerEncryptPassword)
-	legacyDecryptCipher = mustCipher(legacyPartnerDecryptPassword)
-)
-
-type LegacyRequest struct {
-	SyncTime int64 `json:"syncTime,omitempty"`
-}
-
-type LegacyResponse struct {
-	Stat string `json:"stat"`
-}
-
-type LegacyPartnerLoginRequest struct {
-	LegacyRequest
-
-	Username    string `json:"username"`
-	Password    string `json:"password"`
-	DeviceModel string `json:"deviceModel"`
-	Version     string `json:"version"`
-
-	IncludeURLs                bool `json:"includeUrls"`
-	ReturnDeviceType           bool `json:"returnDeviceType"`
-	ReturnUpdatePromptVersions bool `json:"returnUpdatePromptVersions"`
-}
-
-type LegacyPartnerLoginResponseResult struct {
-	EncryptedSyncTime string `json:"syncTime"`
-	PartnerID         string `json:"partnerId"`
-	PartnerAuthToken  string `json:"partnerAuthToken"`
-}
-
-type LegacyPartnerLoginResponse struct {
-	LegacyResponse
-	Result LegacyPartnerLoginResponseResult `json:"result"`
-}
-
-type LegacyUserLoginRequest struct {
-	LegacyRequest
-
-	LoginType        string `json:"loginType"`
-	Username         string `json:"username"`
-	Password         string `json:"password"`
-	PartnerAuthToken string `json:"partnerAuthToken"`
-}
-
-type LegacyUserLoginResponseResult struct {
-	UserAuthToken string `json:"userAuthToken"`
-}
-
-type LegacyUserLoginResponse struct {
-	LegacyResponse
-	Result LegacyUserLoginResponseResult `json:"result"`
-}
-
-func blowfishPad(b []byte) []byte {
-	return append(b, make([]byte, blowfishBlockSize-(len(b)%blowfishBlockSize))...)
-}
-
-func legacyEncrypt(dst []byte, src []byte) {
-	for bs, be := 0, blowfishBlockSize; bs < len(src); bs, be = bs+blowfishBlockSize, be+blowfishBlockSize {
-		legacyEncryptCipher.Encrypt(dst[bs:be], src[bs:be])
-	}
-}
-
-func legacyDecrypt(dst []byte, src []byte) {
-	for bs, be := 0, blowfishBlockSize; bs < len(src); bs, be = bs+blowfishBlockSize, be+blowfishBlockSize {
-		legacyDecryptCipher.Decrypt(dst[bs:be], src[bs:be])
-	}
 }
