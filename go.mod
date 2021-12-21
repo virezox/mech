e08c30dc6c38c2faca0cc9f201cc031724a6ab85
name <srpen6@gmail.com>
2021-12-21 17:29:08 -0600

    new file:   instagram/login.go

--- go.mod
+++ go.mod
@@ -2,7 +2,7 @@ module github.com/89z/mech
 
 go 1.17
 
-require github.com/89z/parse v1.14.0
+require github.com/89z/parse v1.14.3
 
 require (
 	github.com/tdewolff/parse/v2 v2.5.22 // indirect
--- instagram/instagram.go
+++ instagram/instagram.go
@@ -1,17 +1,11 @@
 package instagram
 
 import (
-   "bytes"
-   "encoding/json"
    "github.com/89z/mech"
-   "io"
-   "net/http"
-   "net/url"
 )
 
 const (
-   OriginI = "https://i.instagram.com"
-   OriginWWW = "https://www.instagram.com"
+   originI = "https://i.instagram.com"
    queryHash = "2efa04f61586458cef44441f474eee7c"
    // com.instagram.android
    userAgent = "Instagram 214.1.0.29.120 Android"
@@ -51,71 +45,6 @@ type Item struct {
    }
 }
 
-type Login struct {
-   http.Header
-}
-
-func NewLogin(username, password string) (*Login, error) {
-   buf := bytes.NewBufferString("signed_body=SIGNATURE.")
-   sig := map[string]string{
-      "device_id": userAgent,
-      "enc_password": "#PWD_INSTAGRAM:0:0:" + password,
-      "username": username,
-   }
-   if err := json.NewEncoder(buf).Encode(sig); err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest("POST", OriginI + "/api/v1/accounts/login/", buf)
-   if err != nil {
-      return nil, err
-   }
-   req.Header = http.Header{
-      "Content-Type": {"application/x-www-form-urlencoded"},
-      "User-Agent": {userAgent},
-   }
-   LogLevel.Dump(req)
-   res, err := new(http.Transport).RoundTrip(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   return &Login{res.Header}, nil
-}
-
-// This can be used to decode an existing login response.
-func (l *Login) Decode(r io.Reader) error {
-   return json.NewDecoder(r).Decode(&l.Header)
-}
-
-func (l Login) Encode(w io.Writer) error {
-   enc := json.NewEncoder(w)
-   enc.SetIndent("", " ")
-   return enc.Encode(l.Header)
-}
-
-func (l Login) Item(shortcode string) (*Item, error) {
-   req, err := http.NewRequest("GET", OriginI + "/api/v1/clips/item/", nil)
-   if err != nil {
-      return nil, err
-   }
-   req.Header = http.Header{
-      "Authorization": {l.Get("Ig-Set-Authorization")},
-      "User-Agent": {userAgent},
-   }
-   req.URL.RawQuery = "clips_media_shortcode=" + url.QueryEscape(shortcode)
-   LogLevel.Dump(req)
-   res, err := new(http.Transport).RoundTrip(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   item := new(Item)
-   if err := json.NewDecoder(res.Body).Decode(item); err != nil {
-      return nil, err
-   }
-   return item, nil
-}
-
 type Media struct {
    Shortcode_Media struct {
       Display_URL string
@@ -134,32 +63,6 @@ type Media struct {
       }
       Video_URL string
    }
-}
-
-// If `auth` is `nil`, then anonymous request will be used.
-func GraphQL(shortcode string, auth *Login) (*Media, error) {
-   req, err := http.NewRequest("GET", OriginWWW + "/p/" + shortcode + "/", nil)
-   if err != nil {
-      return nil, err
-   }
-   req.Header.Set("User-Agent", userAgent)
-   if auth != nil && auth.Header != nil {
-      req.Header.Set("Authorization", auth.Get("Ig-Set-Authorization"))
-   }
-   req.URL.RawQuery = "__a=1"
-   LogLevel.Dump(req)
-   res, err := new(http.Transport).RoundTrip(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   var car struct {
-      GraphQL Media
-   }
-   if err := json.NewDecoder(res.Body).Decode(&car); err != nil {
-      return nil, err
-   }
-   return &car.GraphQL, nil
 }
 
 func (m Media) Edges() []Edge {
@@ -181,37 +84,4 @@ func NewQuery(shortcode string) Query {
    val.Query_Hash = queryHash
    val.Variables.Shortcode = shortcode
    return val
-}
-
-// If `auth` is `nil`, then anonymous request will be used.
-func (q Query) Data(auth *Login) (*Media, error) {
-   buf := new(bytes.Buffer)
-   err := json.NewEncoder(buf).Encode(q)
-   if err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest("POST", OriginI + "/graphql/query/", buf)
-   if err != nil {
-      return nil, err
-   }
-   req.Header = http.Header{
-      "Content-Type": {"application/json"},
-      "User-Agent": {userAgent},
-   }
-   if auth != nil && auth.Header != nil {
-      req.Header.Set("Authorization", auth.Get("Ig-Set-Authorization"))
-   }
-   LogLevel.Dump(req)
-   res, err := new(http.Transport).RoundTrip(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   var car struct {
-      Data Media
-   }
-   if err := json.NewDecoder(res.Body).Decode(&car); err != nil {
-      return nil, err
-   }
-   return &car.Data, nil
 }
