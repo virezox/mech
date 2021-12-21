6b14b2ef35ea3511b638e33b8067198e85b80244
name <srpen6@gmail.com>
2021-12-21 17:29:08 -0600

    new file:   research/2021-12-16/userlogin.go

--- research/2021-12-16/pandora.go
+++ research/2021-12-16/pandora.go
@@ -6,62 +6,26 @@ import (
    "github.com/89z/mech"
    "golang.org/x/crypto/blowfish"
    "net/http"
+   "net/http/httputil"
    "net/url"
+   "os"
    "strings"
    "time"
 )
 
-type userLogin struct {
-   Result struct {
-      UserID string
-      UserAuthToken string
-   }
-}
+var (
+   _ = hex.EncodeToString
+   _ = time.Sleep
+)
 
-func (p partnerLogin) userLogin(username, password string) (*userLogin, error) {
-   rUser := userLoginRequest{
-      LoginType: "user",
-      PartnerAuthToken: p.Result.PartnerAuthToken,
-      Password: password,
-      SyncTime: time.Now().Unix(),
-      Username: username,
-   }
-   dec, err := json.Marshal(rUser)
-   if err != nil {
-      return nil, err
-   }
-   enc, err := encrypt(dec)
-   if err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest(
-      "POST", origin + "/services/json/",
-      strings.NewReader(hex.EncodeToString(enc)),
-   )
-   if err != nil {
-      return nil, err
-   }
-   req.Header.Set("User-Agent", "Pandora/2110.1 Android/7.0 generic_x86")
-   req.URL.RawQuery =
-      "partner_id=42&method=auth.userLogin&auth_token=" +
-      url.QueryEscape(p.Result.PartnerAuthToken)
-   LogLevel.Dump(req)
-   res, err := new(http.Transport).RoundTrip(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   user := new(userLogin)
-   if err := json.NewDecoder(res.Body).Decode(user); err != nil {
-      return nil, err
-   }
-   return user, nil
-}
+type values map[string]string
 
-type partnerLogin struct {
-   Result struct {
-      PartnerAuthToken string
+func (v values) encode() string {
+   vals := make(url.Values)
+   for key, val := range v {
+      vals.Set(key, val)
    }
+   return vals.Encode()
 }
 
 const origin = "http://android-tuner.pandora.com"
@@ -99,6 +63,12 @@ func encrypt(src []byte) ([]byte, error) {
    return dst, nil
 }
 
+type partnerLogin struct {
+   Result struct {
+      PartnerAuthToken string
+   }
+}
+
 func newPartnerLogin() (*partnerLogin, error) {
    body := `{"username":"android","password":"AC7IBG09A3DTSYM4R41UJWL07VLN8JI7"}`
    req, err := http.NewRequest(
@@ -121,10 +91,72 @@ func newPartnerLogin() (*partnerLogin, error) {
    return login, nil
 }
 
-type userLoginRequest struct {
-   LoginType string `json:"loginType"`
-   PartnerAuthToken string `json:"partnerAuthToken"`
-   Password string `json:"password"`
+type playbackInfo struct {
+   Result struct {
+      AudioUrlMap struct {
+         HighQuality struct {
+            AudioURL string
+         }
+      }
+   }
+}
+
+type playbackInfoRequest struct {
+   // this can be empty, but must be included:
+   DeviceCode string `json:"deviceCode"`
+   IncludeAudioToken bool `json:"includeAudioToken"`
+   PandoraID string `json:"pandoraId"`
    SyncTime int64 `json:"syncTime"`
-   Username string `json:"username"`
+   UserAuthToken string `json:"userAuthToken"`
+}
+
+func (u userLogin) playbackInfo() (*playbackInfo, error) {
+   rInfo := playbackInfoRequest{
+      IncludeAudioToken: true,
+      SyncTime: 2222222222,
+      PandoraID: "TR:1168891",
+      // this is failing:
+      UserAuthToken: u.Result.UserAuthToken,
+   }
+   dec, err := json.Marshal(rInfo)
+   if err != nil {
+      return nil, err
+   }
+   os.Stdout.Write(dec)
+   enc, err := encrypt(dec)
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest(
+      "POST", origin + "/services/json/",
+      strings.NewReader(hex.EncodeToString(enc)),
+   )
+   if err != nil {
+      return nil, err
+   }
+   // method=auth.partnerLogin
+   req.URL.RawQuery = values{
+      // this can be empty, but it must be included:
+      "auth_token": "",
+      "method": "onDemand.getAudioPlaybackInfo",
+      "partner_id": "42",
+      // this can be empty, but it must be included:
+      "user_id": "",
+   }.encode()
+   LogLevel.Dump(req)
+   res, err := new(http.Transport).RoundTrip(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   buf, err := httputil.DumpResponse(res, true)
+   if err != nil {
+      return nil, err
+   }
+   os.Stdout.Write(buf)
+   info := new(playbackInfo)
+   if err := json.NewDecoder(res.Body).Decode(info); err != nil {
+      return nil, err
+   }
+   return info, nil
 }
