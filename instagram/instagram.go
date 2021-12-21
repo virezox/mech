ffd0554938ad071b6c61b787bdd746eaf1382f4e
name <srpen6@gmail.com>
2021-12-21 17:29:08 -0600

    deleted:    instagram/login.go

--- instagram/instagram.go
+++ instagram/instagram.go
@@ -1,7 +1,11 @@
 package instagram
 
 import (
+   "bytes"
+   "encoding/json"
    "github.com/89z/mech"
+   "io"
+   "net/http"
 )
 
 const (
@@ -45,6 +49,78 @@ type Item struct {
    }
 }
 
+type Login struct {
+   Authorization string
+}
+
+func NewLogin(username, password string) (*Login, error) {
+   buf := bytes.NewBufferString("signed_body=SIGNATURE.")
+   sig := map[string]string{
+      "device_id": userAgent,
+      "enc_password": "#PWD_INSTAGRAM:0:0:" + password,
+      "username": username,
+   }
+   if err := json.NewEncoder(buf).Encode(sig); err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest("POST", originI + "/api/v1/accounts/login/", buf)
+   if err != nil {
+      return nil, err
+   }
+   req.Header = http.Header{
+      "Content-Type": {"application/x-www-form-urlencoded"},
+      "User-Agent": {userAgent},
+   }
+   LogLevel.Dump(req)
+   res, err := new(http.Transport).RoundTrip(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   var login Login
+   login.Authorization = res.Header.Get("Ig-Set-Authorization")
+   return &login, nil
+}
+
+// This can be used to decode an existing login response.
+func (l *Login) Decode(src io.Reader) error {
+   return json.NewDecoder(src).Decode(l)
+}
+
+func (l Login) Encode(dst io.Writer) error {
+   enc := json.NewEncoder(dst)
+   enc.SetIndent("", " ")
+   return enc.Encode(l)
+}
+
+// If `Authorization` field is empty, then anonymous request will be used.
+func (l Login) GraphQL(shortcode string) (*Media, error) {
+   req, err := http.NewRequest(
+      "GET", "https://www.instagram.com/p/" + shortcode + "/", nil,
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.Header.Set("User-Agent", userAgent)
+   if l.Authorization != "" {
+      req.Header.Set("Authorization", l.Authorization)
+   }
+   req.URL.RawQuery = "__a=1"
+   LogLevel.Dump(req)
+   res, err := new(http.Transport).RoundTrip(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   var car struct {
+      GraphQL Media
+   }
+   if err := json.NewDecoder(res.Body).Decode(&car); err != nil {
+      return nil, err
+   }
+   return &car.GraphQL, nil
+}
+
 type Media struct {
    Shortcode_Media struct {
       Display_URL string
@@ -70,18 +146,4 @@ func (m Media) Edges() []Edge {
       return nil
    }
    return m.Shortcode_Media.Edge_Sidecar_To_Children.Edges
-}
-
-type Query struct {
-   Query_Hash string `json:"query_hash"`
-   Variables struct {
-      Shortcode string `json:"shortcode"`
-   } `json:"variables"`
-}
-
-func NewQuery(shortcode string) Query {
-   var val Query
-   val.Query_Hash = queryHash
-   val.Variables.Shortcode = shortcode
-   return val
 }
