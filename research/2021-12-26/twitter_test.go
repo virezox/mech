7010174cc8ad15c64bbbf643e44ed4a5a45f03b5
name <srpen6@gmail.com>
2021-12-26 09:12:44 -0600

    +   iAPI = "https://twitter.com/i/api/1.1"

--- research/2021-12-26/twitter_test.go
+++ research/2021-12-26/twitter_test.go
@@ -10,6 +10,10 @@ const (
    statusID = 1470124083547418624
 )
 
+GET /i/api/1.1/live_video_stream/status/28_1468947428984328193?client=web&use_syndication_guest_id=false&cookie_set_host=twitter.com HTTP/1.1
+Host: twitter.com
+authorization: Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA
+x-guest-token: 1474894925598703618
 var guest = &Guest{"1475108770955022337"}
 
 func TestSpace(t *testing.T) {
