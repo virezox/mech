6a7d298749ce60bc9828c42dc89fe4da6c684f4c
Zombo <srpen6@gmail.com>
2022-01-31 21:22:54 -0600

    new file:   research/instagram/mediaitem.go

---
+++ research/instagram/mediaitem.go
@@ -0,0 +1,123 @@
+package instagram
+
+import (
+   "encoding/base64"
+   "encoding/binary"
+   "encoding/json"
+   "github.com/89z/format"
+   "net/http"
+   "strconv"
+)
+
+var LogLevel format.LogLevel
+
+func GetID(shortcode string) (uint64, error) {
+   for len(shortcode) <= 11 {
+      shortcode = "A" + shortcode
+   }
+   buf, err := base64.URLEncoding.DecodeString(shortcode)
+   if err != nil {
+      return 0, err
+   }
+   return binary.BigEndian.Uint64(buf[1:]), nil
+}
+
+type Info struct {
+   Media_Type int
+   Image_Versions2 struct {
+      Candidates []Version
+   }
+   Video_Versions []Version
+}
+
+func (i Info) Version() (*Version, error) {
+   var dst Version
+   switch i.Media_Type {
+   case 1:
+      for _, src := range i.Image_Versions2.Candidates {
+         if src.Height > dst.Height {
+            dst = src
+         }
+      }
+   case 2:
+      done := make(map[string]bool)
+      var length int64
+      for _, src := range i.Video_Versions {
+         if !done[src.URL] {
+            done[src.URL] = true
+            if src.Height > dst.Height {
+               dst = src
+            } else if src.Height == dst.Height {
+               req, err := http.NewRequest("HEAD", src.URL, nil)
+               if err != nil {
+                  return nil, err
+               }
+               LogLevel.Dump(req)
+               res, err := new(http.Transport).RoundTrip(req)
+               if err != nil {
+                  return nil, err
+               }
+               if res.ContentLength > length {
+                  dst = src
+               }
+            }
+         }
+      }
+   }
+   return &dst, nil
+}
+
+type MediaItem struct {
+   Info
+   Carousel_Media []Info
+}
+
+// This gets us image 1241 by 1241, but requires authentication.
+func MediaItems(id uint64) ([]MediaItem, error) {
+   buf := []byte("https://i.instagram.com/api/v1/media/")
+   buf = strconv.AppendUint(buf, id, 10)
+   buf = append(buf, "/info/"...)
+   req, err := http.NewRequest("GET", string(buf), nil)
+   if err != nil {
+      return nil, err
+   }
+   req.Header = http.Header{
+      "Authorization": {auth},
+      "user-agent": {"Instagram 206.1.0.34.121 Android"},
+   }
+   LogLevel.Dump(req)
+   res, err := new(http.Transport).RoundTrip(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   if res.StatusCode != http.StatusOK {
+      return nil, errorString(res.Status)
+   }
+   var info struct {
+      Items []MediaItem
+   }
+   if err := json.NewDecoder(res.Body).Decode(&info); err != nil {
+      return nil, err
+   }
+   return info.Items, nil
+}
+
+func (m MediaItem) Infos() []Info {
+   if m.Media_Type == 8 {
+      return m.Carousel_Media
+   }
+   return []Info{m.Info}
+}
+
+type Version struct {
+   Width int
+   Height int
+   URL string
+}
+
+type errorString string
+
+func (e errorString) Error() string {
+   return string(e)
+}
--- research/instagram/mediaitem_test.go
+++ research/instagram/mediaitem_test.go
@@ -16,28 +16,36 @@ var tests = []testType{
    {"CZVEugIPkVn", 2762134734241678695},
    // type:2 video:1 image:0
    {"CUDJ4YhpF0Z", 2667018861376986393},
-   // type:8 video:0 image:2
+   // type:8 video:0 image:6
    {"CXzGW6RPNmy", 2734557361417804210},
-   // type:8 video:1 image:1
+   // type:8 video:1 image:2
    {"CUK-1wjqqsP", 2669222102324390671},
-   // type:8 video:2 image:0
+   // type:8 video:3 image:0
    {"BQ0eAlwhDrw", 1455920561485265648},
 }
 
 func TestMedia(t *testing.T) {
    for _, test := range tests {
-      med, err := newMedia(test.id)
+      items, err := MediaItems(test.id)
       if err != nil {
          t.Fatal(err)
       }
-      fmt.Printf("%+v\n", med)
+      for _, item := range items {
+         for _, info := range item.Infos() {
+            ver, err := info.Version()
+            if err != nil {
+               t.Fatal(err)
+            }
+            fmt.Println(ver.URL[:99])
+         }
+      }
       time.Sleep(time.Second)
    }
 }
 
 func TestID(t *testing.T) {
    for _, test := range tests {
-      id, err := getID(test.shortcode)
+      id, err := GetID(test.shortcode)
       if err != nil {
          t.Fatal(err)
       }
