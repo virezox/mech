5947efb28bce8e9e1595375cf7da8378b6693cf7
Zombo <srpen6@gmail.com>
2022-02-04 15:40:52 -0600

    +         Media_URL string

--- twitter/twitter.go
+++ twitter/twitter.go
@@ -10,6 +10,92 @@ import (
    "time"
 )
 
+const bearer =
+   "AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs=" +
+   "1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA"
+
+var LogLevel format.LogLevel
+
+func Parse(id string) (uint64, error) {
+   return strconv.ParseUint(id, 10, 64)
+}
+
+type Guest struct {
+   Guest_Token string
+}
+
+func NewGuest() (*Guest, error) {
+   req, err := http.NewRequest(
+      "POST", "https://api.twitter.com/1.1/guest/activate.json", nil,
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.Header.Set("Authorization", "Bearer " + bearer)
+   LogLevel.Dump(req)
+   res, err := new(http.Transport).RoundTrip(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   guest := new(Guest)
+   if err := json.NewDecoder(res.Body).Decode(guest); err != nil {
+      return nil, err
+   }
+   return guest, nil
+}
+
+type Status struct {
+   Extended_Entities struct {
+      Media []struct {
+         Media_URL string
+         Video_Info struct {
+            Variants []Variant
+         }
+      }
+   }
+   User struct {
+      Name string
+   }
+}
+
+func NewStatus(guest *Guest, id uint64) (*Status, error) {
+   buf := []byte("https://api.twitter.com/1.1/statuses/show/")
+   buf = strconv.AppendUint(buf, id, 10)
+   buf = append(buf, ".json?tweet_mode=extended"...)
+   req, err := http.NewRequest("GET", string(buf), nil)
+   if err != nil {
+      return nil, err
+   }
+   req.Header = http.Header{
+      "Authorization": {"Bearer " + bearer},
+      "X-Guest-Token": {guest.Guest_Token},
+   }
+   LogLevel.Dump(req)
+   res, err := new(http.Transport).RoundTrip(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   stat := new(Status)
+   if err := json.NewDecoder(res.Body).Decode(stat); err != nil {
+      return nil, err
+   }
+   return stat, nil
+}
+
+func (s Status) Variants() []Variant {
+   var varis []Variant
+   for _, med := range s.Extended_Entities.Media {
+      for _, vari := range med.Video_Info.Variants {
+         if vari.Content_Type != "application/x-mpegURL"{
+            varis = append(varis, vari)
+         }
+      }
+   }
+   return varis
+}
+
 type Space struct {
    Data struct {
       AudioSpace struct {
@@ -114,105 +200,6 @@ type Stream struct {
    }
 }
 
-type spaceRequest struct {
-   ID string `json:"id"`
-   IsMetatagsQuery bool `json:"isMetatagsQuery"`
-   WithBirdwatchPivots bool `json:"withBirdwatchPivots"`
-   WithDownvotePerspective bool `json:"withDownvotePerspective"`
-   WithReactionsMetadata bool `json:"withReactionsMetadata"`
-   WithReactionsPerspective bool `json:"withReactionsPerspective"`
-   WithReplays bool `json:"withReplays"`
-   WithScheduledSpaces bool `json:"withScheduledSpaces"`
-   WithSuperFollowsTweetFields bool `json:"withSuperFollowsTweetFields"`
-   WithSuperFollowsUserFields bool `json:"withSuperFollowsUserFields"`
-}
-
-const API = "https://api.twitter.com/1.1"
-
-const bearer =
-   "AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs=" +
-   "1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA"
-
-var LogLevel format.LogLevel
-
-func Parse(id string) (uint64, error) {
-   return strconv.ParseUint(id, 10, 64)
-}
-
-type Guest struct {
-   Guest_Token string
-}
-
-func NewGuest() (*Guest, error) {
-   req, err := http.NewRequest("POST", API + "/guest/activate.json", nil)
-   if err != nil {
-      return nil, err
-   }
-   req.Header.Set("Authorization", "Bearer " + bearer)
-   LogLevel.Dump(req)
-   res, err := new(http.Transport).RoundTrip(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   guest := new(Guest)
-   if err := json.NewDecoder(res.Body).Decode(guest); err != nil {
-      return nil, err
-   }
-   return guest, nil
-}
-
-type Status struct {
-   Extended_Entities struct {
-      Media []struct {
-         Video_Info struct {
-            Variants []Variant
-         }
-      }
-   }
-   User struct {
-      Name string
-   }
-}
-
-func NewStatus(guest *Guest, id uint64) (*Status, error) {
-   buf := []byte(API)
-   buf = append(buf, "/statuses/show/"...)
-   buf = strconv.AppendUint(buf, id, 10)
-   buf = append(buf, ".json?tweet_mode=extended"...)
-   req, err := http.NewRequest("GET", string(buf), nil)
-   if err != nil {
-      return nil, err
-   }
-   req.Header = http.Header{
-      "Authorization": {"Bearer " + bearer},
-      "X-Guest-Token": {guest.Guest_Token},
-   }
-   LogLevel.Dump(req)
-   res, err := new(http.Transport).RoundTrip(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   stat := new(Status)
-   if err := json.NewDecoder(res.Body).Decode(stat); err != nil {
-      return nil, err
-   }
-   return stat, nil
-}
-
-func (s Status) Variants() []Variant {
-   var varis []Variant
-   for _, med := range s.Extended_Entities.Media {
-      for _, vari := range med.Video_Info.Variants {
-         if vari.Content_Type != "application/x-mpegURL"{
-            varis = append(varis, vari)
-         }
-      }
-   }
-   return varis
-}
-
 type URL string
 
 func (u URL) String() string {
@@ -228,4 +215,17 @@ func (u URL) String() string {
 type Variant struct {
    Content_Type string
    URL URL
+}
+
+type spaceRequest struct {
+   ID string `json:"id"`
+   IsMetatagsQuery bool `json:"isMetatagsQuery"`
+   WithBirdwatchPivots bool `json:"withBirdwatchPivots"`
+   WithDownvotePerspective bool `json:"withDownvotePerspective"`
+   WithReactionsMetadata bool `json:"withReactionsMetadata"`
+   WithReactionsPerspective bool `json:"withReactionsPerspective"`
+   WithReplays bool `json:"withReplays"`
+   WithScheduledSpaces bool `json:"withScheduledSpaces"`
+   WithSuperFollowsTweetFields bool `json:"withSuperFollowsTweetFields"`
+   WithSuperFollowsUserFields bool `json:"withSuperFollowsUserFields"`
 }
