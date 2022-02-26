5bb23a48370c54973849cd1279266e9a86e818fb
Zombo <srpen6@gmail.com>
2022-02-25 22:14:02 -0600

    new file:   bbc/m3u.go

--- bbc/bbc.go
+++ bbc/bbc.go
@@ -3,10 +3,8 @@ package bbc
 import (
    "encoding/json"
    "github.com/89z/format"
-   "github.com/89z/format/m3u"
    "net/http"
    "path"
-   "strconv"
    "strings"
 )
 
@@ -32,50 +30,6 @@ func NewNewsItem(addr string) (*NewsItem, error) {
       return nil, err
    }
    return item, nil
-}
-
-// #EXT-X-STREAM-INF
-type Stream struct {
-   ID int64
-   Resolution string
-   Bandwidth int64
-   Codecs string
-   URI string
-}
-
-func (s Stream) Information() ([]string, error) {
-   req, err := http.NewRequest("GET", s.URI, nil)
-   if err != nil {
-      return nil, err
-   }
-   LogLevel.Dump(req)
-   res, err := new(http.Transport).RoundTrip(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   dir, _ := path.Split(s.URI)
-   forms, err := m3u.Decode(res.Body, dir)
-   if err != nil {
-      return nil, err
-   }
-   var infos []string
-   for _, form := range forms {
-      infos = append(infos, form["URI"])
-   }
-   return infos, nil
-}
-
-func (s Stream) String() string {
-   buf := []byte("ID:")
-   buf = strconv.AppendInt(buf, s.ID, 10)
-   buf = append(buf, " Resolution:"...)
-   buf = append(buf, s.Resolution...)
-   buf = append(buf, " Bandwidth:"...)
-   buf = strconv.AppendInt(buf, s.Bandwidth, 10)
-   buf = append(buf, " Codecs:"...)
-   buf = append(buf, s.Codecs...)
-   return string(buf)
 }
 
 type NewsItem struct {
@@ -153,41 +107,6 @@ func (m Media) href() string {
    }
    return ""
 }
-
-func (m Media) Streams() ([]Stream, error) {
-   addr := m.href()
-   req, err := http.NewRequest("GET", addr, nil)
-   if err != nil {
-      return nil, err
-   }
-   LogLevel.Dump(req)
-   res, err := new(http.Transport).RoundTrip(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   dir, _ := path.Split(addr)
-   forms, err := m3u.Decode(res.Body, dir)
-   if err != nil {
-      return nil, err
-   }
-   var streams []Stream
-   for i, form := range forms {
-      var stream Stream
-      stream.Bandwidth, err = strconv.ParseInt(form["BANDWIDTH"], 10, 64)
-      if err != nil {
-         return nil, err
-      }
-      stream.Codecs = form["CODECS"]
-      stream.ID = int64(i)
-      stream.Resolution = form["RESOLUTION"]
-      stream.URI = form["URI"]
-      streams = append(streams, stream)
-   }
-   return streams, nil
-}
-
-////////////////////////////////////////////////////////////////////////////////
 
 func (m Media) Name(item *NewsItem) (string, error) {
    ext, err := format.ExtensionByType(m.Type)
--- research/bbc/bbc.go
+++ research/bbc/bbc.go
@@ -2,14 +2,26 @@ package bbc
 
 import (
    "encoding/json"
+   "github.com/89z/format"
    "net/http"
+   "path"
    "strings"
 )
+
+var LogLevel format.LogLevel
+
+type Connection struct {
+   Protocol string
+   Supplier string
+   TransferFormat string
+   Href string
+}
 
 type Media struct {
    Kind string
    Type string
    Connection []Connection
+   mediaset *Mediaset
 }
 
 func (m Media) GetConnection() *Connection {
@@ -25,22 +37,67 @@ func (m Media) GetConnection() *Connection {
    return nil
 }
 
+func (m Media) Name() (string, error) {
+   ext, err := format.ExtensionByType(m.Type)
+   if err != nil {
+      return "", err
+   }
+   return m.mediaset.relation.newsItem.Base() + ext, nil
+}
+
 type Mediaset struct {
    Media []Media
+   relation *Relation
 }
 
 func (m Mediaset) GetMedia() *Media {
    for _, med := range m.Media {
       if med.Kind == "video" {
+         med.mediaset = &m
          return &med
       }
    }
    return nil
 }
 
+type NewsItem struct {
+   ShortName string
+   IstatsLabels struct {
+      CPS_Asset_ID string
+   }
+   Relations []Relation
+}
+
+func NewNewsItem(addr string) (*NewsItem, error) {
+   var buf strings.Builder
+   buf.WriteString("http://walter-producer-cdn.api.bbci.co.uk")
+   buf.WriteString("/content/cps/news/")
+   buf.WriteString(path.Base(addr))
+   req, err := http.NewRequest("GET", buf.String(), nil)
+   if err != nil {
+      return nil, err
+   }
+   LogLevel.Dump(req)
+   res, err := new(http.Transport).RoundTrip(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   item := new(NewsItem)
+   if err := json.NewDecoder(res.Body).Decode(item); err != nil {
+      return nil, err
+   }
+   return item, nil
+}
+
+func (n NewsItem) Base() string {
+   return n.ShortName + "-" + n.IstatsLabels.CPS_Asset_ID
+}
+
 func (n NewsItem) Relation() *Relation {
    for _, rel := range n.Relations {
       if rel.PrimaryType == "bbc.mobile.news.video" {
+         rel.newsItem = &n
          return &rel
       }
    }
@@ -52,6 +109,7 @@ type Relation struct {
    Content struct {
       ExternalID string
    }
+   newsItem *NewsItem
 }
 
 func (r Relation) Mediaset() (*Mediaset, error) {
@@ -69,7 +127,7 @@ func (r Relation) Mediaset() (*Mediaset, error) {
       return nil, err
    }
    defer res.Body.Close()
-   set := new(Mediaset)
+   set := &Mediaset{relation: &r}
    if err := json.NewDecoder(res.Body).Decode(set); err != nil {
       return nil, err
    }
