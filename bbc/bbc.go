7dbbf0f5654cd358633a1c717f5b9464fdfdad3c
Zombo <srpen6@gmail.com>
2022-02-25 18:08:04 -0600

    deleted:    bbc/notfound.go

--- bbc/bbc.go
+++ bbc/bbc.go
@@ -34,42 +34,6 @@ func NewNewsItem(addr string) (*NewsItem, error) {
    return item, nil
 }
 
-func (m Media) Streams() ([]Stream, error) {
-   addr, err := m.address()
-   if err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest("GET", addr, nil)
-   if err != nil {
-      return nil, err
-   }
-   LogLevel.Dump(req)
-   res, err := new(http.Transport).RoundTrip(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   dir, _ := path.Split(addr)
-   forms, err := m3u.Decode(res.Body, dir)
-   if err != nil {
-      return nil, err
-   }
-   var streams []Stream
-   for i, form := range forms {
-      var stream Stream
-      stream.Bandwidth, err = strconv.ParseInt(form["BANDWIDTH"], 10, 64)
-      if err != nil {
-         return nil, err
-      }
-      stream.Codecs = form["CODECS"]
-      stream.ID = int64(i)
-      stream.Resolution = form["RESOLUTION"]
-      stream.URI = form["URI"]
-      streams = append(streams, stream)
-   }
-   return streams, nil
-}
-
 // #EXT-X-STREAM-INF
 type Stream struct {
    ID int64
@@ -144,4 +108,80 @@ func (m Media) Name(item *NewsItem) (string, error) {
       return "", err
    }
    return item.ShortName + "-" + item.IstatsLabels.CPS_Asset_ID + ext, nil
+}
+
+func (m Media) Streams() ([]Stream, error) {
+   var addr string
+   for _, video := range m.Connection {
+      if video.Protocol == "http" {
+         if video.TransferFormat == "hls" {
+            if video.Supplier == "mf_akamai" {
+               addr = video.Href
+            }
+         }
+      }
+   }
+   req, err := http.NewRequest("GET", addr, nil)
+   if err != nil {
+      return nil, err
+   }
+   LogLevel.Dump(req)
+   res, err := new(http.Transport).RoundTrip(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   dir, _ := path.Split(addr)
+   forms, err := m3u.Decode(res.Body, dir)
+   if err != nil {
+      return nil, err
+   }
+   var streams []Stream
+   for i, form := range forms {
+      var stream Stream
+      stream.Bandwidth, err = strconv.ParseInt(form["BANDWIDTH"], 10, 64)
+      if err != nil {
+         return nil, err
+      }
+      stream.Codecs = form["CODECS"]
+      stream.ID = int64(i)
+      stream.Resolution = form["RESOLUTION"]
+      stream.URI = form["URI"]
+      streams = append(streams, stream)
+   }
+   return streams, nil
+}
+
+func (n NewsItem) Media() (*Media, error) {
+   var buf strings.Builder
+   buf.WriteString("http://open.live.bbc.co.uk")
+   buf.WriteString("/mediaselector/6/select/version/2.0/mediaset/pc/vpid/")
+   for _, rel := range n.Relations {
+      if rel.PrimaryType == "bbc.mobile.news.video" {
+         buf.WriteString(rel.Content.ExternalID)
+      }
+   }
+   req, err := http.NewRequest("GET", buf.String(), nil)
+   if err != nil {
+      return nil, err
+   }
+   LogLevel.Dump(req)
+   res, err := new(http.Transport).RoundTrip(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   var mediaset struct {
+      Media []Media
+   }
+   if err := json.NewDecoder(res.Body).Decode(&mediaset); err != nil {
+      return nil, err
+   }
+   var media Media
+   for _, media = range mediaset.Media {
+      if media.Kind == "video" {
+         break
+      }
+   }
+   return &media, nil
 }
