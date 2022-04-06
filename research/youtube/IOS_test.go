9ad3f5ef2ec66a3cace483bd0c60edea154c5253
Zombo <srpen6@gmail.com>
2022-04-05 22:18:59 -0500

    +func TestIos(t *testing.T) {

--- research/youtube/IOS_test.go
+++ research/youtube/IOS_test.go
@@ -5,6 +5,19 @@ import (
    "testing"
 )
 
+func TestIos(t *testing.T) {
+   const (
+      name = "IOS"
+      version = "17.11.34"
+   )
+   res, err := post(name, version)
+   if err != nil {
+      t.Fatal(err)
+   }
+   defer res.Body.Close()
+   fmt.Println(res.Status, name, version)
+}
+
 func TestIosCreator(t *testing.T) {
    const (
       name = "IOS_CREATOR"
@@ -18,10 +31,88 @@ func TestIosCreator(t *testing.T) {
    fmt.Println(res.Status, name, version)
 }
 
-func TestIos(t *testing.T) {
+func TestIosEmbed(t *testing.T) {
    const (
-      name = "IOS"
+      name = "IOS_EMBEDDED_PLAYER"
+      version = "2.0"
+   )
+   res, err := post(name, version)
+   if err != nil {
+      t.Fatal(err)
+   }
+   defer res.Body.Close()
+   fmt.Println(res.Status, name, version)
+}
+
+func TestIosKids(t *testing.T) {
+   const (
+      name = "IOS_KIDS"
+      version = "7.10.3"
+   )
+   res, err := post(name, version)
+   if err != nil {
+      t.Fatal(err)
+   }
+   defer res.Body.Close()
+   fmt.Println(res.Status, name, version)
+}
+
+func TestIosLive(t *testing.T) {
+   const (
+      name = "IOS_LIVE_CREATION_EXTENSION"
       version = "17.11.34"
+   )
+   res, err := post(name, version)
+   if err != nil {
+      t.Fatal(err)
+   }
+   defer res.Body.Close()
+   fmt.Println(res.Status, name, version)
+}
+
+func TestIosMessage(t *testing.T) {
+   const (
+      name = "IOS_MESSAGES_EXTENSION"
+      version = "17.11.34"
+   )
+   res, err := post(name, version)
+   if err != nil {
+      t.Fatal(err)
+   }
+   defer res.Body.Close()
+   fmt.Println(res.Status, name, version)
+}
+
+func TestIosMusic(t *testing.T) {
+   const (
+      name = "IOS_MUSIC"
+      version = "4.70.50"
+   )
+   res, err := post(name, version)
+   if err != nil {
+      t.Fatal(err)
+   }
+   defer res.Body.Close()
+   fmt.Println(res.Status, name, version)
+}
+
+func TestIosProduce(t *testing.T) {
+   const (
+      name = "IOS_PRODUCER"
+      version = "0.1"
+   )
+   res, err := post(name, version)
+   if err != nil {
+      t.Fatal(err)
+   }
+   defer res.Body.Close()
+   fmt.Println(res.Status, name, version)
+}
+
+func TestIosUnplug(t *testing.T) {
+   const (
+      name = "IOS_UNPLUGGED"
+      version = "6.12.1"
    )
    res, err := post(name, version)
    if err != nil {
