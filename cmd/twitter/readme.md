24c0822d20e4ef3ef578d80a5cc0969c27a33d67
Zombo <srpen6@gmail.com>
2022-04-14 08:53:36 -0500

    new file:   twitter/internal/Ended.json

---
+++ cmd/twitter/readme.md
@@ -0,0 +1,8 @@
+# Twitter
+
+~~~
+go run . -c 1DXxyDEZPNdJM -i
+go run . -c 1DXxyDEZPNdJM 
+~~~
+
+https://twitter.com/i/spaces/1DXxyDEZPNdJM
--- cmd/twitter/twitter.go
+++ cmd/twitter/twitter.go
@@ -2,11 +2,62 @@ package main
 
 import (
    "fmt"
+   "github.com/89z/format"
    "github.com/89z/format/hls"
    "github.com/89z/mech/twitter"
+   "io"
    "net/http"
    "os"
 )
+
+func doSpace(id string, info bool) error {
+   guest, err := twitter.NewGuest()
+   if err != nil {
+      return err
+   }
+   space, err := guest.AudioSpace(id)
+   if err != nil {
+      return err
+   }
+   if info {
+      fmt.Println(space)
+   } else {
+      source, err := guest.Source(space)
+      if err != nil {
+         return err
+      }
+      fmt.Println("GET", source.Location)
+      res, err := http.Get(source.Location)
+      if err != nil {
+         return err
+      }
+      defer res.Body.Close()
+      seg, err := hls.NewScanner(res.Body).Segment(res.Request.URL)
+      if err != nil {
+         return err
+      }
+      file, err := os.Create(space.Base() + seg.Ext())
+      if err != nil {
+         return err
+      }
+      defer file.Close()
+      pro := format.ProgressChunks(file, len(seg.Info))
+      for _, info := range seg.Info {
+         res, err := http.Get(info.URI.String())
+         if err != nil {
+            return err
+         }
+         pro.AddChunk(res.ContentLength)
+         if _, err := io.Copy(pro, res.Body); err != nil {
+            return err
+         }
+         if err := res.Body.Close(); err != nil {
+            return err
+         }
+      }
+   }
+   return nil
+}
 
 func doStatus(id, bitrate int64, info bool) error {
    guest, err := twitter.NewGuest()
@@ -42,54 +93,6 @@ func doStatus(id, bitrate int64, info bool) error {
                   return err
                }
             }
-         }
-      }
-   }
-   return nil
-}
-
-func doSpace(id string, info bool) error {
-   guest, err := twitter.NewGuest()
-   if err != nil {
-      return err
-   }
-   space, err := guest.AudioSpace(id)
-   if err != nil {
-      return err
-   }
-   if info {
-      fmt.Println(space)
-   } else {
-      source, err := guest.Source(space)
-      if err != nil {
-         return err
-      }
-      fmt.Println("GET", source.Location)
-      res, err := http.Get(source.Location)
-      if err != nil {
-         return err
-      }
-      defer res.Body.Close()
-      seg, err := hls.NewSegment(res.Request.URL, res.Body)
-      if err != nil {
-         return err
-      }
-      file, err := os.Create(space.Base() + seg.Ext())
-      if err != nil {
-         return err
-      }
-      defer file.Close()
-      for i, info := range seg.Info {
-         fmt.Print(seg.Progress(i))
-         res, err := http.Get(info.URI.String())
-         if err != nil {
-            return err
-         }
-         if _, err := file.ReadFrom(res.Body); err != nil {
-            return err
-         }
-         if err := res.Body.Close(); err != nil {
-            return err
          }
       }
    }
