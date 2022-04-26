f1a047c29b8e27007d645289f55ab7925d99a401
Zombo <srpen6@gmail.com>
2022-04-26 11:30:22 -0500

    deleted:    research/channel4/channel4.py

--- research/channel4/channel4.go
+++ research/channel4/channel4.go
@@ -11,20 +11,44 @@ import (
    "strings"
 )
 
-const buildInfo = "Xiaomi/nitrogen/nitrogen:10/QKQ1.190910.002" +
-   "/V12.0.1.0.QEDMIXM:user/release-keys"
+type Header struct {
+   // this can be empty, but must be included
+   BuildInfo string `json:"buildInfo"`
+   PSSH string `json:"pssh"`
+}
 
-const licenseURL = "https://c4.eme.lp.aws.redbeemedia.com" +
-   "/wvlicenceproxy-service/widevine/acquire"
+func (h Header) Payload(token []byte) (*Payload, error) {
+   pssh := new(bytes.Buffer)
+   err := json.NewEncoder(pssh).Encode(h)
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest("POST", "http://getwvkeys.cc/pssh", pssh)
+   if err != nil {
+      return nil, err
+   }
+   req.Header.Set("Content-Type", "application/json")
+   LogLevel.Dump(req)
+   res, err := new(http.Transport).RoundTrip(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   var pay Payload
+   if err := json.Unmarshal(token, &pay); err != nil {
+      return nil, err
+   }
+   message, err := io.ReadAll(res.Body)
+   if err != nil {
+      return nil, err
+   }
+   pay.Message = base64.StdEncoding.EncodeToString(message)
+   return &pay, nil
+}
 
 const uuid = "edef8ba9-79d6-4ace-a3c8-27dcd51d21ed"
 
 var LogLevel format.LogLevel
-
-type Header struct {
-   BuildInfo string `json:"buildInfo"`
-   PSSH string `json:"pssh"`
-}
 
 func NewHeader(kid string) (*Header, error) {
    decode := func(s string) ([]byte, error) {
@@ -45,41 +69,8 @@ func NewHeader(kid string) (*Header, error) {
    pssh = append(pssh, 0, 0, 0, 0x12, 0x12, 0x10)
    pssh = append(pssh, dKID...)
    var head Header
-   head.BuildInfo = buildInfo
    head.PSSH = base64.StdEncoding.EncodeToString(pssh)
    return &head, nil
-}
-
-func (h Header) Payload(token []byte) (*Payload, error) {
-   pssh := new(bytes.Buffer)
-   err := json.NewEncoder(pssh).Encode(h)
-   if err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest("POST", "http://getwvkeys.cc/pssh", pssh)
-   if err != nil {
-      return nil, err
-   }
-   req.Header.Set("Content-Type", "application/json")
-   LogLevel.Dump(req)
-   res, err := new(http.Transport).RoundTrip(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   if res.StatusCode != http.StatusOK {
-      return nil, errorString(res.Status)
-   }
-   var pay Payload
-   if err := json.Unmarshal(token, &pay); err != nil {
-      return nil, err
-   }
-   message, err := io.ReadAll(res.Body)
-   if err != nil {
-      return nil, err
-   }
-   pay.Message = base64.StdEncoding.EncodeToString(message)
-   return &pay, nil
 }
 
 type Payload struct {
@@ -90,61 +81,17 @@ type Payload struct {
    } `json:"video"`
 }
 
-func (p Payload) Widevine() (*Widevine, error) {
-   payload := new(bytes.Buffer)
-   err := json.NewEncoder(payload).Encode(p)
-   if err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest("POST", licenseURL, payload)
-   if err != nil {
-      return nil, err
-   }
-   req.Header = http.Header{
-      "Accept-Language": {"en-US,en;q=0.9"},
-      "Cache-Control": {"no-cache"},
-      "Connection": {"keep-alive"},
-      "Content-Type": {"application/json"},
-      "Origin": {"https://www.channel4.com"},
-      "Pragma": {"no-cache"},
-      "Referer": {"https://www.channel4.com/"},
-      "Sec-Fetch-Dest": {"empty"},
-      "Sec-Fetch-Mode": {"cors"},
-      "Sec-Fetch-Site": {"cross-site"},
-      "User-Agent": {"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36"},
-      "sec-ch-ua-mobile": {"?0"},
-      "sec-ch-ua-platform": {"Windows"},
-   }
-   LogLevel.Dump(req)
-   res, err := new(http.Transport).RoundTrip(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   if res.StatusCode != http.StatusOK {
-      return nil, errorString(res.Status)
-   }
-   vine := new(Widevine)
-   if err := json.NewDecoder(res.Body).Decode(vine); err != nil {
-      return nil, err
-   }
-   return vine, nil
-}
-
-type errorString string
-
-func (e errorString) Error() string {
-   return string(e)
-}
-
 type Widevine struct {
    License string
 }
 
-func (w Widevine) decrypt(h Header) ([]byte, error) {
+const licenseURL = "https://c4.eme.lp.aws.redbeemedia.com" +
+   "/wvlicenceproxy-service/widevine/acquire"
+
+func (w Widevine) Decrypt(h *Header) ([]byte, error) {
    buf := new(bytes.Buffer)
    err := json.NewEncoder(buf).Encode(map[string]string{
-      "buildInfo": buildInfo,
+      "buildInfo": "",
       "headers": "",
       "license": licenseURL,
       "license_response": w.License,
@@ -165,4 +112,28 @@ func (w Widevine) decrypt(h Header) ([]byte, error) {
    }
    defer res.Body.Close()
    return io.ReadAll(res.Body)
+}
+
+func (p Payload) Widevine() (*Widevine, error) {
+   payload := new(bytes.Buffer)
+   err := json.NewEncoder(payload).Encode(p)
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest("POST", licenseURL, payload)
+   if err != nil {
+      return nil, err
+   }
+   req.Header.Set("Content-Type", "application/json")
+   LogLevel.Dump(req)
+   res, err := new(http.Transport).RoundTrip(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   vine := new(Widevine)
+   if err := json.NewDecoder(res.Body).Decode(vine); err != nil {
+      return nil, err
+   }
+   return vine, nil
 }
--- research/channel4/channel4_test.go
+++ research/channel4/channel4_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 var token = []byte(`
-{"request_id":5273616,"token":"aWRZZ3dUeFHXY-QtzKQAUazrQXa6LhiCFy5gqhV6fgeDlcSKEFH2qqXB_3zh5hCVE7_iZxKVTt43zq4uZVROavSFmmZlRIqvkVp_hA4vFKoj7LgbDTShkPAuPwzL1aNe0WH6PAgLSQx4jXU37NJoBP4U6gGNMEhF","video":{"type":"ondemand","url":"https://ak-jos-c4assets-com.akamaized.net/CH4_44_7_900_18926001001003_001/CH4_44_7_900_18926001001003_001_J01.ism/stream.mpd?c3.ri=13636097564397426568&mpd_segment_template=time&filter=%28type%3D%3D%22video%22%26%26%28%28DisplayHeight%3E%3D288%29%26%26%28systemBitrate%3C4800000%29%29%29%7C%7Ctype%21%3D%22video%22&ts=1650988156&e=600&st=M180f_umcmSyVCcECTX4MAkzRjY7uvNGp8ELT0GYkTo"},"message":"CAQ="}
+{"request_id":5273616,"token":"RmVycnNYTFVrXrl_6fPWvGgZOzj0h2U5X_TJv2BkerbH4CRhndeazwutTIT-HWs-c8Yn_zpvMEViC1wOOAel8nq_cjFXZITMgHkYtwbKZJsmHvkfPot44--PcU4uBtKYpTQwRy3odNLa4q5BCrKcsYmWcH5iyOeV","video":{"type":"ondemand","url":"https://ak-jos-c4assets-com.akamaized.net/CH4_44_7_900_18926001001003_001/CH4_44_7_900_18926001001003_001_J01.ism/stream.mpd?c3.ri=13630749547086512690&mpd_segment_template=time&filter=%28type%3D%3D%22video%22%26%26%28%28DisplayHeight%3E%3D288%29%26%26%28systemBitrate%3C4800000%29%29%29%7C%7Ctype%21%3D%22video%22&ts=1650990334&e=600&st=PEkooWh_S71X0m1tqNXdAjDeAd16bOHd70bAdjJ7mDM"},"message":"CAQ="}
 `)
 
 const kid = "00000000-0000-0000-0000-000004246624"
@@ -25,4 +25,10 @@ func TestWidevine(t *testing.T) {
       t.Fatal(err)
    }
    fmt.Printf("%+v\n", widevine)
+   return
+   key, err := widevine.Decrypt(header)
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Println(string(key))
 }
