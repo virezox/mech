70abeb844afd96c37bdcdd3782ea747abd75b984
Zombo <srpen6@gmail.com>
2022-04-25 21:26:55 -0500

    +func createPSSH(kid string) (string, error) {

--- research/channel4/channel4.go
+++ research/channel4/channel4.go
@@ -2,18 +2,23 @@ package channel4
 
 import (
    "encoding/base64"
+   "encoding/hex"
    "strings"
 )
 
-func createPSSH(kid string) string {
-   buf := []byte{
-      0,    0,    0,    '2', 'p',   's',  's', 'h',
-      0,    0,    0,    0,
-      0xed, 0xef, 0x8b, 0xa9, 0x79, 0xd6, 0x4a, 0xce,
-      0xa3, 0xc8, 0x27, 0xdc, 0xd5, 0x1d, 0x21, 0xed,
-      0,    0,    0,    0x12, 0x12, 0x10,
+func createPSSH(kid string) (string, error) {
+   a := []byte{0, 0, 0, '2', 'p', 's', 's', 'h', 0, 0, 0, 0}
+   b, err := hex.DecodeString("edef8ba979d64acea3c827dcd51d21ed")
+   if err != nil {
+      return "", err
    }
+   a = append(a, b...)
+   a = append(a, 0, 0, 0, 0x12, 0x12, 0x10)
    kid = strings.ReplaceAll(kid, "-", "")
-   buf = append(buf, kid...)
-   return base64.StdEncoding.EncodeToString(buf)
+   c, err := hex.DecodeString(kid)
+   if err != nil {
+      return "", err
+   }
+   a = append(a, c...)
+   return base64.StdEncoding.EncodeToString(a), nil
 }
--- research/channel4/channel4_test.go
+++ research/channel4/channel4_test.go
@@ -1,13 +1,20 @@
 package channel4
 
 import (
-   "fmt"
    "testing"
 )
 
-const kid = "00000000-0000-0000-0000-000004246624"
+const (
+   in = "00000000-0000-0000-0000-000004246624"
+   out = "AAAAMnBzc2gAAAAA7e+LqXnWSs6jyCfc1R0h7QAAABISEAAAAAAAAAAAAAAAAAQkZiQ="
+)
 
 func TestPSSH(t *testing.T) {
-   pssh := createPSSH(kid)
-   fmt.Println(pssh)
+   pssh, err := createPSSH(in)
+   if err != nil {
+      t.Fatal(err)
+   }
+   if pssh != out {
+      t.Fatal(pssh)
+   }
 }
