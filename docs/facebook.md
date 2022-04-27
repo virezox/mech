487120a68c19f00e5c3a36c0b985533183f99167
Zombo <srpen6@gmail.com>
2022-04-27 10:08:15 -0500

    new file:   vimeo/video.go

---
+++ docs/facebook.md
@@ -0,0 +1,3 @@
+# Facebook
+
+No image permalinks
--- docs/topics.go
+++ docs/topics.go
@@ -13,7 +13,6 @@ const addr = "https://api.github.com/repos/89z/mech/topics"
 
 var names = []string{
    "youtube",
-   "facebook",
    "instagram",
    "tiktok",
    "twitter",
