ec69aa2eb7c09196b1d829ed047fcb8c6dcc2513
Zombo <srpen6@gmail.com>
2022-05-01 15:14:40 -0500

    +   Duration int64

--- cbc/cbc.go
+++ cbc/cbc.go
@@ -7,47 +7,26 @@ import (
    "strings"
 )
 
-type Asset struct {
-   ID string
-   PlaySession struct {
-      MediaID string
-      URL string
-   }
-}
-
-type Media struct {
-   Message string
-   URL string
-}
-
 const forwardedFor = "99.224.0.0"
 
-func (p Profile) Media(asset *Asset) (*Media, error) {
-   req, err := http.NewRequest("GET", asset.PlaySession.URL, nil)
-   if err != nil {
-      return nil, err
-   }
-   req.Header = http.Header{
-      "X-Claims-Token": {p.ClaimsToken},
-      "X-Forwarded-For": {forwardedFor},
-   }
-   LogLevel.Dump(req)
-   res, err := new(http.Transport).RoundTrip(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   med := new(Media)
-   if err := json.NewDecoder(res.Body).Decode(med); err != nil {
-      return nil, err
-   }
-   return med, nil
-}
+var LogLevel format.LogLevel
 
 // gem.cbc.ca/media/downton-abbey/s01e05
 func GetID(addr string) string {
    _, after, _ := strings.Cut(addr, "/media/")
    return after
+}
+
+type Asset struct {
+   AirDate int64
+   Duration int64
+   ID string
+   PlaySession struct {
+      MediaID string
+      URL string
+   }
+   Series string
+   Title string
 }
 
 func NewAsset(id string) (*Asset, error) {
@@ -72,4 +51,29 @@ func NewAsset(id string) (*Asset, error) {
    return asset, nil
 }
 
-var LogLevel format.LogLevel
+type Media struct {
+   Message string
+   URL string
+}
+
+func (p Profile) Media(asset *Asset) (*Media, error) {
+   req, err := http.NewRequest("GET", asset.PlaySession.URL, nil)
+   if err != nil {
+      return nil, err
+   }
+   req.Header = http.Header{
+      "X-Claims-Token": {p.ClaimsToken},
+      "X-Forwarded-For": {forwardedFor},
+   }
+   LogLevel.Dump(req)
+   res, err := new(http.Transport).RoundTrip(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   med := new(Media)
+   if err := json.NewDecoder(res.Body).Decode(med); err != nil {
+      return nil, err
+   }
+   return med, nil
+}
--- cbc/profile.go
+++ cbc/profile.go
@@ -8,74 +8,7 @@ import (
    "net/url"
 )
 
-type OverTheTop struct {
-   AccessToken string
-}
-
-func (o OverTheTop) Profile() (*Profile, error) {
-   req, err := http.NewRequest(
-      "GET", "https://services.radio-canada.ca/ott/cbc-api/v2/profile", nil,
-   )
-   if err != nil {
-      return nil, err
-   }
-   req.Header.Set("OTT-Access-Token", o.AccessToken)
-   LogLevel.Dump(req)
-   res, err := new(http.Transport).RoundTrip(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   pro := new(Profile)
-   if err := json.NewDecoder(res.Body).Decode(pro); err != nil {
-      return nil, err
-   }
-   return pro, nil
-}
-
-type WebToken struct {
-   Signature string
-}
-
-func (w WebToken) OverTheTop() (*OverTheTop, error) {
-   buf := new(bytes.Buffer)
-   err := json.NewEncoder(buf).Encode(map[string]string{
-      "jwt": w.Signature,
-   })
-   if err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest(
-      "POST", "https://services.radio-canada.ca/ott/cbc-api/v2/token", buf,
-   )
-   if err != nil {
-      return nil, err
-   }
-   LogLevel.Dump(req)
-   res, err := new(http.Transport).RoundTrip(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   top := new(OverTheTop)
-   if err := json.NewDecoder(res.Body).Decode(top); err != nil {
-      return nil, err
-   }
-   return top, nil
-}
-
-type Profile struct {
-   Tier string
-   ClaimsToken string
-}
-
-func (p Profile) Create(elem ...string) error {
-   return format.Create(p, elem...)
-}
-
-func OpenProfile(elem ...string) (*Profile, error) {
-   return format.Open[Profile](elem...)
-}
+const apiKey = "3f4beddd-2061-49b0-ae80-6f1f2ed65b37"
 
 type Login struct {
    Access_Token string
@@ -137,4 +70,71 @@ func (l Login) WebToken() (*WebToken, error) {
    return web, nil
 }
 
-const apiKey = "3f4beddd-2061-49b0-ae80-6f1f2ed65b37"
+type OverTheTop struct {
+   AccessToken string
+}
+
+func (o OverTheTop) Profile() (*Profile, error) {
+   req, err := http.NewRequest(
+      "GET", "https://services.radio-canada.ca/ott/cbc-api/v2/profile", nil,
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.Header.Set("OTT-Access-Token", o.AccessToken)
+   LogLevel.Dump(req)
+   res, err := new(http.Transport).RoundTrip(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   pro := new(Profile)
+   if err := json.NewDecoder(res.Body).Decode(pro); err != nil {
+      return nil, err
+   }
+   return pro, nil
+}
+
+type Profile struct {
+   Tier string
+   ClaimsToken string
+}
+
+func OpenProfile(elem ...string) (*Profile, error) {
+   return format.Open[Profile](elem...)
+}
+
+func (p Profile) Create(elem ...string) error {
+   return format.Create(p, elem...)
+}
+
+type WebToken struct {
+   Signature string
+}
+
+func (w WebToken) OverTheTop() (*OverTheTop, error) {
+   buf := new(bytes.Buffer)
+   err := json.NewEncoder(buf).Encode(map[string]string{
+      "jwt": w.Signature,
+   })
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest(
+      "POST", "https://services.radio-canada.ca/ott/cbc-api/v2/token", buf,
+   )
+   if err != nil {
+      return nil, err
+   }
+   LogLevel.Dump(req)
+   res, err := new(http.Transport).RoundTrip(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   top := new(OverTheTop)
+   if err := json.NewDecoder(res.Body).Decode(top); err != nil {
+      return nil, err
+   }
+   return top, nil
+}
