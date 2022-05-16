f66fecb567b35499836c099b204af4f51f68f6b9
Zombo <srpen6@gmail.com>
2022-05-15 19:41:21 -0500

    new file:   research/paramount/res1.json

--- research/go.mod
+++ research/go.mod
@@ -3,8 +3,9 @@ module research
 go 1.18
 
 require (
-	github.com/89z/format v1.33.7
+	github.com/89z/format v1.34.1
 	github.com/chris124567/hulu v0.0.0-20220101232151-8c095fc96aa9
+	lukechampine.com/frand v1.4.2
 )
 
 require (
@@ -12,5 +13,4 @@ require (
 	github.com/aead/cmac v0.0.0-20160719120800-7af84192f0b1 // indirect
 	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
-	lukechampine.com/frand v1.4.2 // indirect
 )
--- research/paramount/main.go
+++ research/paramount/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
    "bufio"
+   "crypto/rsa"
+   "crypto/sha1"
+   "crypto/x509"
    "encoding/json"
+   "encoding/pem"
+   "errors"
    "github.com/89z/format/protobuf"
-   "github.com/chris124567/hulu/widevine"
    "io"
    "net/http"
    "os"
@@ -15,43 +19,56 @@ func main() {
    if err != nil {
       panic(err)
    }
-   id, err := os.ReadFile("ignore/device_client_id_blob")
+   cdm, err := NewCDM(key)
    if err != nil {
       panic(err)
    }
-   file, err := os.Open("ignore/stream.mpd")
+   licenseResponse, err := readResponse("ignore/res2.txt")
    if err != nil {
       panic(err)
    }
-   defer file.Close()
-   initData, err := widevine.InitDataFromMPD(file)
-   if err != nil {
-      panic(err)
-   }
-   cdm, err := widevine.NewCDM(string(key), id, initData)
-   if err != nil {
-      panic(err)
-   }
-   _ = cdm
-   // piece of shit server sends more data than the Content-Length, as least
-   // with the response. So we need to look at the headers too.
-   licenseRequest, err := readRequest("ignore/req.txt")
-   if err != nil {
-      panic(err)
-   }
-   licenseResponse, err := readResponse("ignore/res.txt")
-   if err != nil {
-      panic(err)
-   }
-   _ = licenseResponse
-   //keys, err := cdm.GetLicenseKeys(licenseRequest, licenseResponse)
-   licenseRequestParsed, err := protobuf.Unmarshal(licenseRequest)
+   license, err := protobuf.Unmarshal(licenseResponse)
    if err != nil {
       panic(err)
    }
    enc := json.NewEncoder(os.Stdout)
    enc.SetIndent("", " ")
-   enc.Encode(licenseRequestParsed)
+   enc.Encode(license)
+   licenseSessionKey, err := license.GetBytes(4)
+   if err != nil {
+      panic(err)
+   }
+   if _, err := rsa.DecryptOAEP(
+      sha1.New(),
+      nil,
+      cdm.privateKey,
+      licenseSessionKey,
+      nil,
+   ); err != nil {
+      panic(err)
+   }
+}
+
+type CDM struct {
+   privateKey *rsa.PrivateKey
+}
+
+func NewCDM(privateKey []byte) (*CDM, error) {
+   block, _ := pem.Decode(privateKey)
+   if block == nil || (block.Type != "PRIVATE KEY" && block.Type != "RSA PRIVATE KEY") {
+      return nil, errors.New("failed to decode device private key")
+   }
+   keyParsed, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+   if err != nil {
+      pcks8Key, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+      if err != nil {
+         return nil, err
+      }
+      keyParsed = pcks8Key.(*rsa.PrivateKey)
+   }
+   return &CDM{
+      privateKey: keyParsed,
+   }, nil
 }
 
 func readResponse(s string) ([]byte, error) {
