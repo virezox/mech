ba87ec633e0036bfc0b6bead64f4d8716a4ef5ed
Zombo <srpen6@gmail.com>
2022-05-22 14:21:52 -0500

    new file:   paramount/preview.json

--- paramount/old.go
+++ paramount/old.go
@@ -6,13 +6,9 @@ import (
    "encoding/base64"
    "encoding/hex"
    "encoding/json"
-   "encoding/xml"
-   "errors"
    "github.com/89z/format"
-   "github.com/89z/mech"
    "net/http"
    "net/url"
-   "strconv"
    "strings"
 )
 
@@ -22,99 +18,6 @@ const (
 )
 
 var LogLevel format.LogLevel
-
-func (m Media) Video() (*Video, error) {
-   for _, vid := range m.Body.Seq.Video {
-      return &vid, nil
-   }
-   // No AssetType/ProtectionScheme/Format Matches
-   return nil, errors.New(m.Body.Seq.Ref.Title)
-}
-
-type Media struct {
-   Body struct {
-      Seq  struct {
-         Ref struct {
-            Title string `xml:"title,attr"`
-         } `xml:"ref"`
-         Video []Video `xml:"video"`
-      } `xml:"seq"`
-   } `xml:"body"`
-}
-
-type Video struct {
-   Title string `xml:"title,attr"`
-   Src string `xml:"src,attr"`
-   Param []struct {
-      Name string `xml:"name,attr"`
-      Value string `xml:"value,attr"`
-   } `xml:"param"`
-}
-
-func (v Video) Base() string {
-   var buf strings.Builder
-   buf.WriteString(v.Title)
-   buf.WriteByte('-')
-   buf.WriteString(v.SeasonNumber())
-   buf.WriteByte('-')
-   buf.WriteString(v.EpisodeNumber())
-   return mech.Clean(buf.String())
-}
-
-func (v Video) EpisodeNumber() string {
-   for _, par := range v.Param {
-      if par.Name == "EpisodeNumber" {
-         return par.Value
-      }
-   }
-   return ""
-}
-
-func (v Video) SeasonNumber() string {
-   for _, par := range v.Param {
-      if par.Name == "SeasonNumber" {
-         return par.Value
-      }
-   }
-   return ""
-}
-
-func HLS(guid string) (*Media, error) {
-   return newMedia(guid, "MPEG4,M3U", "StreamPack")
-}
-
-func DASH(guid string) (*Media, error) {
-   return newMedia(guid, "MPEG-DASH", "DASH_CENC")
-}
-
-func newMedia(guid, formats, asset string) (*Media, error) {
-   buf := []byte("https://link.theplatform.com/s/")
-   buf = append(buf, sid...)
-   buf = append(buf, "/media/guid/"...)
-   buf = strconv.AppendInt(buf, aid, 10)
-   buf = append(buf, '/')
-   buf = append(buf, guid...)
-   req, err := http.NewRequest("GET", string(buf), nil)
-   if err != nil {
-      return nil, err
-   }
-   req.URL.RawQuery = url.Values{
-      "assetTypes": {asset},
-      "format": {"SMIL"},
-      "formats": {formats},
-   }.Encode()
-   LogLevel.Dump(req)
-   res, err := new(http.Transport).RoundTrip(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   med := new(Media)
-   if err := xml.NewDecoder(res.Body).Decode(med); err != nil {
-      return nil, err
-   }
-   return med, nil
-}
 
 const (
    aes_key = "302a6a0d70a7e9b967f91d39fef3e387816e3095925ae4537bce96063311f9c5"
@@ -153,11 +56,6 @@ func pad(b []byte) []byte {
    return b
 }
 
-type Session struct {
-   URL string
-   LS_Session string
-}
-
 func NewSession(contentID string) (*Session, error) {
    token, err := newToken()
    if err != nil {
@@ -189,4 +87,9 @@ func (s Session) Header() http.Header {
    head := make(http.Header)
    head.Set("Authorization", "Bearer " + s.LS_Session)
    return head
+}
+
+type Session struct {
+   URL string
+   LS_Session string
 }
--- paramount/paramount.go
+++ paramount/paramount.go
@@ -1,6 +1,8 @@
 package paramount
 
 import (
+   "encoding/json"
+   "github.com/89z/mech"
    "net/http"
    "net/url"
    "strconv"
@@ -27,7 +29,50 @@ func (a Address) String() string {
    return string(buf)
 }
 
-func (a Address) media(guid, formats, asset string) (*url.URL, error) {
+func (p Preview) Base() string {
+   var buf []byte
+   buf = append(buf, p.Title...)
+   buf = append(buf, '-')
+   buf = strconv.AppendInt(buf, p.SeasonNumber, 10)
+   buf = append(buf, '-')
+   buf = append(buf, p.EpisodeNumber...)
+   return mech.Clean(string(buf))
+}
+
+type Preview struct {
+   Title string
+   SeasonNumber int64 `json:"cbs$SeasonNumber"`
+   EpisodeNumber string `json:"cbs$EpisodeNumber"`
+}
+
+func (a Address) Preview() (*Preview, error) {
+   req, err := http.NewRequest("GET", a.String(), nil)
+   if err != nil {
+      return nil, err
+   }
+   req.URL.RawQuery = "format=preview"
+   LogLevel.Dump(req)
+   res, err := new(http.Transport).RoundTrip(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   prev := new(Preview)
+   if err := json.NewDecoder(res.Body).Decode(prev); err != nil {
+      return nil, err
+   }
+   return prev, nil
+}
+
+func (a Address) HLS(guid string) (*url.URL, error) {
+   return a.location("MPEG4,M3U", "StreamPack")
+}
+
+func (a Address) DASH(guid string) (*url.URL, error) {
+   return a.location("MPEG-DASH", "DASH_CENC")
+}
+
+func (a Address) location(formats, asset string) (*url.URL, error) {
    req, err := http.NewRequest("HEAD", a.String(), nil)
    if err != nil {
       return nil, err
