aa8c4910c626997023a5491ea39c0b0ab2152262
Zombo <srpen6@gmail.com>
2022-06-09 20:21:38 -0500

    +func main() {

--- research/cmd/roku/roku.go
+++ research/cmd/roku/roku.go
@@ -15,75 +15,6 @@ import (
    "path/filepath"
 )
 
-func (d downloader) HLS(bandwidth int64) error {
-   video, err := d.Content.HLS()
-   if err != nil {
-      return err
-   }
-   fmt.Println("GET", video.URL)
-   res, err := http.Get(video.URL)
-   if err != nil {
-      return err
-   }
-   defer res.Body.Close()
-   master, err := hls.NewScanner(res.Body).Master()
-   if err != nil {
-      return err
-   }
-   stream := master.Streams.GetBandwidth(bandwidth)
-   if !d.info {
-      addr, err := res.Request.URL.Parse(stream.RawURI)
-      if err != nil {
-         return err
-      }
-      return downloadHLS(addr, d.Base())
-   }
-   fmt.Println(d.Content)
-   for _, each := range master.Streams {
-      if each.Bandwidth == stream.Bandwidth {
-         fmt.Print("!")
-      }
-      fmt.Println(each)
-   }
-   return nil
-}
-
-func downloadHLS(addr *url.URL, base string) error {
-   fmt.Println("GET", addr)
-   res, err := http.Get(addr.String())
-   if err != nil {
-      return err
-   }
-   defer res.Body.Close()
-   seg, err := hls.NewScanner(res.Body).Segment()
-   if err != nil {
-      return err
-   }
-   file, err := os.Create(base + hls.TS)
-   if err != nil {
-      return err
-   }
-   defer file.Close()
-   pro := format.ProgressChunks(file, len(seg.Clear))
-   for _, clear := range seg.Clear {
-      addr, err := res.Request.URL.Parse(clear)
-      if err != nil {
-         return err
-      }
-      res, err := http.Get(addr.String())
-      if err != nil {
-         return err
-      }
-      pro.AddChunk(res.ContentLength)
-      if _, err := io.Copy(pro, res.Body); err != nil {
-         return err
-      }
-      if err := res.Body.Close(); err != nil {
-         return err
-      }
-   }
-   return nil
-}
 func main() {
    home, err := os.UserHomeDir()
    if err != nil {
@@ -145,6 +76,7 @@ func main() {
       flag.Usage()
    }
 }
+
 type downloader struct {
    *dash.Period
    *roku.Content
@@ -268,3 +200,74 @@ func (d *downloader) download(band int64, fn dash.PeriodFunc) error {
    }
    return nil
 }
+func (d downloader) HLS(bandwidth int64) error {
+   video, err := d.Content.HLS()
+   if err != nil {
+      return err
+   }
+   fmt.Println("GET", video.URL)
+   res, err := http.Get(video.URL)
+   if err != nil {
+      return err
+   }
+   defer res.Body.Close()
+   master, err := hls.NewScanner(res.Body).Master()
+   if err != nil {
+      return err
+   }
+   stream := master.Streams.GetBandwidth(bandwidth)
+   if !d.info {
+      addr, err := res.Request.URL.Parse(stream.RawURI)
+      if err != nil {
+         return err
+      }
+      return downloadHLS(addr, d.Base())
+   }
+   fmt.Println(d.Content)
+   for _, each := range master.Streams {
+      if each.Bandwidth == stream.Bandwidth {
+         fmt.Print("!")
+      }
+      fmt.Println(each)
+   }
+   return nil
+}
+
+func downloadHLS(addr *url.URL, base string) error {
+   fmt.Println("GET", addr)
+   res, err := http.Get(addr.String())
+   if err != nil {
+      return err
+   }
+   defer res.Body.Close()
+   seg, err := hls.NewScanner(res.Body).Segment()
+   if err != nil {
+      return err
+   }
+   file, err := os.Create(base + hls.TS)
+   if err != nil {
+      return err
+   }
+   defer file.Close()
+   pro := format.ProgressChunks(file, len(seg.Clear))
+   for _, clear := range seg.Clear {
+      addr, err := res.Request.URL.Parse(clear)
+      if err != nil {
+         return err
+      }
+      res, err := http.Get(addr.String())
+      if err != nil {
+         return err
+      }
+      pro.AddChunk(res.ContentLength)
+      if _, err := io.Copy(pro, res.Body); err != nil {
+         return err
+      }
+      if err := res.Body.Close(); err != nil {
+         return err
+      }
+   }
+   return nil
+}
+
+
