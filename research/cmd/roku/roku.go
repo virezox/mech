c3f3377e112f2d274328df3ab93722b1cfb285a1
Zombo <srpen6@gmail.com>
2022-06-09 19:56:57 -0500

    new file:   research/cmd/roku/roku.go

--- research/cmd/roku/roku.go
+++ research/cmd/roku/roku.go
@@ -7,23 +7,95 @@ import (
    "github.com/89z/format/dash"
    "github.com/89z/format/hls"
    "github.com/89z/mech"
-   "github.com/89z/mech/paramount"
+   "github.com/89z/mech/roku"
+   "io"
    "net/http"
    "net/url"
    "os"
    "path/filepath"
-   "sort"
 )
 
+func (d downloader) HLS(bandwidth int64) error {
+   video, err := d.Content.HLS()
+   if err != nil {
+      return err
+   }
+   fmt.Println("GET", video.URL)
+   res, err := http.Get(video.URL)
+   if err != nil {
+      return err
+   }
+   defer res.Body.Close()
+   master, err := hls.NewScanner(res.Body).Master()
+   if err != nil {
+      return err
+   }
+   stream := master.Streams.GetBandwidth(bandwidth)
+   if !d.info {
+      addr, err := res.Request.URL.Parse(stream.RawURI)
+      if err != nil {
+         return err
+      }
+      return downloadHLS(addr, d.Base())
+   }
+   fmt.Println(d.Content)
+   for _, each := range master.Streams {
+      if each.Bandwidth == stream.Bandwidth {
+         fmt.Print("!")
+      }
+      fmt.Println(each)
+   }
+   return nil
+}
+
+func downloadHLS(addr *url.URL, base string) error {
+   fmt.Println("GET", addr)
+   res, err := http.Get(addr.String())
+   if err != nil {
+      return err
+   }
+   defer res.Body.Close()
+   seg, err := hls.NewScanner(res.Body).Segment()
+   if err != nil {
+      return err
+   }
+   file, err := os.Create(base + hls.TS)
+   if err != nil {
+      return err
+   }
+   defer file.Close()
+   pro := format.ProgressChunks(file, len(seg.Clear))
+   for _, clear := range seg.Clear {
+      addr, err := res.Request.URL.Parse(clear)
+      if err != nil {
+         return err
+      }
+      res, err := http.Get(addr.String())
+      if err != nil {
+         return err
+      }
+      pro.AddChunk(res.ContentLength)
+      if _, err := io.Copy(pro, res.Body); err != nil {
+         return err
+      }
+      if err := res.Body.Close(); err != nil {
+         return err
+      }
+   }
+   return nil
+}
 func main() {
    home, err := os.UserHomeDir()
    if err != nil {
       panic(err)
    }
    var down downloader
+   // a
+   var address string
+   flag.StringVar(&address, "a", "", "address")
    // b
-   var guid string
-   flag.StringVar(&guid, "b", "", "GUID")
+   var id string
+   flag.StringVar(&id, "b", "", "ID")
    // c
    down.client = filepath.Join(home, "mech/client_id.bin")
    flag.StringVar(&down.client, "c", down.client, "client ID")
@@ -31,13 +103,13 @@ func main() {
    var isDASH bool
    flag.BoolVar(&isDASH, "d", false, "DASH download")
    // f
-   // paramountplus.com/shows/video/x6XrF8A_tiSDRwc4Rt349KFKnCZ8QmtY
+   // therokuchannel.roku.com/watch/597a64a4a25c5bf6af4a8c7053049a6f
    var video int64
-   flag.Int64Var(&video, "f", 1611000, "video bandwidth")
+   flag.Int64Var(&video, "f", 1920832, "video bandwidth")
    // g
-   // paramountplus.com/shows/video/x6XrF8A_tiSDRwc4Rt349KFKnCZ8QmtY
+   // therokuchannel.roku.com/watch/597a64a4a25c5bf6af4a8c7053049a6f
    var audio int64
-   flag.Int64Var(&audio, "g", 999999, "audio bandwidth")
+   flag.Int64Var(&audio, "g", 128000, "audio bandwidth")
    // i
    flag.BoolVar(&down.info, "i", false, "information")
    // k
@@ -48,11 +120,13 @@ func main() {
    flag.BoolVar(&verbose, "v", false, "verbose")
    flag.Parse()
    if verbose {
-      paramount.LogLevel = 1
+      roku.LogLevel = 1
    }
-   if guid != "" {
-      var err error
-      down.Preview, err = paramount.NewMedia(guid).Preview()
+   if id != "" || address != "" {
+      if id == "" {
+         id = roku.ContentID(address)
+      }
+      down.Content, err = roku.NewContent(id)
       if err != nil {
          panic(err)
       }
@@ -71,10 +145,9 @@ func main() {
       flag.Usage()
    }
 }
-
 type downloader struct {
    *dash.Period
-   *paramount.Preview
+   *roku.Content
    *url.URL
    client string
    info bool
@@ -83,7 +156,11 @@ type downloader struct {
 }
 
 func (d *downloader) setKey() error {
-   sess, err := paramount.NewSession(d.Preview.GUID)
+   site, err := roku.NewCrossSite()
+   if err != nil {
+      return err
+   }
+   play, err := site.Playback(d.Meta.ID)
    if err != nil {
       return err
    }
@@ -99,11 +176,33 @@ func (d *downloader) setKey() error {
    if err != nil {
       return err
    }
-   d.key, err = sess.Key(privateKey, clientID, kID)
+   d.key, err = play.Key(privateKey, clientID, kID)
    if err != nil {
       return err
    }
    return nil
+}
+
+func (d downloader) DASH(video, audio int64) error {
+   if d.info {
+      fmt.Println(d.Content)
+   }
+   videoDASH := d.Content.DASH()
+   fmt.Println("GET", videoDASH.URL)
+   res, err := http.Get(videoDASH.URL)
+   if err != nil {
+      return err
+   }
+   defer res.Body.Close()
+   d.URL = res.Request.URL
+   d.Period, err = dash.NewPeriod(res.Body)
+   if err != nil {
+      return err
+   }
+   if err := d.download(audio, dash.Audio); err != nil {
+      return err
+   }
+   return d.download(video, dash.Video)
 }
 
 func (d *downloader) download(band int64, fn dash.PeriodFunc) error {
@@ -111,9 +210,6 @@ func (d *downloader) download(band int64, fn dash.PeriodFunc) error {
       return nil
    }
    reps := d.Represents(fn)
-   sort.Slice(reps, func(a, b int) bool {
-      return reps[a].Bandwidth < reps[b].Bandwidth
-   })
    rep := reps.Represent(band)
    if d.info {
       for _, each := range reps {
@@ -133,7 +229,7 @@ func (d *downloader) download(band int64, fn dash.PeriodFunc) error {
       if err != nil {
          return err
       }
-      file, err := os.Create(d.Base()+ext)
+      file, err := os.Create(d.Content.Base()+ext)
       if err != nil {
          return err
       }
@@ -172,108 +268,3 @@ func (d *downloader) download(band int64, fn dash.PeriodFunc) error {
    }
    return nil
 }
-
-func (d downloader) DASH(video, audio int64) error {
-   addr, err := paramount.NewMedia(d.GUID).DASH()
-   if err != nil {
-      return err
-   }
-   fmt.Println("GET", addr)
-   res, err := http.Get(addr.String())
-   if err != nil {
-      return err
-   }
-   defer res.Body.Close()
-   d.URL = addr
-   d.Period, err = dash.NewPeriod(res.Body)
-   if err != nil {
-      return err
-   }
-   if err := d.download(audio, dash.Audio); err != nil {
-      return err
-   }
-   return d.download(video, dash.Video)
-}
-
-
-func (d downloader) HLS(bandwidth int64) error {
-   addr, err := paramount.NewMedia(d.GUID).HLS()
-   if err != nil {
-      return err
-   }
-   fmt.Println("GET", addr)
-   res, err := http.Get(addr.String())
-   if err != nil {
-      return err
-   }
-   defer res.Body.Close()
-   master, err := hls.NewScanner(res.Body).Master()
-   if err != nil {
-      return err
-   }
-   sort.Slice(master.Streams, func(a, b int) bool {
-      return master.Streams[a].Bandwidth < master.Streams[b].Bandwidth
-   })
-   stream := master.Streams.GetBandwidth(bandwidth)
-   if d.info {
-      fmt.Println(d.Title)
-      for _, each := range master.Streams {
-         if each.Bandwidth == stream.Bandwidth {
-            fmt.Print("!")
-         }
-         fmt.Println(each)
-      }
-   } else {
-      return download(stream.RawURI, d.Base())
-   }
-   return nil
-}
-
-func download(addr, base string) error {
-   seg, err := newSegment(addr)
-   if err != nil {
-      return err
-   }
-   fmt.Println("GET", seg.RawKey)
-   res, err := http.Get(seg.RawKey)
-   if err != nil {
-      return err
-   }
-   defer res.Body.Close()
-   block, err := hls.NewCipher(res.Body)
-   if err != nil {
-      return err
-   }
-   file, err := os.Create(base + hls.TS)
-   if err != nil {
-      return err
-   }
-   defer file.Close()
-   pro := format.ProgressChunks(file, len(seg.Protected))
-   for _, addr := range seg.Protected {
-      res, err := http.Get(addr)
-      if err != nil {
-         return err
-      }
-      pro.AddChunk(res.ContentLength)
-      if _, err := block.Copy(pro, res.Body, nil); err != nil {
-         return err
-      }
-      if err := res.Body.Close(); err != nil {
-         return err
-      }
-   }
-   return nil
-}
-
-func newSegment(addr string) (*hls.Segment, error) {
-   fmt.Println("GET", addr)
-   res, err := http.Get(addr)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   return hls.NewScanner(res.Body).Segment()
-}
-
-
