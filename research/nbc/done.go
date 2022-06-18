5db68ff747bc15e9ce14cb1de2a295d4e6f45b21
Zombo <srpen6@gmail.com>
2022-06-18 18:59:10 -0500

    new file:   research/nbc/todo.go

---
+++ research/nbc/done.go
@@ -0,0 +1,108 @@
+package nbc
+
+import (
+   "crypto/hmac"
+   "crypto/sha256"
+   "encoding/hex"
+   "encoding/json"
+   "errors"
+   "github.com/89z/format"
+   "github.com/89z/mech"
+   "io"
+   "net/http"
+   "strconv"
+)
+
+const persistedQuery = "872a3dffc3ae6cdb3dc69fe3d9a949b539de7b579e95b2942e68d827b1a6ec62"
+
+var (
+   LogLevel format.LogLevel
+   secretKey = []byte("2b84a073ede61c766e4c0b3f1e656f7f")
+)
+
+func writeHash(dst io.Writer, text string, key []byte) {
+   mac := hmac.New(sha256.New, key)
+   io.WriteString(mac, text)
+   hex.NewEncoder(dst).Write(mac.Sum(nil))
+}
+
+type AccessVOD struct {
+   ManifestPath string // this is only valid for one minute
+}
+
+type Video struct {
+   Data struct {
+      BonanzaPage struct {
+         Analytics struct {
+            ConvivaAssetName string
+         }
+         Metadata struct {
+            MpxAccountID string
+         }
+      }
+   }
+}
+
+func (v Video) Base() string {
+   return mech.Clean(v.Data.BonanzaPage.Analytics.ConvivaAssetName)
+}
+
+type videoRequest struct {
+   Extensions struct {
+      PersistedQuery struct {
+         Sha256Hash string `json:"sha256Hash"`
+      } `json:"persistedQuery"`
+   } `json:"extensions"`
+   Variables struct {
+      App string `json:"app"`
+      Name string `json:"name"` // String cannot represent a non string value
+      OneApp bool `json:"oneApp"`
+      Platform string `json:"platform"`
+      Type string `json:"type"`
+      UserID string `json:"userId"` // can be empty
+   } `json:"variables"`
+}
+
+type vodRequest struct {
+   Device string `json:"device"`
+   DeviceID string `json:"deviceId"`
+   ExternalAdvertiserID string `json:"externalAdvertiserId"`
+   Mpx struct {
+      AccountID int `json:"accountId"`
+   } `json:"mpx"`
+}
+
+func NewVideo(guid int64) (*Video, error) {
+   var body videoRequest
+   body.Extensions.PersistedQuery.Sha256Hash = persistedQuery
+   body.Variables.App = "nbc"
+   body.Variables.Name = strconv.FormatInt(guid, 10)
+   body.Variables.OneApp = true
+   body.Variables.Platform = "android"
+   body.Variables.Type = "VIDEO"
+   buf, err := mech.Encode(body)
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest(
+      "POST", "https://friendship.nbc.co/v2/graphql", buf,
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.Header.Set("Content-Type", "application/json")
+   LogLevel.Dump(req)
+   res, err := new(http.Transport).RoundTrip(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   if res.StatusCode != http.StatusOK {
+      return nil, errors.New(res.Status)
+   }
+   vid := new(Video)
+   if err := json.NewDecoder(res.Body).Decode(vid); err != nil {
+      return nil, err
+   }
+   return vid, nil
+}
