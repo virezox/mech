6c4b88ba618d4840f8dd1be22365d9e1a325a309
Zombo <srpen6@gmail.com>
2022-07-01 19:14:57 -0500

    +func (f flags) DASH() error {

--- cmd/amc/done.go
+++ cmd/amc/done.go
@@ -7,6 +7,66 @@ import (
    "path/filepath"
 )
 
+func (f flags) DASH() error {
+   home, err := os.UserHomeDir()
+   if err != nil {
+      return err
+   }
+   auth, err := amc.Open_Auth(home + "/mech/amc.json")
+   if err != nil {
+      return err
+   }
+   if err := auth.Refresh(); err != nil {
+      return err
+   }
+   if err := auth.Create(home + "/mech/amc.json"); err != nil {
+      return err
+   }
+   if f.nid == 0 {
+      f.nid, err = amc.Get_NID(f.address)
+      if err != nil {
+         return err
+      }
+   }
+   play, err = auth.Playback(nid)
+   if err != nil {
+      return err
+   }
+   source := play.Source()
+   res, err := amc.Client.Redirect(nil).Get(source.Src)
+   if err != nil {
+      return err
+   }
+   defer res.Body.Close()
+   // Media
+   var media dash.Media
+   if err := xml.NewDecoder(res.Body).Decode(&media); err != nil {
+      return err
+   }
+   reps := media.Representations().Video()
+   if f.video_bandwidth >= 1 {
+      rep := reps.Get_Bandwidth(f.video_bandwidth)
+      if f.info {
+         for _, each := range reps {
+            if each.Bandwidth == rep.Bandwidth {
+               fmt.Print("!")
+            }
+            fmt.Println(each)
+         }
+      } else {
+         var key []byte
+         if source.Key_Systems != nil {
+            key, err = f.key(play, rep.ContentProtection.Default_KID)
+            if err != nil {
+               return err
+            }
+         }
+         return download(key, play.Base())
+      }
+   }
+   return nil
+}
+
 type stream struct {
    bandwidth int
    base string
--- cmd/amc/todo.go
+++ cmd/amc/todo.go
@@ -11,63 +11,45 @@ import (
    "io"
 )
 
-func (f flags) DASH() error {
-   home, err := os.UserHomeDir()
+func download(key []byte base string) error {
+   file, err := os.Create(str.base + rep.Ext())
    if err != nil {
       return err
    }
-   auth, err := amc.Open_Auth(home + "/mech/amc.json")
-   if err != nil {
-      return err
-   }
-   if err := auth.Refresh(); err != nil {
-      return err
-   }
-   if err := auth.Create(home + "/mech/amc.json"); err != nil {
-      return err
-   }
-   if f.nid == 0 {
-      f.nid, err = amc.Get_NID(f.address)
-      if err != nil {
-         return err
-      }
-   }
-   play, err = auth.Playback(nid)
-   if err != nil {
-      return err
-   }
-   key := play.Source().Key_Systems
-   if key == nil {
-      // no key
-      return nil
-   }
-   res, err := amc.Client.Redirect(nil).Get(play.DASH().Src)
+   defer file.Close()
+   res, err := amc.Client.Redirect(nil).Get(rep.Initialization())
    if err != nil {
       return err
    }
    defer res.Body.Close()
-   // Media
-   var media dash.Media
-   if err := xml.NewDecoder(res.Body).Decode(&media); err != nil {
+   media := rep.Media()
+   pro := os.Progress_Chunks(file, len(media))
+   dec := mp4.New_Decrypt(pro)
+   if err := dec.Init(res.Body); err != nil {
       return err
    }
-   reps := media.Representations()
-   // stream
-   var str stream
-   str.base = play.Base()
-   // audio
-   str.Representations = reps.Audio()
-   str.bandwidth = f.audio_bandwidth
-   if err := f.download(str); err != nil {
-      return err
+   for _, addr := range media {
+      res, err := amc.Client.Redirect(nil).Level(0).Get(addr.String())
+      if err != nil {
+         return err
+      }
+      pro.Add_Chunk(res.ContentLength)
+      if f.key != nil {
+         err = dec.Segment(res.Body, f.key)
+      } else {
+         _, err = io.Copy(pro, res.Body)
+      }
+      if err != nil {
+         return err
+      }
+      if err := res.Body.Close(); err != nil {
+         return err
+      }
    }
-   // video
-   str.Representations = reps.Video()
-   str.bandwidth = f.video_bandwidth
-   return f.download(str)
+   return nil
 }
 
-func (f *flags) set_key() error {
+func (f *flags) key(p widevine.Poster, raw_key_id string) ([]byte, error) {
    private_key, err := os.ReadFile(f.private_key)
    if err != nil {
       return err
@@ -76,7 +58,6 @@ func (f *flags) set_key() error {
    if err != nil {
       return err
    }
-   raw_key_id := f.media.Representations()[0].ContentProtection.Default_KID
    key_ID, err := widevine.Key_ID(raw_key_id)
    if err != nil {
       return err
@@ -85,66 +66,9 @@ func (f *flags) set_key() error {
    if err != nil {
       return err
    }
-   keys, err := mod.Post(f.Playback)
+   keys, err := mod.Post(p)
    if err != nil {
       return err
    }
-   f.key = keys.Content().Key
-   return nil
+   return keys.Content().Key, nil
 }
-
-func (f *flags) download(str stream) error {
-   if str.bandwidth == 0 {
-      return nil
-   }
-   rep := reps.Get_Bandwidth(str.bandwidth)
-   if f.info {
-      for _, each := range reps {
-         if each.Bandwidth == rep.Bandwidth {
-            fmt.Print("!")
-         }
-         fmt.Println(each)
-      }
-   } else {
-      file, err := os.Create(str.base + rep.Ext())
-      if err != nil {
-         return err
-      }
-      defer file.Close()
-      res, err := amc.Client.Redirect(nil).Get(rep.Initialization())
-      if err != nil {
-         return err
-      }
-      defer res.Body.Close()
-      media := rep.Media()
-      pro := os.Progress_Chunks(file, len(media))
-      dec := mp4.New_Decrypt(pro)
-      if err := dec.Init(res.Body); err != nil {
-         return err
-      }
-      for _, raw := range media {
-         addr, err := f.url.Parse(raw)
-         if err != nil {
-            return err
-         }
-         res, err := amc.Client.Redirect(nil).Level(0).Get(addr.String())
-         if err != nil {
-            return err
-         }
-         pro.Add_Chunk(res.ContentLength)
-         if f.key != nil {
-            err = dec.Segment(res.Body, f.key)
-         } else {
-            _, err = io.Copy(pro, res.Body)
-         }
-         if err != nil {
-            return err
-         }
-         if err := res.Body.Close(); err != nil {
-            return err
-         }
-      }
-   }
-   return nil
-}
-
