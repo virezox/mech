7e6deac9e1086e335f9d7b4f9807cdc8ed593459
Zombo <srpen6@gmail.com>
2022-07-26 12:07:10 -0500

    new file:   research/bally/bally.go

---
+++ research/bally/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+   "flag"
+   "github.com/89z/mech/widevine"
+   "github.com/89z/rosso/http"
+   "github.com/89z/rosso/mp4"
+)
+
+var client = http.Default_Client
+
+type flags struct {
+   Client_ID string
+   Info bool
+   Name string
+   Poster widevine.Poster
+   Private_Key string
+   address string
+   bandwidth int64
+   codec string
+   key string
+   base string
+   init bool
+   vpos int
+   apos int
+   video mp4.Decrypt
+   audio mp4.Decrypt
+}
+
+func main() {
+   var f flags
+   // a
+   flag.StringVar(&f.address, "a", "", "address")
+   // f
+   flag.Int64Var(&f.bandwidth, "f", 1, "video bandwidth")
+   // g
+   flag.StringVar(&f.codec, "g", "mp4a", "audio codec")
+   // i
+   flag.BoolVar(&f.Info, "i", false, "information")
+   // k
+   flag.StringVar(&f.key, "k", "", "key")
+   // o
+   flag.StringVar(&f.Name, "o", "ballysports", "output")
+   flag.Parse()
+   if f.address != "" {
+      if err := f.DASH(); err != nil {
+         panic(err)
+      }
+   } else {
+      flag.Usage()
+   }
+}
