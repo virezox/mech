3f1886198fcafba5817e2814d5f98ca2a7dcc05f
Zombo <srpen6@gmail.com>
2022-08-08 15:24:48 -0500

    deleted:    vimeo/video.json

--- research/vimeo/vimeo_test.go
+++ research/vimeo/vimeo_test.go
@@ -2,26 +2,36 @@ package vimeo
 
 import (
    "fmt"
+   "strconv"
    "testing"
    "time"
 )
 
-var tests = []string{
-   "http://embed.vhx.tv/subscriptions/17901?vimeo=1",
-   "http://embed.vhx.tv/videos/17901?vimeo=1",
+const (
+   id = 18432
+   origin = "http://embed.vhx.tv"
+)
+
+func tests() [2]string {
+   var refs [2]string
+   refs[0] = origin + "/subscriptions/" + strconv.Itoa(id) + "?vimeo=1"
+   refs[1] = origin + "/videos/" + strconv.Itoa(id) + "?vimeo=1"
+   return refs
 }
 
 func Test_Embed(t *testing.T) {
-   for _, test := range tests {
-      emb, err := New_Embed(test)
-      if err != nil {
-         t.Fatal(err)
+   for i := 0; i < 9; i++ {
+      for _, test := range tests() {
+         emb, err := New_Embed(test)
+         if err != nil {
+            t.Fatal(err)
+         }
+         con, err := emb.Config()
+         if err != nil {
+            t.Fatal(err)
+         }
+         fmt.Println(con)
+         time.Sleep(time.Second)
       }
-      con, err := emb.Config()
-      if err != nil {
-         t.Fatal(err)
-      }
-      fmt.Println(con)
-      time.Sleep(time.Second)
    }
 }
