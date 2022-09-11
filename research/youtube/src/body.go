3642a987e1d7d05cb2d0f190bb53c55e712ee358
Zombo <srpen6@gmail.com>
2022-09-11 17:21:28 -0500

    new file:   research/youtube/Cargo.toml

---
+++ research/youtube/src/body.go
@@ -0,0 +1,126 @@
+package youtube
+
+import (
+   "bytes"
+   "encoding/json"
+   "net/http"
+)
+
+const goog_API = "AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8"
+
+// com.google.android.youtube
+const android_version = "17.32.35"
+
+func Mobile_Web() Request {
+   var r Request
+   r.body.Context.Client.Name = "MWEB"
+   r.body.Context.Client.Version = "2.20220322.05.00"
+   return r
+}
+
+func Android() Request {
+   var r Request
+   r.body.Context.Client.Name = "ANDROID"
+   r.body.Context.Client.Version = android_version
+   return r
+}
+
+func Android_Embed() Request {
+   var r Request
+   r.body.Context.Client.Name = "ANDROID_EMBEDDED_PLAYER"
+   r.body.Context.Client.Version = android_version
+   return r
+}
+
+func Android_Racy() Request {
+   var r Request
+   r.body.Context.Client.Name = "ANDROID"
+   r.body.Context.Client.Version = android_version
+   r.body.Racy_Check_OK = true
+   return r
+}
+
+func Android_Content() Request {
+   var r Request
+   r.body.Content_Check_OK = true
+   r.body.Context.Client.Name = "ANDROID"
+   r.body.Context.Client.Version = android_version
+   r.body.Racy_Check_OK = true
+   return r
+}
+
+func (r Request) Search(query string) (*Search, error) {
+   filter := New_Filter()
+   filter.Type(Type["Video"])
+   param := New_Params()
+   param.Filter(filter)
+   r.body.Params = param.Marshal()
+   r.body.Query = query
+   buf, err := json.Marshal(r.body)
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest(
+      "POST", origin + "/youtubei/v1/search", bytes.NewReader(buf),
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.Header.Set("X-Goog-API-Key", goog_API)
+   res, err := HTTP_Client.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   search := new(Search)
+   if err := json.NewDecoder(res.Body).Decode(search); err != nil {
+      return nil, err
+   }
+   return search, nil
+}
+
+type Request struct {
+   Header *Header
+   body struct {
+      Context struct {
+         Client struct {
+            Name string `json:"clientName"`
+            Version string `json:"clientVersion"`
+         } `json:"client"`
+      } `json:"context"`
+      Content_Check_OK bool `json:"contentCheckOk,omitempty"`
+      Params []byte `json:"params,omitempty"`
+      Query string `json:"query,omitempty"`
+      Racy_Check_OK bool `json:"racyCheckOk,omitempty"`
+      Video_ID string `json:"videoId,omitempty"`
+   }
+}
+
+func (r Request) Player(id string) (*Player, error) {
+   r.body.Video_ID = id
+   buf, err := json.MarshalIndent(r.body, "", " ")
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest(
+      "POST", origin + "/youtubei/v1/player", bytes.NewReader(buf),
+   )
+   if err != nil {
+      return nil, err
+   }
+   if r.Header != nil {
+      req.Header.Set("Authorization", "Bearer " + r.Header.Access_Token)
+   } else {
+      req.Header.Set("X-Goog-API-Key", goog_API)
+   }
+   res, err := HTTP_Client.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   play := new(Player)
+   if err := json.NewDecoder(res.Body).Decode(play); err != nil {
+      return nil, err
+   }
+   return play, nil
+}
---
+++ research/youtube/src/format.go
@@ -0,0 +1,120 @@
+package youtube
+
+import (
+   "errors"
+   "github.com/89z/rosso/os"
+   "io"
+   "mime"
+   "net/http"
+   "strconv"
+)
+
+func (f Format) Ext() (string, error) {
+   media, _, err := mime.ParseMediaType(f.MimeType)
+   if err != nil {
+      return "", err
+   }
+   switch media {
+   case "audio/mp4":
+      return ".m4a", nil
+   case "audio/webm":
+      return ".weba", nil
+   case "video/mp4":
+      return ".m4v", nil
+   case "video/webm":
+      return ".webm", nil
+   }
+   return "", errors.New(f.MimeType)
+}
+
+type Format struct {
+   AudioQuality string
+   QualityLabel string
+   Width int
+   Height int
+   Bitrate int64
+   ContentLength int64 `json:"contentLength,string"`
+   MimeType string
+   URL string
+}
+
+func (f Format) MarshalText() ([]byte, error) {
+   b := []byte("Quality:")
+   if f.QualityLabel != "" {
+      b = append(b, f.QualityLabel...)
+   } else {
+      b = append(b, f.AudioQuality...)
+   }
+   b = append(b, " Bitrate:"...)
+   b = strconv.AppendInt(b, f.Bitrate, 10)
+   if f.ContentLength >= 1 { // Tq92D6wQ1mg
+      b = append(b, " ContentLength:"...)
+      b = strconv.AppendInt(b, f.ContentLength, 10)
+   }
+   b = append(b, "\n\tMimeType:"...)
+   b = append(b, f.MimeType...)
+   return append(b, '\n'), nil
+}
+
+type Formats []Format
+
+func (f Format) Encode(w io.Writer) error {
+   req, err := http.NewRequest("GET", f.URL, nil)
+   if err != nil {
+      return err
+   }
+   pro := os.Progress_Bytes(w, f.ContentLength)
+   var pos int64
+   for pos < f.ContentLength {
+      b := []byte("bytes=")
+      b = strconv.AppendInt(b, pos, 10)
+      b = append(b, '-')
+      b = strconv.AppendInt(b, pos+chunk-1, 10)
+      req.Header.Set("Range", string(b))
+      res, err := HTTP_Client.Level(0).Redirect(nil).Status(206).Do(req)
+      if err != nil {
+         return err
+      }
+      if _, err := io.Copy(pro, res.Body); err != nil {
+         return err
+      }
+      if err := res.Body.Close(); err != nil {
+         return err
+      }
+      pos += chunk
+   }
+   return nil
+}
+
+func (f Formats) Audio(quality string) (*Format, bool) {
+   for _, form := range f {
+      if form.AudioQuality == quality {
+         return &form, true
+      }
+   }
+   return nil, false
+}
+
+func (f Formats) Video(height int) (*Format, bool) {
+   distance := func(f *Format) int {
+      if f.Height > height {
+         return f.Height - height
+      }
+      return height - f.Height
+   }
+   var (
+      ok bool
+      output *Format
+   )
+   for i, input := range f {
+      // since codecs are in this order avc1,vp9,av01,
+      // do "<=" so we can get last one
+      if output == nil || distance(&input) <= distance(output) {
+         output = &f[i]
+         ok = true
+      }
+   }
+   return output, ok
+}
+
+const chunk = 10_000_000
