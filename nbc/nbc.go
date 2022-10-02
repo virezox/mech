5907bf83ef7479b503afb166ab98df535f7edbb1
Zombo <srpen6@gmail.com>
2022-10-02 14:23:28 -0500

    +func authorization() string {

--- nbc/nbc.go
+++ nbc/nbc.go
@@ -13,6 +13,71 @@ import (
    "time"
 )
 
+const persisted_query = "872a3dffc3ae6cdb3dc69fe3d9a949b539de7b579e95b2942e68d827b1a6ec62"
+
+var (
+   Client = http.Default_Client
+   secret_key = []byte("2b84a073ede61c766e4c0b3f1e656f7f")
+)
+
+func authorization() string {
+   now := strconv.FormatInt(time.Now().UnixMilli(), 10)
+   str := new(strings.Builder)
+   str.WriteString("NBC-Security key=android_nbcuniversal,version=2.4")
+   str.WriteString(",time=")
+   str.WriteString(now)
+   str.WriteString(",hash=")
+   mac := hmac.New(sha256.New, secret_key)
+   io.WriteString(mac, now)
+   hex.NewEncoder(str).Write(mac.Sum(nil))
+   return str.String()
+}
+
+type Bonanza_Page struct {
+   Analytics struct {
+      ConvivaAssetName string
+   }
+   Metadata struct {
+      MpxAccountId string
+   }
+   Name string
+}
+
+func New_Bonanza_Page(guid int64) (*Bonanza_Page, error) {
+   var p page_request
+   p.Extensions.Persisted_Query.SHA_256_Hash = persisted_query
+   p.Variables.App = "nbc"
+   p.Variables.Name = strconv.FormatInt(guid, 10)
+   p.Variables.One_App = true
+   p.Variables.Platform = "android"
+   p.Variables.Type = "VIDEO"
+   body, err := json.MarshalIndent(p, "", " ")
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest(
+      "POST", "https://friendship.nbc.co/v2/graphql", bytes.NewReader(body),
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.Header.Set("Content-Type", "application/json")
+   res, err := Client.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   var page struct {
+      Data struct {
+         BonanzaPage Bonanza_Page
+      }
+   }
+   if err := json.NewDecoder(res.Body).Decode(&page); err != nil {
+      return nil, err
+   }
+   return &page.Data.BonanzaPage, nil
+}
+
 func (b Bonanza_Page) Video() (*Video, error) {
    var v video_request
    v.Device = "android"
@@ -23,11 +88,11 @@ func (b Bonanza_Page) Video() (*Video, error) {
    if err != nil {
       return nil, err
    }
-   var buf strings.Builder
-   buf.WriteString("http://access-cloudpath.media.nbcuni.com")
-   buf.WriteString("/access/vod/nbcuniversal/")
-   buf.WriteString(b.Name)
-   req, err := http.NewRequest("POST", buf.String(), bytes.NewReader(body))
+   var str strings.Builder
+   str.WriteString("http://access-cloudpath.media.nbcuni.com")
+   str.WriteString("/access/vod/nbcuniversal/")
+   str.WriteString(b.Name)
+   req, err := http.NewRequest("POST", str.String(), bytes.NewReader(body))
    if err != nil {
       return nil, err
    }
@@ -75,68 +140,3 @@ type video_request struct {
       Account_ID string `json:"accountId"`
    } `json:"mpx"`
 }
-const persisted_query = "872a3dffc3ae6cdb3dc69fe3d9a949b539de7b579e95b2942e68d827b1a6ec62"
-
-var (
-   Client = http.Default_Client
-   secret_key = []byte("2b84a073ede61c766e4c0b3f1e656f7f")
-)
-
-func authorization() string {
-   now := strconv.FormatInt(time.Now().UnixMilli(), 10)
-   buf := new(strings.Builder)
-   buf.WriteString("NBC-Security key=android_nbcuniversal,version=2.4")
-   buf.WriteString(",time=")
-   buf.WriteString(now)
-   buf.WriteString(",hash=")
-   mac := hmac.New(sha256.New, secret_key)
-   io.WriteString(mac, now)
-   hex.NewEncoder(buf).Write(mac.Sum(nil))
-   return buf.String()
-}
-
-func New_Bonanza_Page(guid int64) (*Bonanza_Page, error) {
-   var p page_request
-   p.Extensions.Persisted_Query.SHA_256_Hash = persisted_query
-   p.Variables.App = "nbc"
-   p.Variables.Name = strconv.FormatInt(guid, 10)
-   p.Variables.One_App = true
-   p.Variables.Platform = "android"
-   p.Variables.Type = "VIDEO"
-   buf, err := json.MarshalIndent(p, "", " ")
-   if err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest(
-      "POST", "https://friendship.nbc.co/v2/graphql", bytes.NewReader(buf),
-   )
-   if err != nil {
-      return nil, err
-   }
-   req.Header.Set("Content-Type", "application/json")
-   res, err := Client.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   var page struct {
-      Data struct {
-         BonanzaPage Bonanza_Page
-      }
-   }
-   if err := json.NewDecoder(res.Body).Decode(&page); err != nil {
-      return nil, err
-   }
-   return &page.Data.BonanzaPage, nil
-}
-
-type Bonanza_Page struct {
-   Analytics struct {
-      ConvivaAssetName string
-   }
-   Metadata struct {
-      MpxAccountId string
-   }
-   Name string
-}
-
