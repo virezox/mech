3e2e3c62b34b1f5dd707497a6a03f852c7b7edcc
Zombo <srpen6@gmail.com>
2022-10-27 17:53:10 -0500

    +   verbose bool

--- cmd/vimeo/main.go
+++ cmd/vimeo/main.go
@@ -10,17 +10,19 @@ type flags struct {
    address string
    height int64
    info bool
+   verbose bool
 }
 
 func main() {
    var f flags
-   // a
    flag.StringVar(&f.address, "a", "", "address")
-   // f
    flag.Int64Var(&f.height, "f", 720, "target height")
-   // i
    flag.BoolVar(&f.info, "i", false, "info only")
+   flag.BoolVar(&f.verbose, "v", false, "verbose")
    flag.Parse()
+   if f.verbose {
+      vimeo.Client.Log_Level = 2
+   }
    if strings.Contains(f.address, "vimeo.com/") {
       err := f.vimeo()
       if err != nil {
